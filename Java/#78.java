9738649590271fc1efb49ee19e98299ba7f4e2d7
uprightclear <uprightclear@gmail.com>
2021-07-31 10:15:06 +0800

    update

---
+++ Java/#78.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<Integer> tmp = new ArrayDeque<>();
+        for(int i = 0; i <= nums.length; i++){
+            dfs(nums, i, 0, tmp, res);  
+        }
+        return res;
+    }
+    //n：长度为n的数组 s：长度为n的数组中的起始位置
+    public void dfs(int[] nums, int n, int s, Deque<Integer> tmp, List<List<Integer>> res){
+        if(n == tmp.size()){
+            res.add(new ArrayList<>(tmp));
+            return;
+        }
+        for(int i = s; i < nums.length; i++){
+            tmp.addLast(nums[i]);
+            dfs(nums, n, i + 1, tmp, res);
+            tmp.removeLast();  
+        }
+    }
+}
