0bc5ca655a489e61b480677bd1717edf1100fc55
uprightclear <uprightclear@gmail.com>
2021-08-04 09:39:13 +0800

    20210803 update

---
+++ Java/611.java
@@ -0,0 +1,45 @@
+//排序+二分查找
+class Solution {
+    public int triangleNumber(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int ans = 0;
+        //如果我们将三条边进行升序排序，使它们满足 a ≤ b ≤ c，那么 a + c > b 和 b + c > a 使一定成立的，我们只需要保证 a + b > c
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int left = j + 1, right = n - 1, k = j;
+                //二分查找，找出最大的 nums[k] < nums[i] + nums[j]
+                while (left <= right) {
+                    int mid = (left + right) / 2;
+                    if (nums[mid] < nums[i] + nums[j]) {
+                        k = mid;
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+                ans += k - j;
+            }
+        }
+        return ans;
+    }
+}
+
+//排序+双指针
+class Solution {
+    public int triangleNumber(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int k = i;
+            for (int j = i + 1; j < n; ++j) {
+                while (k + 1 < n && nums[k + 1] < nums[i] + nums[j]) {
+                    ++k;
+                }
+                ans += Math.max(k - j, 0);
+            }
+        }
+        return ans;
+    }
+}
