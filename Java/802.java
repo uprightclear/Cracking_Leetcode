c1d3892bc4b0bb73ef975b70222c309834798644
uprightclear <uprightclear@gmail.com>
2021-08-05 15:55:29 +0800

    20210805 update

---
+++ Java/802.java
@@ -0,0 +1,72 @@
+//DFS + 三色标记
+//0：该节点尚未被访问 1：该节点位于递归栈中 2：该节点搜索完毕，是一个安全节点
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+        int[] color = new int[n];
+        List<Integer> ans = new ArrayList<Integer>();
+        for (int i = 0; i < n; ++i) {
+            if (safe(graph, color, i)) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+
+    public boolean safe(int[][] graph, int[] color, int x) {
+        if (color[x] > 0) {
+            return color[x] == 2;
+        }
+        color[x] = 1;
+        for (int y : graph[x]) {
+            if (!safe(graph, color, y)) {
+                return false;
+            }
+        }
+        color[x] = 2;
+        return true;
+    }
+}
+
+//拓扑排序
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+        List<List<Integer>> rg = new ArrayList<List<Integer>>();
+        for (int i = 0; i < n; ++i) {
+            rg.add(new ArrayList<Integer>());
+        }
+        int[] inDeg = new int[n];
+        for (int x = 0; x < n; ++x) {
+            for (int y : graph[x]) {
+                //反向拓扑 x -> y
+                rg.get(y).add(x);
+            }
+            //x的入度
+            inDeg[x] = graph[x].length;
+        }
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for (int i = 0; i < n; ++i) {
+            if (inDeg[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int y = queue.poll();
+            for (int x : rg.get(y)) {
+                if (--inDeg[x] == 0) {
+                    queue.offer(x);
+                }
+            }
+        }
+
+        List<Integer> ans = new ArrayList<Integer>();
+        for (int i = 0; i < n; ++i) {
+            if (inDeg[i] == 0) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
