3e9a84131cb14e07378184ec7f9e6b4740d5db0d
uprightclear <uprightclear@gmail.com>
2021-08-06 11:29:08 +0800

    20210806 update

---
+++ Java/847.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int shortestPathLength(int[][] graph) {
+        int n = graph.length;
+        Queue<int[]> queue = new LinkedList<int[]>();
+        boolean[][] seen = new boolean[n][1 << n];
+        for (int i = 0; i < n; ++i) {
+            queue.offer(new int[]{i, 1 << i, 0});
+            seen[i][1 << i] = true;
+        }
+
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int[] tuple = queue.poll();
+            int u = tuple[0], mask = tuple[1], dist = tuple[2];
+            if (mask == (1 << n) - 1) {
+                ans = dist;
+                break;
+            }
+            // 搜索相邻的节点
+            for (int v : graph[u]) {
+                // 将 mask 的第 v 位置为 1
+                int maskV = mask | (1 << v);
+                if (!seen[v][maskV]) {
+                    queue.offer(new int[]{v, maskV, dist + 1});
+                    seen[v][maskV] = true;
+                }
+            }
+        }
+        return ans;
+    }
+}
