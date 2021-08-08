f25646c519a487a49708c060de4b5dc3cb9554e4
uprightclear <uprightclear@gmail.com>
2021-08-08 10:30:37 +0800

    20210808 update

---
+++ Java/1137.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n <= 2) {
+            return 1;
+        }
+        int p = 0, q = 0, r = 1, s = 1;
+        for (int i = 3; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = s;
+            s = p + q + r;
+        }
+        return s;
+    }
+}
+
+//矩阵快速幂
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n <= 2) {
+            return 1;
+        }
+        int[][] q = {{1, 1, 1}, {1, 0, 0}, {0, 1, 0}};
+        int[][] res = pow(q, n);
+        return res[0][2];
+    }
+
+    public int[][] pow(int[][] a, int n) {
+        int[][] ret = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                ret = multiply(ret, a);
+            }
+            n >>= 1;
+            a = multiply(a, a);
+        }
+        return ret;
+    }
+    //矩阵乘法
+    public int[][] multiply(int[][] a, int[][] b) {
+        int[][] c = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j] + a[i][2] * b[2][j];
+            }
+        }
+        return c;
+    }
+}
