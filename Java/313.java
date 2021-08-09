38b7cac19e0bc07b11efb2d41d5e4e42718a8cf3
uprightclear <uprightclear@gmail.com>
2021-08-09 09:40:53 +0800

    20210809 update

---
+++ Java/313.java
@@ -0,0 +1,47 @@
+//最小堆
+class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        Set<Long> seen = new HashSet<Long>();
+        PriorityQueue<Long> heap = new PriorityQueue<Long>();
+        seen.add(1L);
+        heap.offer(1L);
+        int ugly = 0;
+        for (int i = 0; i < n; i++) {
+            long curr = heap.poll();
+            ugly = (int) curr;
+            for (int prime : primes) {
+                long next = curr * prime;
+                if (seen.add(next)) {
+                    heap.offer(next);
+                }
+            }
+        }
+        return ugly;
+    }
+}
+
+//DP
+class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        int m = primes.length;
+        int[] pointers = new int[m];
+        Arrays.fill(pointers, 1);
+        for (int i = 2; i <= n; i++) {
+            int[] nums = new int[m];
+            int minNum = Integer.MAX_VALUE;
+            for (int j = 0; j < m; j++) {
+                nums[j] = dp[pointers[j]] * primes[j];
+                minNum = Math.min(minNum, nums[j]);
+            }
+            dp[i] = minNum;
+            for (int j = 0; j < m; j++) {
+                if (minNum == nums[j]) {
+                    pointers[j]++;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
