069316d57eab581fdf56c43d315b40b3720e7443
uprightclear <uprightclear@gmail.com>
2021-08-13 15:25:12 +0800

    20210813 update

---
+++ Java/189.java
@@ -0,0 +1,57 @@
+//使用额外数组
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] newArr = new int[n];
+        for (int i = 0; i < n; ++i) {
+            newArr[(i + k) % n] = nums[i];
+        }
+        System.arraycopy(newArr, 0, nums, 0, n);
+    }
+}
+
+
+//环状替换   gcd：最大公约数
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        int count = gcd(k, n);
+        for (int start = 0; start < count; ++start) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % n;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+            } while (start != current);
+        }
+    }
+    //求最大公约数
+    public int gcd(int x, int y) {
+        return y > 0 ? gcd(y, x % y) : x;
+    }
+}
+
+
+//数组翻转
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start += 1;
+            end -= 1;
+        }
+    }
+}
---
+++ Java/977.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortedSquares(int[] A) {
+        int n = A.length;
+        int[] ans = new int[n];
+        for (int i = 0, j = n - 1, pos = n - 1; i <= j;) {
+            if (A[i] * A[i] > A[j] * A[j]) {
+                ans[pos] = A[i] * A[i];
+                ++i;
+            } else {
+                ans[pos] = A[j] * A[j];
+                --j;
+            }
+            --pos;
+        }
+        return ans;
+    }
+}
