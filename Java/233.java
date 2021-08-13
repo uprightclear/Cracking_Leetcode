be5be87483fa078a739d6f4eca445fd87160ad84
uprightclear <uprightclear@gmail.com>
2021-08-13 14:56:14 +0800

    20210813 update

---
+++ Java/233.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countDigitOne(int n) {
+        String s = String.valueOf(n);
+        int m = s.length();
+        if (m == 1) return n > 0 ? 1 : 0;
+        // 计算第 i 位前缀代表的数值，和后缀代表的数值
+        // 例如 abcde 则有 ps[2] = ab; ss[2] = de
+        int[] ps = new int[m], ss = new int[m];
+        ss[0] = Integer.parseInt(s.substring(1));
+        for (int i = 1; i < m - 1; i++) {
+            ps[i] = Integer.parseInt(s.substring(0, i));
+            ss[i] = Integer.parseInt(s.substring(i + 1));
+        }
+        ps[m - 1] = Integer.parseInt(s.substring(0, m - 1));
+
+        // 分情况讨论
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            // x 为当前位数值，len 为当前位后面长度为多少
+            int x = s.charAt(i) - '0', len = m - i - 1;
+            int prefix = ps[i], suffix = ss[i];
+            int tot = 0;
+            tot += prefix * Math.pow(10, len);
+            if (x == 0) {
+            } else if (x == 1) {
+                tot += suffix + 1;
+            } else {
+                tot += Math.pow(10, len);
+            }
+            ans += tot;
+        }
+        return ans;
+    }
+}
