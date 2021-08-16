24c8499cb674c8cc55db3117d48ae57ca705d05f
uprightclear <uprightclear@gmail.com>
2021-08-16 12:43:50 +0800

    20210816 update

---
+++ Java/19.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode first = head;
+        ListNode second = dummy;
+        for (int i = 0; i < n; ++i) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        ListNode ans = dummy.next;
+        return ans;
+    }
+}
