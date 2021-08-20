468011d5d0322ef98e3d0f666f3ba952840d7710
uprightclear <uprightclear@gmail.com>
2021-08-20 09:13:59 +0800

    20210820 update

---
+++ Java/541.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String reverseStr(String s, int k) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < n; i += 2 * k) {
+            reverse(arr, i, Math.min(i + k, n) - 1);
+        }
+        return new String(arr);
+    }
+
+    public void reverse(char[] arr, int left, int right) {
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
