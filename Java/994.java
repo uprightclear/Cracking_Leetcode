951b980115fff0d4a6e88df2f5319b9030598031
uprightclear <uprightclear@gmail.com>
2021-08-20 13:02:26 +0800

    20210820 update

--- Java/994.java
+++ Java/994.java
@@ -1,45 +1,38 @@
 class Solution {
-    int[] dr = new int[]{-1, 0, 1, 0};
-    int[] dc = new int[]{0, -1, 0, 1};
-
+    int[][] direction = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
     public int orangesRotting(int[][] grid) {
-        int R = grid.length, C = grid[0].length;
-        Queue<Integer> queue = new ArrayDeque<Integer>();
-        Map<Integer, Integer> depth = new HashMap<Integer, Integer>();
-        for (int r = 0; r < R; ++r) {
-            for (int c = 0; c < C; ++c) {
-                if (grid[r][c] == 2) {
-                    //code 第几个格子
-                    int code = r * C + c;
-                    queue.add(code);
-                    //第0分钟就已经腐烂
-                    depth.put(code, 0);
+        int m = grid.length;
+        int n = grid[0].length;
+        int res = 0, orange = 0;
+        Queue<int[]> queue = new LinkedList<>(); 
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 2){
+                    queue.offer(new int[]{i, j});
+                }else if(grid[i][j] == 1){
+                    orange++;
                 }
             }
         }
-        int ans = 0;
-        while (!queue.isEmpty()) {
-            int code = queue.remove();
-            int r = code / C, c = code % C;
-            for (int k = 0; k < 4; ++k) {
-                int nr = r + dr[k];
-                int nc = c + dc[k];
-                if (0 <= nr && nr < R && 0 <= nc && nc < C && grid[nr][nc] == 1) {
-                    grid[nr][nc] = 2;
-                    int ncode = nr * C + nc;
-                    queue.add(ncode);
-                    depth.put(ncode, depth.get(code) + 1);
-                    ans = depth.get(ncode);
+
+        if(orange == 0) return 0;
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            res++;
+            for(int i = 0; i < size; i++){
+                int[] rot = queue.poll();
+                int x = rot[0], y = rot[1];
+                for(int k = 0; k < 4; k++){
+                    int nx = x + direction[k][0], ny = y + direction[k][1];
+                    if(nx < 0 || nx >= m || ny < 0 || ny >= n || grid[nx][ny] != 1) continue;
+                    grid[nx][ny] = 2;
+                    orange--;
+                    queue.offer(new int[]{nx, ny});
                 }
             }
         }
-        for (int[] row: grid) {
-            for (int v: row) {
-                if (v == 1) {
-                    return -1;
-                }
-            }
-        }
-        return ans;
+        if(orange > 0) return -1;
+        return res - 1;
     }
 }
