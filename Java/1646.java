c0b77acc137e62591c89ac2bc03958968cfba1da
uprightclear <uprightclear@gmail.com>
2021-08-23 09:21:08 +0800

    20210823 update

---
+++ Java/1646.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int getMaximumGenerated(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int[] nums = new int[n + 1];
+        nums[1] = 1;
+        int max = 1;
+        for (int i = 2; i <= n; ++i) {
+            nums[i] = nums[i / 2] + i % 2 * nums[i / 2 + 1];
+            if(max < nums[i]){
+                max = nums[i];
+            }
+        }
+        return max;
+    }
+}
