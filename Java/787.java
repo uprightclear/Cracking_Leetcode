d0b44f33233b07ec0503ffa8a1da2eb51c7f43ab
uprightclear <uprightclear@gmail.com>
2021-08-24 09:44:58 +0800

    20210824 update

---
+++ Java/787.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        final int INF = 10000 * 101 + 1;
+        int[][] f = new int[k + 2][n];
+        for (int i = 0; i < k + 2; ++i) {
+            Arrays.fill(f[i], INF);
+        }
+        f[0][src] = 0;
+        for (int t = 1; t <= k + 1; ++t) {
+            for (int[] flight : flights) {
+                int j = flight[0], i = flight[1], cost = flight[2];
+                f[t][i] = Math.min(f[t][i], f[t - 1][j] + cost);
+            }
+        }
+        int ans = INF;
+        for (int t = 1; t <= k + 1; ++t) {
+            ans = Math.min(ans, f[t][dst]);
+        }
+        return ans == INF ? -1 : ans;
+    }
+}
+
+//优化
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        final int INF = 10000 * 101 + 1;
+        int[] f = new int[n];
+        Arrays.fill(f, INF);
+        f[src] = 0;
+        int ans = INF;
+        for (int t = 1; t <= k + 1; ++t) {
+            int[] g = new int[n];
+            Arrays.fill(g, INF);
+            for (int[] flight : flights) {
+                int j = flight[0], i = flight[1], cost = flight[2];
+                g[i] = Math.min(g[i], f[j] + cost);
+            }
+            f = g;
+            ans = Math.min(ans, f[dst]);
+        }
+        return ans == INF ? -1 : ans;
+    }
+}
