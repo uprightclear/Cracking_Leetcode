3453baf20e63049d2658fa537341b5f651825e0f
uprightclear <uprightclear@gmail.com>
2021-08-25 11:24:58 +0800

    20210825 update

---
+++ Java/1014.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxScoreSightseeingPair(int[] A) {
+        int ans = 0, mx = A[0] + 0;
+        for (int j = 1; j < A.length; ++j) {
+            ans = Math.max(ans, mx + A[j] - j);
+            // 边遍历边维护
+            mx = Math.max(mx, A[j] + j);
+        }
+        return ans;
+    }
+}
