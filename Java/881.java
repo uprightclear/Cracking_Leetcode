1e1dacdd181eeaf45b51372a002eac1b5cb506fc
uprightclear <uprightclear@gmail.com>
2021-08-26 09:29:50 +0800

    20210826 update

---
+++ Java/881.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int ans = 0;
+        Arrays.sort(people);
+        int light = 0, heavy = people.length - 1;
+        while (light <= heavy) {
+            if (people[light] + people[heavy] <= limit) {
+                ++light;
+            }
+            --heavy;
+            ++ans;
+        }
+        return ans;
+    }
+}
