1373cc63601af28c996762cb5ae66f988fc33a08
uprightclear <uprightclear@gmail.com>
2021-08-27 09:58:23 +0800

    20210827 update

---
+++ Java/295.java
@@ -0,0 +1,31 @@
+//priority queue
+class MedianFinder {
+    PriorityQueue<Integer> queMin;
+    PriorityQueue<Integer> queMax;
+
+    public MedianFinder() {
+        queMin = new PriorityQueue<Integer>((a, b) -> (b - a)); //由大到小，队顶元素是最大的
+        queMax = new PriorityQueue<Integer>((a, b) -> (a - b)); //由小到大，队顶元素是最小的
+    }
+    
+    public void addNum(int num) {
+        if (queMin.isEmpty() || num <= queMin.peek()) {
+            queMin.offer(num);
+            if (queMax.size() + 1 < queMin.size()) {
+                queMax.offer(queMin.poll());
+            }
+        } else {
+            queMax.offer(num);
+            if (queMax.size() > queMin.size()) {
+                queMin.offer(queMax.poll());
+            }
+        }
+    }
+    
+    public double findMedian() {
+        if (queMin.size() > queMax.size()) {
+            return queMin.peek();
+        }
+        return (queMin.peek() + queMax.peek()) / 2.0;
+    }
+}
