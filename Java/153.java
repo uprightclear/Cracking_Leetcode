85669b2c6405722549095a5798bc2807ff5c85a9
uprightclear <uprightclear@gmail.com>
2021-08-27 14:56:48 +0800

    20210827 update

---
+++ Java/153.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (nums[pivot] < nums[high]) {
+                high = pivot;
+            } else {
+                low = pivot + 1;
+            }
+        }
+        return nums[low];
+    }
+}
