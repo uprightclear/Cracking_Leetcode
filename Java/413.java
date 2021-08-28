872ccf97e285f815d17079cd2131ab3c80fa12b5
uprightclear <uprightclear@gmail.com>
2021-08-28 08:57:28 +0800

    20210828 update

--- Java/413.java
+++ Java/413.java
@@ -40,3 +40,25 @@ class Solution {
         return ans;
     }
 }
+
+//DP
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int len = nums.length;
+        if (len < 3) {
+            return 0;
+        }
+        
+        // dp[i] 表示以：nums[i] 结尾的、且长度大于等于 3 的连续等差数列的个数
+        int[] dp = new int[len];
+        int res = 0;
+        // 从下标 2 开始，才有可能构成长度至少大于等于 3 的等差数列
+        for (int i = 2; i < len; i++) {
+            if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                dp[i] = dp[i - 1] + 1;
+                res += dp[i];
+            }
+        }
+        return res;
+    }
+}
---
+++ Java/91.java
@@ -0,0 +1,15 @@
+//DP
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for(int i = 1; i <= n; i++){
+            if(s.charAt(i - 1) != '0') dp[i] += dp[i - 1];
+            if (i > 1 && s.charAt(i - 2) != '0' && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+}
