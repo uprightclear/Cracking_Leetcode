b8bd82c3c9812cf17be7b93475d6f51e2aa6a4a8
uprightclear <uprightclear@gmail.com>
2021-08-28 09:48:47 +0800

    20210828 update

---
+++ Java/82.java
@@ -0,0 +1,18 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
