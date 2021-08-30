111e6392aa87a26baddddfe629cba2b25abc1eef
uprightclear <uprightclear@gmail.com>
2021-08-30 11:01:39 +0800

    20210830 update

---
+++ Java/119.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> C = new ArrayList<List<Integer>>();
+        for (int i = 0; i <= rowIndex; ++i) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(C.get(i - 1).get(j - 1) + C.get(i - 1).get(j));
+                }
+            }
+            C.add(row);
+        }
+        return C.get(rowIndex);
+    }
+}
+
+//空间优化
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> pre = new ArrayList<Integer>();
+        for (int i = 0; i <= rowIndex; ++i) {
+            List<Integer> cur = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    cur.add(1);
+                } else {
+                    cur.add(pre.get(j - 1) + pre.get(j));
+                }
+            }
+            pre = cur;
+        }
+        return pre;
+    }
+}
