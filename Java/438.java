713ae547c45a3079f18644968586d930e8ca5760
uprightclear <uprightclear@gmail.com>
2021-08-30 11:35:24 +0800

    20210830 update

---
+++ Java/438.java
@@ -0,0 +1,54 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int n = s.length(), m = p.length();
+        List<Integer> res = new ArrayList<>();
+        if(n < m) return res;
+        int[] pCnt = new int[26];
+        int[] sCnt = new int[26];
+        for(int i = 0; i < m; i++){
+            pCnt[p.charAt(i) - 'a']++;
+            sCnt[s.charAt(i) - 'a']++;
+        }
+        if(Arrays.equals(sCnt, pCnt)){
+            res.add(0);
+        }
+        for(int i = m; i < n; i++){
+            sCnt[s.charAt(i - m) - 'a']--;
+            sCnt[s.charAt(i) - 'a']++;
+            if(Arrays.equals(sCnt, pCnt)){
+                res.add(i - m + 1);
+            }
+        }
+        return res;
+    }
+}
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int n = s.length(), m = p.length();
+        List<Integer> res = new ArrayList<>();
+        if(n < m) return res;
+
+        int[] pCnt = new int[26];
+        int[] sCnt = new int[26];
+
+        for(int i = 0; i < m; i++){
+            pCnt[p.charAt(i) - 'a'] ++;
+        }
+
+        int left = 0;
+        for(int right = 0; right < n; right++){
+            int curRight = s.charAt(right) - 'a';
+            sCnt[curRight]++;
+            while(sCnt[curRight] > pCnt[curRight]){
+                int curLeft = s.charAt(left) - 'a';
+                sCnt[curLeft]--;
+                left++;
+            }
+            if(right - left + 1 == m){
+                res.add(left);
+            }
+        }
+        return res;
+    }
+}
