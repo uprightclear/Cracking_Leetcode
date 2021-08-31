42bd1ddfbc179867c7a5301518506be089552a5a
uprightclear <uprightclear@gmail.com>
2021-08-31 08:57:10 +0800

    20210831 update

---
+++ Java/931.java
@@ -0,0 +1,22 @@
+//DP 从最后一行向前运行
+class Solution {
+    public int minFallingPathSum(int[][] A) {
+        int N = A.length;
+        for (int r = N-2; r >= 0; --r) {
+            for (int c = 0; c < N; ++c) {
+                // best = min(A[r+1][c-1], A[r+1][c], A[r+1][c+1])
+                int best = A[r+1][c];
+                if (c > 0)
+                    best = Math.min(best, A[r+1][c-1]);
+                if (c+1 < N)
+                    best = Math.min(best, A[r+1][c+1]);
+                A[r][c] += best;
+            }
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for (int x: A[0])
+            ans = Math.min(ans, x);
+        return ans;
+    }
+}
