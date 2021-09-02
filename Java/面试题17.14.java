c0d42a77738fd26b028aa3d45e2176acfb43e9dc
uprightclear <uprightclear@gmail.com>
2021-09-02 14:01:19 -0700

    20210902 update

--- Java/面试题17.14.java
+++ Java/面试题17.14.java
@@ -1,3 +1,4 @@
+//堆
 class Solution {
     public int[] smallestK(int[] arr, int k) {
         int[] vec = new int[k];
@@ -22,5 +23,58 @@ class Solution {
             vec[i] = queue.poll();
         }
         return vec;
+    }
+}
+
+//快速排序
+class Solution {
+    public int[] smallestK(int[] arr, int k) {
+        randomizedSelected(arr, 0, arr.length - 1, k);
+        int[] vec = new int[k];
+        for (int i = 0; i < k; ++i) {
+            vec[i] = arr[i];
+        }
+        return vec;
+    }
+
+    private void randomizedSelected(int[] arr, int l, int r, int k) {
+        if (l >= r) {
+            return;
+        }
+        int pos = randomizedPartition(arr, l, r);
+        int num = pos - l + 1;
+        if (k == num) {
+            return;
+        } else if (k < num) {
+            randomizedSelected(arr, l, pos - 1, k);
+        } else {
+            randomizedSelected(arr, pos + 1, r, k - num);
+        }
+    }
+
+    // 基于随机的划分
+    private int randomizedPartition(int[] nums, int l, int r) {
+        int i = new Random().nextInt(r - l + 1) + l;
+        swap(nums, r, i);
+        return partition(nums, l, r);
+    }
+
+    private int partition(int[] nums, int l, int r) {
+        int pivot = nums[r];
+        int i = l - 1;
+        for (int j = l; j <= r - 1; ++j) {
+            if (nums[j] <= pivot) {
+                i = i + 1;
+                swap(nums, i, j);
+            }
+        }
+        swap(nums, i + 1, r);
+        return i + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
