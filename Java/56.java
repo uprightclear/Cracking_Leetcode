9b49408a7a2db097db48f0423346badb16470060
uprightclear <uprightclear@gmail.com>
2021-09-10 09:25:53 -0700

    20210910 update

---
+++ Java/56.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        int len = intervals.length;
+        if (len < 2) {
+            return intervals;
+        }
+
+        // 按照起点排序
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+
+        // 也可以使用 Stack，因为我们只关心结果集的最后一个区间
+        List<int[]> res = new ArrayList<>();
+        res.add(intervals[0]);
+
+        for (int i = 1; i < len; i++) {
+            int[] curInterval = intervals[i];
+
+            // 每次新遍历到的列表与当前结果集中的最后一个区间的末尾端点进行比较
+            int[] peek = res.get(res.size() - 1);
+
+            if (curInterval[0] > peek[1]) {
+                res.add(curInterval);
+            } else {
+                // 注意，这里应该取最大
+                peek[1] = Math.max(curInterval[1], peek[1]);
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) {
+            return new int[0][2];
+        }
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] interval1, int[] interval2) {
+                return interval1[0] - interval2[0];
+            }
+        });
+        List<int[]> merged = new ArrayList<int[]>();
+        for (int i = 0; i < intervals.length; ++i) {
+            int L = intervals[i][0], R = intervals[i][1];
+            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < L) {
+                merged.add(new int[]{L, R});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], R);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
---
+++ Java/75.java
@@ -0,0 +1,50 @@
+//single pointer
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int ptr = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = temp;
+                ++ptr;
+            }
+        }
+        for (int i = ptr; i < n; ++i) {
+            if (nums[i] == 1) {
+                int temp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = temp;
+                ++ptr;
+            }
+        }
+    }
+}
+
+//2 pointers
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int p0 = 0, p1 = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 1) {
+                int temp = nums[i];
+                nums[i] = nums[p1];
+                nums[p1] = temp;
+                ++p1;
+            } else if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[p0];
+                nums[p0] = temp;
+                if (p0 < p1) {
+                    temp = nums[i];
+                    nums[i] = nums[p1];
+                    nums[p1] = temp;
+                }
+                ++p0;
+                ++p1;
+            }
+        }
+    }
+}
