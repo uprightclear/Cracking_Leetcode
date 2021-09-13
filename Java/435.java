a66f3cb1873376921e4f76df66ce77188341a1a3
uprightclear <uprightclear@gmail.com>
2021-09-12 21:06:15 -0700

    20210912 update

--- Java/435.java
+++ Java/435.java
@@ -1,3 +1,4 @@
+//time out
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         if (intervals.length == 0) {
@@ -21,5 +22,31 @@ class Solution {
             }
         }
         return n - Arrays.stream(f).max().getAsInt();
+    }
+}
+
+//greedy
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+        
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] interval1, int[] interval2) {
+                return interval1[1] - interval2[1];
+            }
+        });
+
+        int n = intervals.length;
+        int right = intervals[0][1];
+        int ans = 1;
+        for (int i = 1; i < n; ++i) {
+            if (intervals[i][0] >= right) {
+                ++ans;
+                right = intervals[i][1];
+            }
+        }
+        return n - ans;
     }
 }
---
+++ Java/560.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, pre = 0;
+        HashMap < Integer, Integer > mp = new HashMap < > ();
+        mp.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            pre += nums[i];
+            if (mp.containsKey(pre - k)) {
+                count += mp.get(pre - k);
+            }
+            mp.put(pre, mp.getOrDefault(pre, 0) + 1);
+        }
+        return count;
+    }
+}
