18d706a34440da3af5489398a26afdc794876b55
uprightclear <uprightclear@gmail.com>
2021-09-13 10:51:52 -0700

    20210913 update

--- Java/524.java
+++ Java/524.java
@@ -1,3 +1,4 @@
+//2 pointers
 class Solution {
     public String findLongestWord(String s, List<String> dictionary) {
         String res = "";
@@ -11,6 +12,43 @@ class Solution {
             }
             if (i == t.length()) {
                 if (t.length() > res.length() || (t.length() == res.length() && t.compareTo(res) < 0)) {
+                    res = t;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+//dp
+class Solution {
+    public String findLongestWord(String s, List<String> dictionary) {
+        int m = s.length();
+        int[][] f = new int[m + 1][26];
+        Arrays.fill(f[m], m);
+
+        for (int i = m - 1; i >= 0; --i) {
+            for (int j = 0; j < 26; ++j) {
+                if (s.charAt(i) == (char) ('a' + j)) {
+                    f[i][j] = i;
+                } else {
+                    f[i][j] = f[i + 1][j];
+                }
+            }
+        }
+        String res = "";
+        for (String t : dictionary) {
+            boolean match = true;
+            int j = 0;
+            for (int i = 0; i < t.length(); ++i) {
+                if (f[j][t.charAt(i) - 'a'] == m) {
+                    match = false;
+                    break;
+                }
+                j = f[j][t.charAt(i) - 'a'] + 1;
+            }
+            if (match) {
+                if (t.length() > res.length() ||  (t.length() == res.length() && t.compareTo(res) < 0)) {
                     res = t;
                 }
             }
