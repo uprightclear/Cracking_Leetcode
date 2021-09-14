a09ecf7e107fec1197250d4d98b5b1dc970cf6d2
uprightclear <uprightclear@gmail.com>
2021-09-14 16:42:24 -0700

    20210914 update

---
+++ Java/763.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int[] last = new int[26];
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            last[s.charAt(i) - 'a'] = i;
+        }
+        List<Integer> partition = new ArrayList<Integer>();
+        int start = 0, end = 0;
+        for (int i = 0; i < length; i++) {
+            end = Math.max(end, last[s.charAt(i) - 'a']);
+            if (i == end) {
+                partition.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return partition;
+    }
+}
