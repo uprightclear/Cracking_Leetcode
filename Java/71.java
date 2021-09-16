4c56354c5702b3193444724867f86ddad98a71fe
uprightclear <uprightclear@gmail.com>
2021-09-15 18:46:34 -0700

    20210915 update

--- Java/71.java
+++ Java/71.java
@@ -24,3 +24,32 @@ class Solution {
         return res.toString();
     }
 }
+
+class Solution {
+    public String simplifyPath(String path) {
+        // 双端队列
+        Deque<String> queue = new LinkedList<>();
+        // 分割字符
+        String[] res = path.split("/");
+        for(int i = 0; i < res.length; i++){
+            String s = res[i];
+            if(s.equals(".") || s.equals("")) continue;
+            else if (s.equals("..")){
+                if(!queue.isEmpty()){
+                    queue.pollLast();
+                }
+            }else{
+                queue.offer(s);
+            }
+        }
+        // 拼接
+        if(queue.isEmpty()) return "/";
+
+        StringBuilder sb = new StringBuilder("/");
+        while(!queue.isEmpty()){
+            sb.append("/" + queue.poll());
+        }
+        // 判空
+        return sb.toString();
+    }
+}
