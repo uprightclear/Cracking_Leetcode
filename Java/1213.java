717d1111c0ed56b0606b7d095041a08ce608b33f
uprightclear <uprightclear@gmail.com>
2021-09-16 16:43:57 -0700

    20210916 update

--- Java/1213.java
+++ Java/1213.java
@@ -1,3 +1,4 @@
+//3 pointers
 class Solution {
     public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
         List<Integer> ans = new ArrayList <>();
@@ -24,5 +25,34 @@ class Solution {
             }
         }
         return ans;
+    }
+}
+
+//brute force
+class Solution {
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        List<Integer> ans = new ArrayList<>();
+
+        // note that HashMap won't work here as it does not guarantee the key orders
+        Map<Integer, Integer> counter = new TreeMap<>();
+
+        // iterate through arr1, arr2, and arr3 to count the frequencies
+        for (Integer e: arr1) {
+            counter.put(e, counter.getOrDefault(e, 0) + 1);
+        }
+        for (Integer e: arr2) {
+            counter.put(e, counter.getOrDefault(e, 0) + 1);
+        }
+        for (Integer e: arr3) {
+            counter.put(e, counter.getOrDefault(e, 0) + 1);
+        }
+
+        for (Integer item: counter.keySet()) {
+            if (counter.get(item) == 3) {
+                ans.add(item);
+            }
+        }
+        return ans;
+
     }
 }
