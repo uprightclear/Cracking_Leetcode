21edcde6943bf52c2f17da91df5371c76a687c17
uprightclear <uprightclear@gmail.com>
2021-09-17 10:48:57 -0700

    20210917 update

--- Java/29.java
+++ Java/29.java
@@ -1,36 +1,102 @@
-public int divide(int dividend, int divisor) {
+//Time Limit Exceeded
+class Solution {
+    public int divide(int dividend, int divisor) {
 
-    // Special case: overflow.
-    if (dividend == Integer.MIN_VALUE && divisor == -1) {
-        return Integer.MAX_VALUE;
+        // Special case: overflow.
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+    
+        /* We need to convert both numbers to negatives
+         * for the reasons explained above.
+         * Also, we count the number of negatives signs. */
+        int negatives = 2;
+        if (dividend > 0) {
+            negatives--;
+            dividend = -dividend;
+        }
+        if (divisor > 0) {
+            negatives--;
+            divisor = -divisor;
+        }
+    
+        /* Count how many times the divisor has to be added
+         * to get the dividend. This is the quotient. */
+        int quotient = 0;
+        while (dividend - divisor <= 0) {
+            quotient--;
+            dividend -= divisor;
+        }
+    
+        /* If there was originally one negative sign, then
+         * the quotient remains negative. Otherwise, switch
+         * it to positive. */
+        if (negatives != 1) {
+            quotient = -quotient;
+        }
+        return quotient;
     }
+}
 
-    /* We need to convert both numbers to negatives
-     * for the reasons explained above.
-     * Also, we count the number of negatives signs. */
-    int negatives = 2;
-    if (dividend > 0) {
-        negatives--;
-        dividend = -dividend;
-    }
-    if (divisor > 0) {
-        negatives--;
-        divisor = -divisor;
-    }
+class Solution {
+    private static int HALF_INT_MIN = -1073741824;
 
-    /* Count how many times the divisor has to be added
-     * to get the dividend. This is the quotient. */
-    int quotient = 0;
-    while (dividend - divisor <= 0) {
-        quotient--;
-        dividend -= divisor;
-    }
+    public int divide(int dividend, int divisor) {
 
-    /* If there was originally one negative sign, then
-     * the quotient remains negative. Otherwise, switch
-     * it to positive. */
-    if (negatives != 1) {
-        quotient = -quotient;
+        // Special cases: overflow.
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        if (dividend == Integer.MIN_VALUE && divisor == 1) {
+            return Integer.MIN_VALUE;
+        }
+
+        /* We need to convert both numbers to negatives.
+         * Also, we count the number of negatives signs. */
+        int negatives = 2;
+        if (dividend > 0) {
+            negatives--;
+            dividend = -dividend;
+        }
+        if (divisor > 0) {
+            negatives--;
+            divisor = -divisor;
+        }
+
+        /* We want to find the largest doubling of the divisor in the negative 32-bit
+         * integer range that could fit into the dividend.
+         * Note if it would cause an overflow by being less than HALF_INT_MIN,
+         * then we just stop as we know double it would not fit into INT_MIN anyway. */
+        int maxBit = 0;
+        while (divisor >= HALF_INT_MIN && divisor + divisor >= dividend) {
+            maxBit += 1;
+            divisor += divisor;
+        }
+
+        int quotient = 0;
+        /* We start from the biggest bit and shift our divisor to the right
+         * until we can't shift it any further */
+        for (int bit = maxBit; bit >= 0; bit--) {
+            /* If the divisor fits into the dividend, then we should set the current
+             * bit to 1. We can do this by subtracting a 1 shifted by the appropriate
+             * number of bits. */
+            if (divisor >= dividend) {
+                quotient -= (1 << bit);
+                /* Remove the current divisor from the dividend, as we've now
+                 * considered this part. */
+                dividend -= divisor;
+            }
+            /* Shift the divisor to the right so that it's in the right place
+             * for the next positon we're checking at. */
+            divisor = (divisor + 1) >> 1;
+        }
+
+        /* If there was originally one negative sign, then
+         * the quotient remains negative. Otherwise, switch
+         * it to positive. */
+        if (negatives != 1) {
+            quotient = -quotient;
+        }
+        return quotient;
     }
-    return quotient;
 }
