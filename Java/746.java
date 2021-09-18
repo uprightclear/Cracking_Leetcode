91574b1ac28ee4f784842882f9fd404a8dc0b157
uprightclear <uprightclear@gmail.com>
2021-09-17 17:48:55 -0700

    20210917 update

--- Java/746.java
+++ Java/746.java
@@ -4,10 +4,10 @@ class Solution {
         int[] dp = new int[len + 1];
         dp[0] = 0;
         dp[1] = 0;
-        for(int i = 2; i <= len + 1; i++){
+        for(int i = 2; i <= len; i++){
             dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
         }
-        return dp[len + 1];
+        return dp[len];
     }
 }
 
