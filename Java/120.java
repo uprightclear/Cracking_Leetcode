109d61ed0c445f7729e71ae1d7b820b0e31bf7d8
uprightclear <uprightclear@gmail.com>
2021-09-21 20:27:46 -0700

    20210921 update

--- Java/120.java
+++ Java/120.java
@@ -38,3 +38,18 @@ class Solution {
         return minTotal;
     }
 }
+
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int len = triangle.size();
+        
+        for(int i = len - 2; i >= 0; i--) {
+            for(int j = 0; j < i + 1; j++) {
+                triangle.get(i).set(j, triangle.get(i).get(j) + Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1)));
+            }
+        }
+        
+        return triangle.get(0).get(0);
+    }
+}
