efaf69a90b65a440fb7251106a74b955b52b9b4c
uprightclear <uprightclear@gmail.com>
2021-09-23 21:05:54 -0700

    20210923 update

--- Java/253.java
+++ Java/253.java
@@ -17,13 +17,11 @@ class Solution {
             });
 
     // Sort the intervals by start time
-    Arrays.sort(
-        intervals,
-        new Comparator<int[]>() {
-          public int compare(final int[] a, final int[] b) {
-            return a[0] - b[0];
-          }
-        });
+    Arrays.sort(intervals, new Comparator<int[]>() {
+      public int compare(final int[] a, final int[] b) {
+        return a[0] - b[0];
+      }
+    });
 
     // Add the first meeting
     allocator.add(intervals[0][1]);
