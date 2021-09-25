8abe8d8a1b7b1d82468cee9cc7862d08669bfa74
uprightclear <uprightclear@gmail.com>
2021-09-25 13:03:34 -0700

    20210925 update

---
+++ Java/371.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int answer = a ^ b;
+            int carry = (a & b) << 1;
+            a = answer;
+            b = carry;
+        }
+        
+        return a;
+    }
+}
