88902c651c86ecdf4e9112d9038235e6582988a9
uprightclear <uprightclear@gmail.com>
2021-09-28 11:07:14 -0700

    20210928 update

---
+++ Java/517.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMinMoves(int[] machines) {
+        int tot = Arrays.stream(machines).sum();
+        int n = machines.length;
+        if (tot % n != 0) {
+            return -1;
+        }
+        int avg = tot / n;
+        int ans = 0, sum = 0;
+        for (int num : machines) {
+            num -= avg;
+            sum += num;
+            ans = Math.max(ans, Math.max(Math.abs(sum), num));
+        }
+        return ans;
+    }
+}
