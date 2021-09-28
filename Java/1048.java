852f47e9ab97c7381fcd5eda6de9fc97141b2d24
uprightclear <uprightclear@gmail.com>
2021-09-28 11:36:40 -0700

    20210928 update

---
+++ Java/1048.java
@@ -0,0 +1,39 @@
+
+class Solution {
+
+    private int dfs(Set<String> words, Map<String, Integer> memo, String currentWord) {
+        // If the word is encountered previously we just return its value present in the map (memoization).
+        if (memo.containsKey(currentWord)) {
+            return memo.get(currentWord);
+        }
+        // This stores the maximum length of word sequence possible with the 'currentWord' as the
+        int maxLength = 1;
+        StringBuilder sb = new StringBuilder(currentWord);
+
+        // creating all possible strings taking out one character at a time from the `currentWord`
+        for (int i = 0; i < currentWord.length(); i++) {
+            sb.deleteCharAt(i);
+            String newWord = sb.toString();
+            // If the new word formed is present in the list, we do a dfs search with this newWord.
+            if (words.contains(newWord)) {
+                int currentLength = 1 + dfs(words, memo, newWord);
+                maxLength = Math.max(maxLength, currentLength);
+            }
+            sb.insert(i, currentWord.charAt(i));
+        }
+        memo.put(currentWord, maxLength);
+
+        return maxLength;
+    }
+
+    public int longestStrChain(String[] words) {
+        Map<String, Integer> memo = new HashMap<>();
+        Set<String> wordsPresent = new HashSet<>();
+        Collections.addAll(wordsPresent, words);
+        int ans = 0;
+        for (String word : words) {
+            ans = Math.max(ans, dfs(wordsPresent, memo, word));
+        }
+        return ans;
+    }
+}
