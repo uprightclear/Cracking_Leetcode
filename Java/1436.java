b88bc393db0f34541a00fecc3e0504a43663af61
uprightclear <uprightclear@gmail.com>
2021-09-30 12:33:39 -0700

    20210930 update

---
+++ Java/1436.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Set<String> citiesA = new HashSet<String>();
+        for (List<String> path : paths) {
+            citiesA.add(path.get(0));
+        }
+        for (List<String> path : paths) {
+            if (!citiesA.contains(path.get(1))) {
+                return path.get(1);
+            }
+        }
+        return "";
+    }
+}
