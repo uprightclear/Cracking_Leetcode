bb023661bf279008af49d981937b2f3bcc525622
uprightclear <uprightclear@gmail.com>
2021-10-01 09:44:50 -0700

    20211001 update

---
+++ Java/405.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String toHex(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i = 7; i >= 0; i --) {
+            int val = (num >> (4 * i)) & 0xf;
+            if (sb.length() > 0 || val > 0) {
+                char digit = val < 10 ? (char) ('0' + val) : (char) ('a' + val - 10);
+                sb.append(digit);
+            }
+        }
+        return sb.toString();
+    }
+}
