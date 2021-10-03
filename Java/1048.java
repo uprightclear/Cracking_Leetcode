c8cd3ecccd86188254d1164afedb9f1c3fa9a64e
uprightclear <uprightclear@gmail.com>
2021-10-03 16:56:43 -0700

    20211003 update

--- Java/1048.java
+++ Java/1048.java
@@ -1,4 +1,3 @@
-
 class Solution {
 
     private int dfs(Set<String> words, Map<String, Integer> memo, String currentWord) {
@@ -35,5 +34,34 @@ class Solution {
             ans = Math.max(ans, dfs(wordsPresent, memo, word));
         }
         return ans;
+    }
+}
+
+
+
+
+class Solution {
+    public int longestStrChain(String[] words) {
+        Map<String, Integer> dp = new HashMap<>();
+
+        // Sorting the list in terms of the word length.
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+
+        int longestWordSequenceLength = 1;
+
+        for (String word : words) {
+            int presentLength = 1;
+            // Find all possible predecessors for the current word by removing one letter at a time.
+            for (int i = 0; i < word.length(); i++) {l
+                StringBuilder temp = new StringBuilder(word);
+                temp.deleteCharAt(i);
+                String predecessor = temp.toString();
+                int previousLength = dp.getOrDefault(predecessor, 0);
+                presentLength = Math.max(presentLength, previousLength + 1);
+            }
+            dp.put(word, presentLength);
+            longestWordSequenceLength = Math.max(longestWordSequenceLength, presentLength);
+        }
+        return longestWordSequenceLength;
     }
 }
