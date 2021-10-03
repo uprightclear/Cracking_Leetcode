a44a7ebb6073f7c3e20b21d7c460e0f0abe07401
uprightclear <uprightclear@gmail.com>
2021-10-03 00:26:15 -0700

    20211002 update

--- Java/696.java
+++ Java/696.java
@@ -19,3 +19,19 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int ans = 0, prev = 0, cur = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i-1) != s.charAt(i)) {
+                ans += Math.min(prev, cur);
+                prev = cur;
+                cur = 1;
+            } else {
+                cur++;
+            }
+        }
+        return ans + Math.min(prev, cur);
+    }
+}
