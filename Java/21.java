3f073e03ae2770bfcbcb0f0f64a583d6b04b29ed
uprightclear <uprightclear@gmail.com>
2021-10-05 12:02:42 -0700

    20211005 update

--- Java/21.java
+++ Java/21.java
@@ -1,3 +1,6 @@
+//Recursion
+//T: O(m + n)
+//S: O(m + n)
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) {
@@ -11,5 +14,33 @@ class Solution {
             l2.next = mergeTwoLists(l1, l2.next);
             return l2;
         }
+    }
+}
+
+//Iteration
+//T: O(m + n)
+//S: O(1)
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // maintain an unchanging reference to node ahead of the return node.
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // At least one of l1 and l2 can still have nodes at this point, so connect
+        // the non-null list to the end of the merged list.
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
     }
 }
