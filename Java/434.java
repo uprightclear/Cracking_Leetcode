d17df6bdcb7f0ca0ade44d2d0e09bd97561b51a7
uprightclear <uprightclear@gmail.com>
2021-10-06 10:17:47 -0700

    20211006 update

---
+++ Java/434.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countSegments(String s) {
+        int res = 0;
+        for (String t : s.split(" ")) {
+            if (!"".equals(t)) {
+                ++res;
+            }
+        }
+        return res;
+    }
+}
+
+
+class Solution {
+    public int countSegments(String s) {
+        int segmentCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i - 1) == ' ') && s.charAt(i) != ' ') {
+                segmentCount++;
+            }
+        }
+
+        return segmentCount;
+    }
+}
