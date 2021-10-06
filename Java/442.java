9e27a335344583c1dd7b1d61c1585f3ad8c05604
uprightclear <uprightclear@gmail.com>
2021-10-06 11:30:10 -0700

    20211006 update

---
+++ Java/442.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        int len = nums.length;
+        if (len == 0) {
+            return res;
+        }
+        for (int i = 0; i < len; i++) {
+            while (nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            if (nums[i] - 1 != i) {
+                res.add(nums[i]);
+            }
+        }
+        return res;
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        if (index1 == index2) {
+            return;
+        }
+        nums[index1] = nums[index1] ^ nums[index2];
+        nums[index2] = nums[index1] ^ nums[index2];
+        nums[index1] = nums[index1] ^ nums[index2];
+    }
+}
