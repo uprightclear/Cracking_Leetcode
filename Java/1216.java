79e2bdeb2b7bb4c7284962776d889fbaf3627249
uprightclear <uprightclear@gmail.com>
2021-10-08 14:59:28 -0700

    20211008 update

---
+++ Java/1216.java
@@ -0,0 +1,107 @@
+class Solution {
+    Integer memo[][];
+    int isValidPalindrome(String s, int i, int j) {
+
+        // Base case, only 1 letter remaining.
+        if (i == j)
+            return 0;
+
+        // Base case 2, only 2 letters remaining.
+        if (i == j - 1)
+            return s.charAt(i) != s.charAt(j) ? 1 : 0;
+
+        //Return the precomputed value if exists.
+        if (memo[i][j] != null)
+            return memo[i][j];
+
+        // Case 1: Character at `i` equals character at `j`
+        if (s.charAt(i) == s.charAt(j))
+            return memo[i][j] = isValidPalindrome(s, i + 1, j - 1);
+
+        // Case 2: Character at `i` does not equal character at `j`.
+        // Either delete character at `i` or delete character at `j`
+        // and try to match the two pointers using recursion.
+        // We need to take the minimum of the two results and add 1
+        // representing the cost of deletion.
+        return memo[i][j] = 1 + Math.min(isValidPalindrome(s, i + 1, j), isValidPalindrome(s, i, j - 1));
+    }
+
+    public boolean isValidPalindrome(String s, int k) {
+        memo = new Integer[s.length()][s.length()];
+
+        // Return true if the minimum cost to create a palindrome by only deleting the letters
+        // is less than or equal to `k`.
+        return isValidPalindrome(s, 0, s.length() - 1) <= k;
+    }
+}
+
+
+class Solution {
+    public boolean isValidPalindrome(String s, int k) {
+        int memo[][] = new int[s.length()][s.length()];
+
+        // Generate all combinations of `i` and `j` in the correct order.
+        for (int i = s.length() - 2; i >= 0; i--)
+            for (int j = i + 1; j < s.length(); j++) {
+                // Case 1: Character at `i` equals character at `j`
+                if (s.charAt(i) == s.charAt(j))
+                    memo[i][j] = memo[i + 1][j - 1];
+
+                // Case 2: Character at `i` does not equal character at `j`.
+                // Either delete character at `i` or delete character at `j`
+                // and try to match the two pointers using recursion.
+                // We need to take the minimum of the two results and add 1
+                // representing the cost of deletion.
+                else
+                    memo[i][j] = 1 + Math.min(memo[i + 1][j], memo[i][j - 1]);
+            }
+
+        // Return true if the minimum cost to create a palindrome by only deleting the letters
+        // is less than or equal to `k`.
+        return memo[0][s.length() - 1] <= k;
+    }
+}
+
+
+class Solution {
+    public boolean isValidPalindrome(String s, int k) {
+        int memo[] = new int[s.length()];
+
+        // To store the previous required values from memo.
+        int temp, prev;
+
+        // Generate all combinations of `i` and `j` in the correct order.
+        for (int i = s.length() - 2; i >= 0; i--) {
+            // 'prev' stores the value at memo[i+1][j-1];
+            prev = 0;
+            for (int j = i + 1; j < s.length(); j++) {
+                // Store the value of memo[i+1][j] temporarily.
+                temp = memo[j];
+
+                // Case 1: Character at `i` equals character at `j`
+                if (s.charAt(i) == s.charAt(j))
+                    memo[j] = prev;
+
+                // Case 2: Character at `i` does not equal character at `j`.
+                // Either delete character at `i` or delete character at `j`
+                // and try to match the two pointers using recursion.
+                // We need to take the minimum of the two results and add 1
+                // representing the cost of deletion.
+                else
+
+                    // memo[j] will contain the value for memo[i+1][j]
+                    // memo[j-1] will contain the value for memo[i][j-1]
+                    memo[j] = 1 + Math.min(memo[j], memo[j - 1]);
+
+                // Copy the value of memo[i+1][j] to `prev`
+                // For the next iteration when j=j+1
+                // `prev` will hold the value memo[i+1][j-1];
+                prev = temp;
+            }
+        }
+
+        // Return true if the minimum cost to create a palindrome by only deleting the letters
+        // is less than or equal to `k`.
+        return memo[s.length() - 1] <= k;
+    }
+}
