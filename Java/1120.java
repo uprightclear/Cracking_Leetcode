8a5060d7be1a3811bfaface29b424e2cad2540cf
uprightclear <uprightclear@gmail.com>
2021-10-08 22:24:24 -0700

    20211008 update

--- Java/1120.java
+++ Java/1120.java
@@ -1,43 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    // for each node in the tree, we will maintain three values
-    class State {
-        // count of nodes in the subtree
-        int nodeCount;
-
-        // sum of values in the subtree
-        int valueSum;
-
-        // max average found in the subtree
-        double maxAverage;
-
-        State(int nodes, int sum, double maxAverage) {
-            this.nodeCount = nodes;
-            this.valueSum = sum;
-            this.maxAverage = maxAverage;
-        }
-    }
-
+    double res = Integer.MIN_VALUE;
     public double maximumAverageSubtree(TreeNode root) {
-        return maxAverage(root).maxAverage;
+        dfs(root);
+        return res;
     }
-
-    State maxAverage(TreeNode root) {
-        if (root == null) {
-            return new State(0, 0, 0);
+    
+    public int[] dfs(TreeNode root) {
+        if(root == null) {
+            return new int[]{0, 0};
         }
-
-        // postorder traversal, solve for both child nodes first.
-        State left = maxAverage(root.left);
-        State right = maxAverage(root.right);
-
-        // now find nodeCount, valueSum and maxAverage for current node `root`
-        int nodeCount = left.nodeCount + right.nodeCount + 1;
-        int sum = left.valueSum + right.valueSum + root.val;
-        double maxAverage = Math.max(
-                (1.0 * (sum)) / nodeCount, // average for current node
-                Math.max(right.maxAverage, left.maxAverage) // max average from child nodes
-        );
-
-        return new State(nodeCount, sum, maxAverage);
+        int[] l = dfs(root.left);
+        int[] r = dfs(root.right);
+        
+        int sum = l[0] + r[0] + root.val;
+        int count = l[1] + r[1] + 1;
+        res = Math.max(res, (sum * 1.0) / count);
+        return new int[]{sum, count};
     }
 }
