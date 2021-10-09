252383091424730c15730b3e286ceae2e5c02973
uprightclear <uprightclear@gmail.com>
2021-10-09 10:55:35 -0700

    20211009 update

---
+++ Java/441.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = (right - left + 1) / 2 + left;
+            if ((long) mid * (mid + 1) <= (long) 2 * n) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
