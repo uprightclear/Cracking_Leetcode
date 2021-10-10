de10a0648c4c318fc39a97026a16d0296c6d7f9b
uprightclear <uprightclear@gmail.com>
2021-10-10 15:55:07 -0700

    20211010 update

---
+++ Java/370.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] getModifiedArray(int length, int[][] updates) {
+        int[] ans = new int[length];
+        for(int[] update : updates) {
+            for(int i = update[0]; i <= update[1]; i++) {
+                ans[i] += update[2];
+            }
+        }
+        return ans;
+    }
+}
+
+
+class Solution {
+    public int[] getModifiedArray(int length, int[][] updates) {
+
+       int[] res = new int[length];
+        for(int[] update : updates) {
+           int value = update[2];
+           int start = update[0];
+           int end = update[1];
+
+           res[start] += value;
+
+           if(end < length - 1)
+               res[end + 1] -= value;
+
+       }
+
+       int sum = 0;
+       for(int i = 0; i < length; i++) {
+           sum += res[i];
+           res[i] = sum;
+       }
+
+       return res;
+   }
+}
