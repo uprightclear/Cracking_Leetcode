38f696074b77f1c90512d1c9a11ba0a58f1d2b44
uprightclear <uprightclear@gmail.com>
2021-10-10 10:33:40 -0700

    20211010 update

---
+++ Java/273.java
@@ -0,0 +1,40 @@
+class Solution {
+    String[] singles = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    String[] teens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    String[] tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    String[] thousands = {"", "Thousand", "Million", "Billion"};
+
+    public String numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i = 3, unit = 1000000000; i >= 0; i--, unit /= 1000) {
+            int curNum = num / unit;
+            if (curNum != 0) {
+                num -= curNum * unit;
+                StringBuffer curr = new StringBuffer();
+                recursion(curr, curNum);
+                curr.append(thousands[i]).append(" ");
+                sb.append(curr);
+            }
+        }
+        return sb.toString().trim();
+    }
+
+    public void recursion(StringBuffer curr, int num) {
+        if (num == 0) {
+            return;
+        } else if (num < 10) {
+            curr.append(singles[num]).append(" ");
+        } else if (num < 20) {
+            curr.append(teens[num - 10]).append(" ");
+        } else if (num < 100) {
+            curr.append(tens[num / 10]).append(" ");
+            recursion(curr, num % 10);
+        } else {
+            curr.append(singles[num / 100]).append(" Hundred ");
+            recursion(curr, num % 100);
+        }
+    }
+}
