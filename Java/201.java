a7fdac30096986812a10953ccc43c2da194ec675
uprightclear <uprightclear@gmail.com>
2021-10-09 23:46:51 -0700

    20211009 update

--- Java/201.java
+++ Java/201.java
@@ -10,3 +10,14 @@ class Solution {
         return m << shift;
     }
 }
+
+//Brian Kernighan
+class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        while (m < n) {
+            // 抹去最右边的 1
+            n = n & (n - 1);
+        }
+        return n;
+    }
+}
