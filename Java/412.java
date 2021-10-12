e55bf11f8ce22e17925510812c5952e6d8d0e01a
uprightclear <uprightclear@gmail.com>
2021-10-12 14:53:35 -0700

    20211012 update

---
+++ Java/412.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            StringBuffer sb = new StringBuffer();
+            if (i % 3 == 0) {
+                sb.append("Fizz");
+            }
+            if (i % 5 == 0) {
+                sb.append("Buzz");
+            }
+            if (sb.length() == 0) {
+                sb.append(i);
+            }
+            answer.add(sb.toString());
+        }
+        return answer;
+    }
+}
