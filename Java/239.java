816c2126246a3754333c243d88f9805e28b25de8
uprightclear <uprightclear@gmail.com>
2021-10-14 16:04:10 -0700

    20211014 update

--- Java/239.java
+++ Java/239.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if(k == 0) return nums;
-
-        int[] res = new int[nums.length - k + 1];
+        int[] ans = new int[nums.length - k + 1];
         Deque<Integer> indices = new LinkedList<>();
-
-        for(int i = 0; i < nums.length; i++){
+        
+        for(int i = 0; i < nums.length; i++) {
+            //make sure the first element in indices is the biggest one's index in the current window
             while(indices.size() > 0 && nums[i] >= nums[indices.getLast()]) indices.removeLast();
             indices.addLast(i);
-            if(i - k + 1 >= 0) res[i - k + 1] = nums[indices.getFirst()];
+            if(i - k + 1 >= 0) ans[i - k + 1] = nums[indices.getFirst()];
             if(i - k + 1 >= indices.getFirst()) indices.removeFirst();
         }
-        return res;
+        return ans;
     }
 }
