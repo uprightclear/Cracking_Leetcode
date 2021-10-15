d0a3e384d83b4ec8e95eedbe7daa79aa62f83c91
uprightclear <uprightclear@gmail.com>
2021-10-15 13:43:58 -0700

    20211015 update

---
+++ Java/302.java
@@ -0,0 +1,44 @@
+public class Solution {
+    public int minArea(char[][] image, int x, int y) {
+        int top = x, bottom = x;
+        int left = y, right = y;
+        for (x = 0; x < image.length; ++x) {
+            for (y = 0; y < image[0].length; ++y) {
+                if (image[x][y] == '1') {
+                    top = Math.min(top, x);
+                    bottom = Math.max(bottom, x + 1);
+                    left = Math.min(left, y);
+                    right = Math.max(right, y + 1);
+                }
+            }
+        }
+        return (right - left) * (bottom - top);
+    }
+}
+
+
+//DFS
+public class Solution {
+    private int top, bottom, left, right;
+    public int minArea(char[][] image, int x, int y) {
+        if(image.length == 0 || image[0].length == 0) return 0;
+        top = bottom = x;
+        left = right = y;
+        dfs(image, x, y);
+        return (right - left) * (bottom - top);
+    }
+    private void dfs(char[][] image, int x, int y){
+        if(x < 0 || y < 0 || x >= image.length || y >= image[0].length ||
+          image[x][y] == '0')
+            return;
+        image[x][y] = '0'; // mark visited black pixel as white
+        top = Math.min(top, x);
+        bottom = Math.max(bottom, x + 1);
+        left = Math.min(left, y);
+        right = Math.max(right, y + 1);
+        dfs(image, x + 1, y);//down
+        dfs(image, x - 1, y);//up
+        dfs(image, x, y - 1);//left
+        dfs(image, x, y + 1);//right
+    }
+}
