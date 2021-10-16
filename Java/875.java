60a9ee161e145702f90131935c5e70de948ada29
uprightclear <uprightclear@gmail.com>
2021-10-16 15:00:27 -0700

    20211016 update

---
+++ Java/875.java
@@ -0,0 +1,48 @@
+//Binary Search
+public class Solution {
+
+    public int minEatingSpeed(int[] piles, int H) {
+        int maxVal = 1;
+        for (int pile : piles) {
+            maxVal = Math.max(maxVal, pile);
+        }
+
+        // 速度最小的时候，耗时最长
+        int left = 1;
+        // 速度最大的时候，耗时最短
+        int right = maxVal;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (calculateSum(piles, mid) > H) {
+                // 耗时太多，说明速度太慢了，下一轮搜索区间是 [mid + 1..right]
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    /**
+     * 如果返回的小时数严格大于 H，就不符合题意
+     *
+     * @param piles
+     * @param speed
+     * @return 需要的小时数
+     */
+    private int calculateSum(int[] piles, int speed) {
+        int sum = 0;
+        for (int pile : piles) {
+            // 上取整可以这样写
+            sum += (pile + speed - 1) / speed;
+        }
+        return sum;
+    }
+}
+
+// 作者：liweiwei1419
+// 链接：https://leetcode-cn.com/problems/koko-eating-bananas/solution/er-fen-cha-zhao-ding-wei-su-du-by-liweiwei1419/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
