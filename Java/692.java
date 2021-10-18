418a52bf14ff3b352d8ea846bd8fdab0ee034ce2
uprightclear <uprightclear@gmail.com>
2021-10-18 13:07:36 -0700

    20211018 update

--- Java/692.java
+++ Java/692.java
@@ -47,3 +47,34 @@ class Solution {
         return ret;
     }
 }
+
+
+//bucket sort
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> count = new HashMap<>();
+        for (String w : words) {
+            count.put(w, count.getOrDefault(w, 0) + 1);
+        }
+        
+        List<String>[] buckets = new ArrayList[words.length];
+        for (int i = 0; i < words.length; i++) buckets[i] = new ArrayList<>();
+
+        for (String w : count.keySet()) {
+            buckets[count.get(w)].add(w);
+        }
+        
+        List<String> ans = new ArrayList<>();
+        for (int i = words.length - 1; i >= 0; i--) {
+            if (buckets[i].size() == 0) continue;
+            Collections.sort(buckets[i]);
+            for (int j = 0; j < buckets[i].size(); j++) {
+                ans.add(buckets[i].get(j));
+                if (ans.size() == k) break;
+            }
+            if (ans.size() == k) break;
+        }
+        
+        return ans;
+    }
+}
