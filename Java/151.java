f7dd5877a695a3e4e98b83e1f68cd369cc2aa34c
uprightclear <uprightclear@gmail.com>
2021-10-20 10:34:22 -0700

    20211020 update

--- Java/151.java
+++ Java/151.java
@@ -9,6 +9,20 @@ class Solution {
     }
 }
 
+class Solution {
+    public String reverseWords(String s) {
+        s = s.trim();
+        int j = s.length() - 1, i = j;
+        StringBuilder res = new StringBuilder();
+        while(i >= 0){
+            while(i >= 0 && s.charAt(i) != ' ') i--;
+            res.append(s.substring(i + 1, j + 1) + " ");
+            while(i >= 0 && s.charAt(i) == ' ') i--;
+            j = i;
+        }
+        return res.toString().trim();
+    }
+}
 
 class Solution {
     public StringBuilder trimSpaces(String s) {
