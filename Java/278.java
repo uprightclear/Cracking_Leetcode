8867dcb3cc863df30a8509c7a5480e7130dc6f07
uprightclear <uprightclear@gmail.com>
2021-10-21 14:22:15 -0700

    20211021 update

---
+++ Java/278.java
@@ -0,0 +1,17 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+      public class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            int l = 1, r = n;
+            while(l < r) {
+                int mid = l + (r - l) / 2;
+                if(isBadVersion(mid) == true) {
+                    r = mid;
+                }else {
+                    l = mid + 1;
+                }
+            }
+            return l;
+        }
+    }
