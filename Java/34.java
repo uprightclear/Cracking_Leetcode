b5a1d375a3507b86fb3da590d6c350100bc7d512
uprightclear <uprightclear@gmail.com>
2021-10-21 11:55:39 -0700

    20211021 update

--- Java/34.java
+++ Java/34.java
@@ -1,32 +1,25 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        int index = binarySearch(nums, target);
+        if(index == -1) return new int[]{-1, -1};
+        int low = index - 1, high = index + 1;
+        while(low >= 0 &&nums[low] == target) low--;
+        while(high < nums.length && nums[high] == target) high++;
+        return new int[]{low + 1, high - 1};
+    }
 
-        int l = 0, r = nums.length - 1;
-        int start = -2,end = 0;
-        while(l <= r) {
-            //获取中间下标
-            int mid = (l + r) / 2;
-            //若中间的数比target小，从右边找
-            if(nums[mid] < target) {
-                l = mid + 1;
-            } else if(nums[mid] > target) {//否则，从左边开始找
-                r = mid - 1;
-            } else {
-                //令start和end都等于mid
-                start = mid;
-                end = mid;
-                //只要start和end满足边界条件，并且等于target值，都可以进行循环
-                while(start >= 0 && nums[start] == target)
-                    start --;
-                while(end < nums.length && nums[end] == target)
-                    end ++;
-                break;
-            }
+    public int binarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
         }
-        //由于循环出来之后nums[start] != target nums[end] != target，所以需要start+1，end-1
-        return new int[]{start + 1, end - 1};
-    } 
+        return -1;
+    }
 }
+
 
 class Solution {
     public int[] searchRange(int[] nums, int target) {
@@ -60,27 +53,5 @@ class Solution {
             ans[1] = l;
             return ans;
         }
-    }
-}
-
-class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int index = binarySearch(nums, target);
-        if(index == -1) return new int[]{-1, -1};
-        int low = index - 1, high = index + 1;
-        while(low >= 0 &&nums[low] == target) low--;
-        while(high < nums.length && nums[high] == target) high++;
-        return new int[]{low + 1, high - 1};
-    }
-
-    public int binarySearch(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while(left <= right) {
-            int mid = (left + right) / 2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
-        }
-        return -1;
     }
 }
