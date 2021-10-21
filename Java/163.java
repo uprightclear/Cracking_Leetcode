f70f73225672d48ccb1dc90ae739bac5c2364781
uprightclear <uprightclear@gmail.com>
2021-10-21 11:34:14 -0700

    20211021 update

---
+++ Java/163.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        List<String> result = new ArrayList<>();
+        int prev = lower - 1;
+        for (int i = 0; i <= nums.length; i++) {
+            int curr = (i < nums.length) ? nums[i] : upper + 1;
+            if (prev + 1 <= curr - 1) {
+                result.add(formatRange(prev + 1, curr - 1));
+            }
+            prev = curr;
+        }
+        return result;
+    }
+
+    // formats range in the requested format
+    private String formatRange(int lower, int upper) {
+        if (lower == upper) {
+            return String.valueOf(lower);
+        }
+        return lower + "->" + upper;
+    }
+}
