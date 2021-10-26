4db26e3d5d3a7272c9dbca564c8ec0bd609276e2
uprightclear <uprightclear@gmail.com>
2021-10-25 20:01:53 -0700

    20211025 update

---
+++ Java/9.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        int cur = 0;
+        int num = x;
+        while(num != 0) {
+            cur = cur * 10 + num % 10;
+            num /= 10;
+        }
+        return cur == x;
+    }
+}
