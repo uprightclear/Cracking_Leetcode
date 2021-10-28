9fb94ec75782b6a54dbc16f5dd4f973676eb6c3f
uprightclear <uprightclear@gmail.com>
2021-10-28 10:58:55 -0700

    20211028 update

---
+++ Java/335.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isSelfCrossing(int[] distance) {
+        int n = distance.length;
+        for (int i = 3; i < n; ++i) {
+            // 第 1 类路径交叉的情况
+            if (distance[i] >= distance[i - 2] && distance[i - 1] <= distance[i - 3]) {
+                return true;
+            }
+
+            // 第 2 类路径交叉的情况
+            if (i == 4 && (distance[3] == distance[1]
+                && distance[4] >= distance[2] - distance[0])) {
+                return true;
+            }
+
+            // 第 3 类路径交叉的情况
+            if (i >= 5 && (distance[i - 3] - distance[i - 5] <= distance[i - 1]
+                && distance[i - 1] <= distance[i - 3]
+                && distance[i] >= distance[i - 2] - distance[i - 4]
+                && distance[i - 2] > distance[i - 4])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+//https://leetcode-cn.com/problems/self-crossing/solution/lu-jing-jiao-cha-by-leetcode-solution-dekx/
