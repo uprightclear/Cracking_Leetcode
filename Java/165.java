609923942ef02affb96f9bbce10e90b2f854656b
uprightclear <uprightclear@gmail.com>
2021-11-03 16:15:36 -0700

    20211103 update

--- Java/165.java
+++ Java/165.java
@@ -20,3 +20,19 @@ class Solution {
         return 0;
     }
 }
+
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        
+        int len = Math.max(v1.length, v2.length);
+        for(int i = 0; i < len; i++) {
+            int a = i < v1.length ? Integer.valueOf(v1[i]) : 0;
+            int b = i < v2.length ? Integer.valueOf(v2[i]) : 0;
+            if(a > b) return 1;
+            if(a < b) return -1;
+        }
+        return 0;
+    }
+}
