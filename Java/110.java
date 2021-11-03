fd3a7927a11153d3bbe3baf90666382f450a7a80
uprightclear <uprightclear@gmail.com>
2021-11-02 17:17:17 -0700

    20211102 update

---
+++ Java/110.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return Math.max(height(root.left), height(root.right)) + 1;
+        }
+    }
+}
