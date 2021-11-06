97c27095f72133b78f3d23594fa53f91afabe520
uprightclear <uprightclear@gmail.com>
2021-11-06 16:25:27 -0700

    20211106 update

---
+++ Java/598.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxCount(int m, int n, int[][] ops) {
+        int mina = m, minb = n;
+        for (int[] op : ops) {
+            mina = Math.min(mina, op[0]);
+            minb = Math.min(minb, op[1]);
+        }
+        return mina * minb;
+    }
+}
