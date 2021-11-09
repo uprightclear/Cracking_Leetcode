011d4f364ced0771f96426bd4b13010277abd2cd
uprightclear <uprightclear@gmail.com>
2021-11-09 10:27:36 -0800

    20211109 update

--- Java/495.java
+++ Java/495.java
@@ -12,3 +12,20 @@ class Solution {
         return ans;
     }
 }
+
+
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int ans = 0;
+        int expired = 0;
+        for (int i = 0; i < timeSeries.length; ++i) {
+            if (timeSeries[i] >= expired) {
+                ans += duration;
+            } else {
+                ans += timeSeries[i] + duration - expired;
+            }
+            expired = timeSeries[i] + duration;
+        }
+        return ans;
+    }
+}
