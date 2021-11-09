68ceed732073ff367f08dbeb8c5ea784027c8c05
uprightclear <uprightclear@gmail.com>
2021-11-09 10:03:48 -0800

    20211109 update

---
+++ Java/1178.java
@@ -0,0 +1,52 @@
+class Solution {
+    public List<Integer> findNumOfValidWords(String[] words, String[] puzzles) {
+        Map<Integer, Integer> frequency = new HashMap<Integer, Integer>();
+
+        for (String word : words) {
+            int mask = 0;
+            for (int i = 0; i < word.length(); ++i) {
+                char ch = word.charAt(i);
+                mask |= (1 << (ch - 'a'));
+            }
+            if (Integer.bitCount(mask) <= 7) {
+                frequency.put(mask, frequency.getOrDefault(mask, 0) + 1);
+            }
+        }
+
+        List<Integer> ans = new ArrayList<Integer>();
+        for (String puzzle : puzzles) {
+            int total = 0;
+
+            // 枚举子集方法一
+            // for (int choose = 0; choose < (1 << 6); ++choose) {
+            //     int mask = 0;
+            //     for (int i = 0; i < 6; ++i) {
+            //         if ((choose & (1 << i)) != 0) {
+            //             mask |= (1 << (puzzle.charAt(i + 1) - 'a'));
+            //         }
+            //     }
+            //     mask |= (1 << (puzzle.charAt(0) - 'a'));
+            //     if (frequency.containsKey(mask)) {
+            //         total += frequency.get(mask);
+            //     }
+            // }
+
+            // 枚举子集方法二
+            int mask = 0;
+            for (int i = 1; i < 7; ++i) {
+                mask |= (1 << (puzzle.charAt(i) - 'a'));
+            }
+            int subset = mask;
+            do {
+                int s = subset | (1 << (puzzle.charAt(0) - 'a'));
+                if (frequency.containsKey(s)) {
+                    total += frequency.get(s);
+                }
+                subset = (subset - 1) & mask;
+            } while (subset != mask);
+            
+            ans.add(total);
+        }
+        return ans;
+    }
+}
