0c03c1a2832badf49465542b9e1996b4ef27c0a5
uprightclear <uprightclear@gmail.com>
2021-11-09 15:32:19 -0800

    20211109 update

--- Java/387.java
+++ Java/387.java
@@ -29,3 +29,18 @@ class Solution {
         return -1;
     }
 }
+
+class Solution {
+    public int firstUniqChar(String s) {
+        int ans = Integer.MAX_VALUE;
+        
+        for(char c = 'a'; c <= 'z'; c++) {
+            int index = s.indexOf(c);
+            if(index != -1 && index == s.lastIndexOf(c)) {
+                ans = Math.min(ans, index);
+            }
+        }
+        
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
