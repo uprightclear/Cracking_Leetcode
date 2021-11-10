3ccca330be6df60dca071fbf7c7a754ec8f883b7
uprightclear <uprightclear@gmail.com>
2021-11-10 09:15:13 -0800

    20211110 update

---
+++ Java/629.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int kInversePairs(int n, int k) {
+        final int MOD = 1000000007;
+        int[][] f = new int[2][k + 1];
+        f[0][0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j <= k; ++j) {
+                int cur = i & 1, prev = cur ^ 1;
+                f[cur][j] = (j - 1 >= 0 ? f[cur][j - 1] : 0) - (j - i >= 0 ? f[prev][j - i] : 0) + f[prev][j];
+                if (f[cur][j] >= MOD) {
+                    f[cur][j] -= MOD;
+                } else if (f[cur][j] < 0) {
+                    f[cur][j] += MOD;
+                }
+            }
+        }
+        return f[n & 1][k];
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/k-inverse-pairs-array/solution/kge-ni-xu-dui-shu-zu-by-leetcode-solutio-0hkr/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
