88f05ccb2c91b03b89fe49846b5774590e59a569
uprightclear <uprightclear@gmail.com>
2021-11-09 22:50:32 -0800

    20211109 update

---
+++ Java/410.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        int n = nums.length;
+        int[][] f = new int[n + 1][m + 1];
+        int[] sub = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                f[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            sub[i + 1] = sub[i] + nums[i];
+        }
+        f[0][0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                for (int k = 0; k < i; k++) {
+                    f[i][j] = Math.min(f[i][j], Math.max(f[k][j - 1], sub[i] - sub[k]));
+                }
+            }
+        }
+        return f[n][m];        
+    }
+}
+
+
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        long l = 0;
+        long r = 0;        
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            r += nums[i];
+            if (l < nums[i]) {
+                l = nums[i];
+            }
+        }
+        long ans = r;
+        while (l <= r) {
+            long mid = (l + r) >> 1;
+            long sum = 0;
+            int cnt = 1;
+            for (int i = 0; i < n; i++) {
+                if (sum + nums[i] > mid) {
+                    cnt ++;
+                    sum = nums[i];
+                } else {
+                    sum += nums[i];
+                }
+            }
+            if (cnt <= m) {
+                ans = Math.min(ans, mid);
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return (int)ans;      
+    }
+}
