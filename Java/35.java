d4a71e9cb8e8bd9a5d903bf1a4743ca9336a95d8
uprightclear <uprightclear@gmail.com>
2021-11-10 16:52:37 -0800

    20211110 update

--- Java/35.java
+++ Java/35.java
@@ -14,3 +14,19 @@ class Solution {
         return left;
     }
 }
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length;
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            if(nums[mid] >= target) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        
+        return l;
+    }
+}
