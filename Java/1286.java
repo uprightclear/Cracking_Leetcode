04a6f2f8f2c40a14c21fbffb21ab6ded5cb9c346
uprightclear <uprightclear@gmail.com>
2021-11-13 17:43:05 -0800

    20211113 update

--- Java/1286.java
+++ Java/1286.java
@@ -1,3 +1,4 @@
+//bitmask
 class CombinationIterator {
     public Deque < String > combinations = new ArrayDeque < String > ();
 
@@ -25,6 +26,47 @@ class CombinationIterator {
 
     public String next() {
         return combinations.pop();
+    }
+
+    public boolean hasNext() {
+        return (!combinations.isEmpty());
+    }
+}
+
+//backtrack
+class CombinationIterator {
+    public Deque < String > combinations = new ArrayDeque < String > ();
+    String characters;
+    int n, k;
+
+    public void backtrack(int first, StringBuilder curr) {
+        // if the combination is done
+        if (curr.length() == k) {
+            combinations.push(curr.toString());
+            // speed up by non-constructing combinations 
+            // with more than k elements  
+            return;
+        }
+
+        for (int i = first; i < n; ++i) {
+            // add i into the current combination
+            curr.append(characters.charAt(i));
+            // use next integers to complete the combination
+            backtrack(i + 1, curr);
+            // backtrack
+            curr.deleteCharAt(curr.length() - 1);
+        }
+    }
+
+    public CombinationIterator(String characters, int combinationLength) {
+        this.n = characters.length();
+        this.k = combinationLength;
+        this.characters = characters;
+        backtrack(0, new StringBuilder());
+    }
+
+    public String next() {
+        return combinations.removeLast();
     }
 
     public boolean hasNext() {
