d40bd0b294a1afd573c39ff7ede9dcdd01f5a7b2
uprightclear <uprightclear@gmail.com>
2021-11-15 14:19:28 -0800

    20211115 update

---
+++ Java/901.java
@@ -0,0 +1,21 @@
+class StockSpanner {
+    Deque<int[]> stack;
+    public StockSpanner() {
+        stack = new LinkedList<>();
+    }
+    
+    public int next(int price) {
+        int count = 1;
+        while(!stack.isEmpty() && stack.peek()[0] <= price) {
+            count += stack.pop()[1];
+        }
+        stack.push(new int[]{price, count});
+        return count;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
