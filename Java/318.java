d8d5b40a6bce8ae2c1add90fc4bd0bf9adf1f290
uprightclear <uprightclear@gmail.com>
2021-11-16 09:49:44 -0800

    20211116 update

---
+++ Java/318.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProduct(String[] words) {
+        int length = words.length;
+        int[] masks = new int[length];
+        for (int i = 0; i < length; i++) {
+            String word = words[i];
+            int wordLength = word.length();
+            for (int j = 0; j < wordLength; j++) {
+                masks[i] |= 1 << (word.charAt(j) - 'a');
+            }
+        }
+        int maxProd = 0;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if ((masks[i] & masks[j]) == 0) {
+                    maxProd = Math.max(maxProd, words[i].length() * words[j].length());
+                }
+            }
+        }
+        return maxProd;
+    }
+}
