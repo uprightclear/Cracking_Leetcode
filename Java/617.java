c015a3ef8be22ac0d22cc1e7bf4f48f132b29a58
uprightclear <uprightclear@gmail.com>
2021-11-16 09:56:05 -0800

    20211116 update

--- Java/617.java
+++ Java/617.java
@@ -16,14 +16,9 @@
 class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
         if(root1 == null || root2 == null) return root1 == null ? root2 : root1;
-        return dfs(root1, root2);
-    }
-
-    public TreeNode dfs(TreeNode r1, TreeNode r2){
-        if(r1 == null || r2 == null) return r1 == null ? r2 : r1;
-        r1.val += r2.val;
-        r1.left = dfs(r1.left, r2.left);
-        r1.right = dfs(r1.right, r2.right);
-        return r1;
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
     }
 }
