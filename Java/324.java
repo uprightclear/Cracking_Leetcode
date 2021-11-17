d8044edccf1db47f3c646ca82174fac2e35b3d67
uprightclear <uprightclear@gmail.com>
2021-11-17 15:23:27 -0800

    20211117 update

---
+++ Java/324.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        int len = nums.length;
+        int[] buckets = new int[50001];
+        
+        for(int num : nums) {
+            buckets[num]++;
+        }
+        
+        int maxindex = 0;
+        int minindex = 0;
+        if(len % 2 == 1) {//odd num
+            maxindex = len - 2;
+            minindex = len - 1;
+        } else {//even num
+            maxindex = len - 1;
+            minindex = len - 2;
+        }
+        
+        int maxLen = 5000;
+        for(int i = 1; i <= maxindex; i += 2) {
+            while(buckets[maxLen] == 0) maxLen--;
+            nums[i] = maxLen;
+            buckets[maxLen]--;
+        }
+        
+        for(int i = 0; i <= minindex; i += 2) {
+            while(buckets[maxLen] == 0) maxLen--;
+            nums[i] = maxLen;
+            buckets[maxLen]--;
+        }
+    }
+}
