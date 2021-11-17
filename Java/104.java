144e3522c6d00ac03bb3ce4213517cc24921a6f8
uprightclear <uprightclear@gmail.com>
2021-11-17 15:37:49 -0800

    20211117 update

--- Java/104.java
+++ Java/104.java
@@ -1,3 +1,11 @@
+//recursion
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
+
 //DFS
 class Solution {
     public int maxDepth(TreeNode root) {
