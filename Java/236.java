2b9c42a4a63941509842c77bfa2454b79cc63fdb
uprightclear <uprightclear@gmail.com>
2021-11-18 15:05:50 -0800

    20211118 update

--- Java/236.java
+++ Java/236.java
@@ -39,3 +39,16 @@ class Solution {
         return this.ans;
     }
 }
+
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return root;
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left == null && right == null) return null;
+        if(left != null && right != null) return root;
+        return left == null ? right : left;
+    }
+}
