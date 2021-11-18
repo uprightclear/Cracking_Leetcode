a434e2f8887ab37608683faeb02edeb556465225
uprightclear <uprightclear@gmail.com>
2021-11-18 11:22:22 -0800

    20211118 update

--- Java/392.java
+++ Java/392.java
@@ -19,3 +19,20 @@ class Solution {
         return ans == lens;
     }
 }
+
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        Integer leftBound = s.length(), rightBound = t.length();
+        Integer pLeft = 0, pRight = 0;
+
+        while (pLeft < leftBound && pRight < rightBound) {
+            // move both pointers or just the right pointer
+            if (s.charAt(pLeft) == t.charAt(pRight)) {
+                pLeft += 1;
+            }
+            pRight += 1;
+        }
+        return pLeft == leftBound;
+    }
+}
