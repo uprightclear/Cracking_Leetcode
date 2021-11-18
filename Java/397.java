768bea5492d6a2e628cd3ec7bbd0975be7899058
uprightclear <uprightclear@gmail.com>
2021-11-18 08:05:20 -0800

    20211118 update

---
+++ Java/397.java
@@ -0,0 +1,12 @@
+//recursion
+class Solution {
+    public int integerReplacement(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return 1 + integerReplacement(n / 2);
+        }
+        return 2 + Math.min(integerReplacement(n / 2), integerReplacement(n / 2 + 1));
+    }
+}
