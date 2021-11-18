0a38422c513ebbf9d4eeb0ee6f6b4e0ebd9b38cd
uprightclear <uprightclear@gmail.com>
2021-11-18 12:21:03 -0800

    20211118 update

--- Java/116.java
+++ Java/116.java
@@ -23,6 +23,25 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
+        if(root == null) return root;
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.offer(root);
+        
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i = 1; i <= size; i++) {
+                Node node = queue.poll();
+                if(i != size) node.next = queue.peek(); 
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+        }
+        return root;
+    }
+}
+
+class Solution {
+    public Node connect(Node root) {
         if (root == null) {
             return root;
         }
