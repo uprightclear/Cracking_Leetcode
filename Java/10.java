0d4dfd72e717fccd7db683da56b789e929452c2c
uprightclear <uprightclear@gmail.com>
2021-11-18 18:41:35 -0800

    20211118 update

--- Java/10.java
+++ Java/10.java
@@ -1,20 +1,24 @@
 class Solution {
-    public boolean isMatch(String text, String pattern) {
-        boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
-        dp[text.length()][pattern.length()] = true;
-
-        for (int i = text.length(); i >= 0; i--){
-            for (int j = pattern.length() - 1; j >= 0; j--){
-                boolean first_match = (i < text.length() &&
-                                       (pattern.charAt(j) == text.charAt(i) ||
-                                        pattern.charAt(j) == '.'));
-                if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
-                    dp[i][j] = dp[i][j+2] || first_match && dp[i+1][j];
+    public boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        
+        for(int i = 1; i <= p.length(); i++) {
+            if(p.charAt(i - 1) == '*') dp[0][i] = dp[0][i - 2];
+        }
+        
+        for(int i = 1; i <= s.length(); i++) {
+            for(int j = 1; j <= p.length(); j++) {
+                if(p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)) dp[i][j] = dp[i - 1][j - 1];
+                else if(p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if(p.charAt(j - 2) == '.' || p.charAt(j - 2) == s.charAt(i - 1)) dp[i][j] |= dp[i - 1][j];
                 } else {
-                    dp[i][j] = first_match && dp[i+1][j+1];
+                    dp[i][j] = false;
                 }
             }
         }
-        return dp[0][0];
+        
+        return dp[s.length()][p.length()];
     }
 }
