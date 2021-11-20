ce661997166b0f18c31cab5c79b0501cd25e5b5a
uprightclear <uprightclear@gmail.com>
2021-11-19 19:33:31 -0800

    20211119 update

--- Java/675.java
+++ Java/675.java
@@ -1,21 +1,61 @@
-public int bfs(List<List<Integer>> forest, int sr, int sc, int tr, int tc) {
-    int R = forest.size(), C = forest.get(0).size();
-    Queue<int[]> queue = new LinkedList();
-    queue.add(new int[]{sr, sc, 0});
-    boolean[][] seen = new boolean[R][C];
-    seen[sr][sc] = true;
-    while (!queue.isEmpty()) {
-        int[] cur = queue.poll();
-        if (cur[0] == tr && cur[1] == tc) return cur[2];
-        for (int di = 0; di < 4; ++di) {
-            int r = cur[0] + dr[di];
-            int c = cur[1] + dc[di];
-            if (0 <= r && r < R && 0 <= c && c < C &&
-                    !seen[r][c] && forest.get(r).get(c) > 0) {
-                seen[r][c] = true;
-                queue.add(new int[]{r, c, cur[2]+1});
+class Solution {
+    static int[][] dir = {{0,1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    public int cutOffTree(List<List<Integer>> forest) {
+        if (forest == null || forest.size() == 0) return 0;
+        int m = forest.size(), n = forest.get(0).size();
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (forest.get(i).get(j) > 1) {
+                    pq.add(new int[] {i, j, forest.get(i).get(j)});
+                }
             }
         }
+
+        int[] start = new int[2];
+        int sum = 0;
+        while (!pq.isEmpty()) {
+            int[] tree = pq.poll();
+            int step = minStep(forest, start, tree, m, n);
+
+            if (step < 0) return -1;
+            sum += step;
+
+            start[0] = tree[0];
+            start[1] = tree[1];
+        }
+
+        return sum;
     }
-    return -1;
+
+    private int minStep(List<List<Integer>> forest, int[] start, int[] tree, int m, int n) {
+        int step = 0;
+        boolean[][] visited = new boolean[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start[0]][start[1]] = true;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = queue.poll();
+                if (curr[0] == tree[0] && curr[1] == tree[1]) return step;
+
+                for (int[] d : dir) {
+                    int nr = curr[0] + d[0];
+                    int nc = curr[1] + d[1];
+                    if (nr < 0 || nr >= m || nc < 0 || nc >= n 
+                        || forest.get(nr).get(nc) == 0 || visited[nr][nc]) continue;
+                    queue.add(new int[] {nr, nc});
+                    visited[nr][nc] = true;
+                }
+            }
+            step++;
+        }
+
+        return -1;
+    }
 }
