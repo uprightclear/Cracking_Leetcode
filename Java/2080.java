6bc6b61f0f21e60673e385a301d49e4f44c43bf1
uprightclear <uprightclear@gmail.com>
2021-11-21 11:55:25 -0800

    20211121 update

--- Java/2080.java
+++ Java/2080.java
@@ -1,57 +1,52 @@
 class RangeFreqQuery {
-    Map<Integer, List<Integer>> pos;
- 
+    
+    Map<Integer, List<Integer>> map = new HashMap();
+    
     public RangeFreqQuery(int[] arr) {
-        pos = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (!pos.containsKey(arr[i])) {
-                pos.put(arr[i], new ArrayList<>());
-           }
-            pos.get(arr[i]).add(i);
-       }
-   }
- 
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            if(!map.containsKey(arr[i]))
+                map.put(arr[i], new ArrayList());
+            map.get(arr[i]).add(i);
+        }
+    }
+    
     public int query(int left, int right, int value) {
-        if (!pos.containsKey(value)) {
+        List<Integer> A = map.get(value);
+        if(A == null || left > A.get(A.size()-1) || right < A.get(0))
             return 0;
-       }
-        List<Integer> p = pos.get(value);
-        int start = bSearch2(p, left);
-        int end = bSearch(p, right);
-        return end - start + 1;
-   }
- 
-    // 二分查找最后一个 <= value 的元素下标
-    int bSearch(List<Integer> arr, int value) {
-        if (arr.size() == 0 || value < arr.get(0)) {
-            return -1;
-       }
-        int left = 0, right = arr.size() - 1;
-        while (left + 1 < right) {
-            int mid = (left + right) / 2;
-            if (arr.get(mid) <= value) {
-                left = mid;
-           } else {
+        
+        int i = ceil(A, left), j = floor(A, right);        
+        return j-i+1;
+    }
+    
+    public int ceil(List<Integer> A, int x){
+        int left = 0, right = A.size()-1; 
+        if(x < A.get(0))
+            return 0;
+        
+        while(left < right){
+            int mid = (left+right)/2;
+            if(A.get(mid) < x)
+                left = mid + 1;
+            else 
                 right = mid;
-           }
-       }
-        return arr.get(right) <= value ? right : left;
-   }
- 
-    // 二分查找第一个 >= value 的元素下标
-    int bSearch2(List<Integer> arr, int value) {
-        if (arr.size() == 0 || arr.get(arr.size() - 1) < value) {
-            return arr.size();
-       }
-        int left = 0, right = arr.size() - 1;
-        while (left + 1 < right) {
-            int mid = (left + right) / 2;
-            if (arr.get(mid) < value) {
+        }
+        return left;
+    }
+    
+    public int floor(List<Integer> A, int x){
+        int left = 0, right = A.size()-1; 
+        if(x > A.get(right))
+            return right;
+        
+        while(left < right){
+            int mid = (left+right)/2+1;
+            if(A.get(mid) > x)
+                right = mid - 1;
+            else 
                 left = mid;
-           } else {
-                right = mid;
-           }
-       }
-        return arr.get(left) < value ? right : left;
-   }
- }
+        }
+        return left;
+    }    
+}
