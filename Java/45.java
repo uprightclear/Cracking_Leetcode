4bf0c1a5b66ae62445eb0098fc87ca89af4857bb
uprightclear <uprightclear@gmail.com>
2021-11-22 11:53:01 -0800

    20211122 update

--- Java/45.java
+++ Java/45.java
@@ -1,17 +1,35 @@
-//DP
+//dp (this solution is too slow, plz consider the next solution with greedy)
 class Solution {
     public int jump(int[] nums) {
         int len = nums.length;
         int[] dp = new int[len];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-        for(int i = 1; i < len; i++){
-            for(int j = 0; j < i; j++){
-                if(nums[j] + j >= i){
+        for(int i = 1; i < len; i++) {//iterate the index from 1 to len - 1
+            for(int j = 0; j < i; j++) {//from where
+                if(nums[j] + j >= i) {//if the next index is include in the current maximum scope
                     dp[i] = Math.min(dp[i], dp[j] + 1);
                 }
             }
         }
         return dp[len - 1];
+    }
+}
+
+//greedy
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0, currentJumpEnd = 0, farthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            // we continuously find the how far we can reach in the current jump
+            farthest = Math.max(farthest, i + nums[i]);
+            // if we have come to the end of the current jump,
+            // we need to make another jump
+            if (i == currentJumpEnd) {
+                jumps++;
+                currentJumpEnd = farthest;
+            }
+        }
+        return jumps;
     }
 }
