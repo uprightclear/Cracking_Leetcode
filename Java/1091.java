798a8280840a3473581ce0c4ac3c76aa0bf26e90
uprightclear <uprightclear@gmail.com>
2021-11-22 15:39:09 -0800

    20211122 update

--- Java/1091.java
+++ Java/1091.java
@@ -27,3 +27,34 @@ class Solution {
         return -1;
     }
 }
+
+
+class Solution {
+    int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+        int ans = 1;
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[n][n];
+        if(grid[0][0] == 1) return -1;
+        visited[0][0] = true;
+        queue.offer(new int[] {0, 0});
+        
+        while(!queue.isEmpty()) {
+            for(int size = queue.size(); size > 0; size--) {
+                int[] cur = queue.poll();
+                if(cur[0] == n - 1 && cur[1] == n - 1) return ans;
+                for(int i = 0; i < 8; i++) {
+                    int newx = cur[0] + directions[i][0];
+                    int newy = cur[1] + directions[i][1];
+                    if(newx >= 0 && newx < n && newy >= 0 && newy < n && grid[newx][newy] == 0 && !visited[newx][newy]) {
+                        visited[newx][newy] = true;
+                        queue.offer(new int[] {newx, newy});
+                    }
+                }
+            }
+            ans++;
+        }
+        return -1;
+    }
+}
