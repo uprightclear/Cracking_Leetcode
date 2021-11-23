8a81269cd0ecb696a445928a3e9f08f31c3790d1
uprightclear <uprightclear@gmail.com>
2021-11-23 14:28:54 -0800

    20211123 update

---
+++ Java/828.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/count-unique-characters-of-all-substrings-of-a-given-string/discuss/128952/C%2B%2BJavaPython-One-pass-O(N)
+
+// index[26][2] record last two occurrence index for every upper characters.
+// Initialise all values in index to -1.
+// Loop on string S, for every character c, update its last two occurrence index to index[c].
+class Solution {
+    public int uniqueLetterString(String S) {
+        int[][] index = new int[26][2];
+        for (int i = 0; i < 26; ++i) Arrays.fill(index[i], -1);
+        int res = 0, N = S.length(), mod = (int)Math.pow(10, 9) + 7;
+        for (int i = 0; i < N; ++i) {
+            int c = S.charAt(i) - 'A';
+            res = (res + (i - index[c][1]) * (index[c][1] - index[c][0]) % mod) % mod;
+            index[c] = new int[] {index[c][1], i};
+        }
+        for (int c = 0; c < 26; ++c)
+            res = (res + (N - index[c][1]) * (index[c][1] - index[c][0]) % mod) % mod;
+        return res;
+    }
+}
