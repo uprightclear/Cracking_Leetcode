3bd54f962549a76a3209a068c1dfc219900e6f2d
uprightclear <uprightclear@gmail.com>
2021-11-23 19:08:58 -0800

    20211123 update

--- Java/828.java
+++ Java/828.java
@@ -18,3 +18,25 @@ class Solution {
         return res;
     }
 }
+
+
+
+class Solution {
+    public int uniqueLetterString(String S) {     
+        int res = 0;
+        if (S == null || S.length() == 0)
+            return res;    
+        int[] showLastPosition = new int[128];
+        int[] contribution = new int[128];
+        int cur = 0;
+        for (int i = 0; i < S.length(); i++) {
+            char x = S.charAt(i);
+            cur -= contribution[x]; 
+            contribution[x] = (i - (showLastPosition[x] - 1));
+            cur += contribution[x]; 
+            showLastPosition[x] = i + 1;
+            res += cur;
+        }   
+        return res;
+    }
+}
