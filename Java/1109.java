f80846804f4a4a3e1742cd8b3599da96ced21842
uprightclear <uprightclear@gmail.com>
2021-11-27 21:11:42 -0800

    20211127 update

--- Java/1109.java
+++ Java/1109.java
@@ -14,3 +14,16 @@ class Solution {
         return nums;
     }
 }
+
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] ans = new int[n];
+        int len = bookings.length;
+        for(int i = 0; i < len; i++) {
+            for(int j = bookings[i][0]; j <= bookings[i][1]; j++) {
+                ans[j - 1] += bookings[i][2];
+            }
+        }
+        return ans;
+    }
+}
