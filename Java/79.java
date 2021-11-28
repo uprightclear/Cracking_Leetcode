f5a2ad8d05aba288f9eee531cf11562ec22e7deb
uprightclear <uprightclear@gmail.com>
2021-11-28 11:55:08 -0800

    20211128 update

--- Java/79.java
+++ Java/79.java
@@ -57,3 +57,39 @@ class Solution {
         return x >= 0 && x < m && y >= 0 && y < n;
     }
 }
+
+class Solution {
+    int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    boolean[][] used;
+    int len;
+    public boolean exist(char[][] board, String word) {
+        int m = board.length, n = board[0].length;
+        used = new boolean[m][n];
+        len = word.length();
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == word.charAt(0)) {
+                    if(dfs(board, i, j, 1, word)) return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean dfs(char[][] board, int x, int y, int depth, String word) {
+        if(depth == len) return true;
+        used[x][y] = true;
+        Character next = word.charAt(depth);
+        
+        for(int i = 0; i < 4; i++) {
+            int newx = x + directions[i][0];
+            int newy = y + directions[i][1];
+            if(newx >= 0 && newx < board.length && newy >= 0 && newy < board[0].length && !used[newx][newy] && board[newx][newy] == next) {
+                if(dfs(board, newx, newy, depth + 1, word)) return true;
+            }
+        }
+        
+        used[x][y] = false;
+        return false;
+    }
+}
