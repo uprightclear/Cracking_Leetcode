6c1fd7bb61fb02b3a13b15b9e7c13127f464cd09
uprightclear <uprightclear@gmail.com>
2021-11-29 15:46:28 -0800

    20211129 update

---
+++ Java/135.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int candy(int[] ratings) {
+        //左规则
+        int[] left = new int[ratings.length];
+        //右规则
+        int[] right = new int[ratings.length];
+        Arrays.fill(left, 1);
+        Arrays.fill(right, 1);
+        for(int i = 1; i < ratings.length; i++) {
+            if(ratings[i] > ratings[i - 1]) left[i] = left[i - 1] + 1;
+        }    
+        int count = left[ratings.length - 1];
+        for(int i = ratings.length - 2; i >= 0; i--) {
+            if(ratings[i] > ratings[i + 1]) right[i] = right[i + 1] + 1;
+            count += Math.max(left[i], right[i]);
+        }
+        return count;
+    }
+}
