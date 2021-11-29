bba179afa4c5b4384ba76b48f6ae4d239ac6bbec
uprightclear <uprightclear@gmail.com>
2021-11-29 11:11:36 -0800

    20211128 update

---
+++ Java/333.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    // return array for each node: 
+    //     [0] --> min
+    //     [1] --> max
+    //     [2] --> largest BST in its subtree(inclusive)
+    
+    public int largestBSTSubtree(TreeNode root) {
+        int[] ret = largestBST(root);
+        return ret[2];
+    }
+    
+    private int[] largestBST(TreeNode node){
+        if(node == null){
+            return new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE, 0};
+        }
+        int[] left = largestBST(node.left);
+        int[] right = largestBST(node.right);
+        if(node.val > left[1] && node.val < right[0]){
+            return new int[]{Math.min(node.val, left[0]), Math.max(node.val, right[1]), left[2] + right[2] + 1};
+        }else{
+            return new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE, Math.max(left[2], right[2])};
+        }
+    }
+}
