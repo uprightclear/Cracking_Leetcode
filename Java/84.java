fc0c9bd2591390b55456ee7098b528183c2a0e11
uprightclear <uprightclear@gmail.com>
2021-11-29 18:49:20 -0800

    20211129 update

---
+++ Java/84.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int maxArea = 0;
+        Stack<Integer> stack = new Stack<>();
+        int p = 0;
+        while (p < heights.length) {
+            //栈空入栈
+            if (stack.isEmpty()) {
+                stack.push(p);
+                p++;
+            } else {
+                int top = stack.peek();
+                //当前高度大于栈顶，入栈
+                if (heights[p] >= heights[top]) {
+                    stack.push(p);
+                    p++;
+                } else {
+                    //保存栈顶高度
+                    int height = heights[stack.pop()];
+                    //左边第一个小于当前柱子的下标
+                    int leftLessMin = stack.isEmpty() ? -1 : stack.peek();
+                    //右边第一个小于当前柱子的下标
+                    int RightLessMin = p;
+                    //计算面积
+                    int area = (RightLessMin - leftLessMin - 1) * height;
+                    maxArea = Math.max(area, maxArea);
+                }
+            }
+        }
+        while (!stack.isEmpty()) {
+            //保存栈顶高度
+            int height = heights[stack.pop()];
+            //左边第一个小于当前柱子的下标
+            int leftLessMin = stack.isEmpty() ? -1 : stack.peek();
+            //右边没有小于当前高度的柱子，所以赋值为数组的长度便于计算
+            int RightLessMin = heights.length;
+            int area = (RightLessMin - leftLessMin - 1) * height;
+            maxArea = Math.max(area, maxArea);
+        }
+        return maxArea;
+    }
+}
