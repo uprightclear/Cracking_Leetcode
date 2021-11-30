0349860db04da0ee8914a4e33fc83216d913a9e2
uprightclear <uprightclear@gmail.com>
2021-11-30 13:34:18 -0800

    20211130 update

--- Java/150.java
+++ Java/150.java
@@ -29,3 +29,28 @@ class Solution {
         else return false;
     }
 }
+
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new LinkedList<>();
+        for(String token : tokens) {
+            if(token.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (token.equals("-")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a - b);
+            } else if (token.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (token.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a / b);
+            } else {
+                stack.push(Integer.valueOf(token));
+            }
+        }
+        return stack.pop();
+    }
+}
