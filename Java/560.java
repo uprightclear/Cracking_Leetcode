1d0ad46508f80b66aa25f2c9e21efe44396336ae
uprightclear <uprightclear@gmail.com>
2021-11-29 20:41:54 -0800

    20211129 update

--- Java/560.java
+++ Java/560.java
@@ -1,14 +1,12 @@
-public class Solution {
+class Solution {
     public int subarraySum(int[] nums, int k) {
         int count = 0, pre = 0;
-        HashMap < Integer, Integer > mp = new HashMap < > ();
-        mp.put(0, 1);
-        for (int i = 0; i < nums.length; i++) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for(int i = 0; i < nums.length; i++) {
             pre += nums[i];
-            if (mp.containsKey(pre - k)) {
-                count += mp.get(pre - k);
-            }
-            mp.put(pre, mp.getOrDefault(pre, 0) + 1);
+            if(map.containsKey(pre - k)) count += map.get(pre - k);
+            map.put(pre, map.getOrDefault(pre, 0) + 1);
         }
         return count;
     }
