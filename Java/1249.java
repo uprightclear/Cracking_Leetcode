238ea36e8a94294eaaede543293733e449a1fe04
uprightclear <uprightclear@gmail.com>
2021-11-29 19:35:09 -0800

    20211129 update

---
+++ Java/1249.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Set<Integer> indexesToRemove = new HashSet<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    indexesToRemove.add(i);
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        // Put any indexes remaining on stack into the set.
+        while (!stack.isEmpty()) indexesToRemove.add(stack.pop());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (!indexesToRemove.contains(i)) {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
