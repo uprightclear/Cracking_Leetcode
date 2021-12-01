273aeafd65fc399740cda680cb19e59b5187e872
uprightclear <uprightclear@gmail.com>
2021-12-01 15:31:57 -0800

    20211201 update

---
+++ Java/173.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+
+    ArrayList<Integer> nodesSorted;
+    int index;
+
+    public BSTIterator(TreeNode root) {
+
+        // Array containing all the nodes in the sorted order
+        this.nodesSorted = new ArrayList<Integer>();
+
+        // Pointer to the next smallest element in the BST
+        this.index = -1;
+
+        // Call to flatten the input binary search tree
+        this._inorder(root);
+    }
+
+    private void _inorder(TreeNode root) {
+
+        if (root == null) {
+            return;
+        }
+
+        this._inorder(root.left);
+        this.nodesSorted.add(root.val);
+        this._inorder(root.right);
+    }
+
+    /**
+     * @return the next smallest number
+     */
+    public int next() {
+        return this.nodesSorted.get(++this.index);
+    }
+
+    /**
+     * @return whether we have a next smallest number
+     */
+    public boolean hasNext() {
+        return this.index + 1 < this.nodesSorted.size();
+    }
+}
+
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
