0869fc144a4aafd35ea5966777518f910efe5129
uprightclear <uprightclear@gmail.com>
2021-12-01 15:01:40 -0800

    20211201 update

--- Java/1047.java
+++ Java/1047.java
@@ -14,3 +14,20 @@ class Solution {
         return sb.toString();
     }
 }
+
+//use StringBuilder directly
+class Solution {
+    public String removeDuplicates(String S) {
+      StringBuilder sb = new StringBuilder();
+      int sbLength = 0;
+      for(char character : S.toCharArray()) {
+        if (sbLength != 0 && character == sb.charAt(sbLength - 1))
+          sb.deleteCharAt(sbLength-- - 1);
+        else {
+          sb.append(character);
+          sbLength++;
+        }
+      }
+      return sb.toString();
+    }
+  }
