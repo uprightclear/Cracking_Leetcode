7ccff94905505657e47c6f82c3bd9e1f30fcad2a
uprightclear <uprightclear@gmail.com>
2021-12-01 12:32:57 -0800

    20211201 update

--- Java/50.java
+++ Java/50.java
@@ -22,3 +22,57 @@ class Solution {
         return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
     }
 }
+
+//Fast Power Algorithm Recursive
+class Solution {
+    private double fastPow(double x, long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+    public double myPow(double x, int n) {
+        int N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return fastPow(x, N);
+    }
+}
+
+//Fast Power Algorithm Iterative
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double ans = 1;
+        double current_product = x;
+        for (long i = N; i > 0; i /= 2) {
+            if ((i % 2) == 1) {
+                ans = ans * current_product;
+            }
+            current_product = current_product * current_product;
+        }
+        return ans;
+    }
+};
+
+//TLE
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1;
+        int m = Math.abs(n);
+        while(m-- > 0) ans *= x;
+        return n < 0 ? 1 / ans : ans;
+    }
+}
