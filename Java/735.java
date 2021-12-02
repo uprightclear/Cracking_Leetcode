be35dfa0e58a05c404cfcfc92c63d88326e2c18b
uprightclear <uprightclear@gmail.com>
2021-12-01 16:01:17 -0800

    20211201 update

--- Java/735.java
+++ Java/735.java
@@ -61,3 +61,29 @@ class Solution {
         return ans;
     }
 }
+
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> s = new Stack<>();
+        for(int i: asteroids){
+            if(i > 0){
+                s.push(i);
+            }else{// i is negative
+                while(!s.isEmpty() && s.peek() > 0 && s.peek() < Math.abs(i)){
+                    s.pop();
+                }
+                if(s.isEmpty() || s.peek() < 0){
+                    s.push(i);
+                }else if(i + s.peek() == 0){
+                    s.pop(); //equal
+                }
+            }
+        }
+        int[] res = new int[s.size()];   
+        for(int i = res.length - 1; i >= 0; i--){
+            res[i] = s.pop();
+        }
+        return res;
+    }
+}
