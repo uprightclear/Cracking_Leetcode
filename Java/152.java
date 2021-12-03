8c59ecbee96dd59221ec052a43cc47fe1231be1d
uprightclear <uprightclear@gmail.com>
2021-12-02 16:39:33 -0800

    20211202 update

--- Java/152.java
+++ Java/152.java
@@ -54,3 +54,16 @@ class Solution {
         return ans;
     }
 }
+
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length, res = nums[0], l = 0, r = 0;
+        for (int i = 0; i < n; i++) {
+            l =  (l == 0 ? 1 : l) * nums[i];
+            r =  (r == 0 ? 1 : r) * nums[n - 1 - i];
+            res = Math.max(res, Math.max(l, r));
+        }
+        return res;
+    }
+}
