e7b59898ecdc8bd8647597254828d2fceb51fdc4
uprightclear <uprightclear@gmail.com>
2021-12-05 10:44:08 -0800

    20211205 update

---
+++ Java/1816.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int n = s.length();
+        int end = 0, count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i == n || s.charAt(i) == ' ') {
+                count++;
+                if (count == k) {
+                    end = i;
+                    break;
+                }
+            }
+        }
+        return s.substring(0, end);
+    }
+}
