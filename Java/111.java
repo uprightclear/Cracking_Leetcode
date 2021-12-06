1c2c35c54e74ed501be717184ab32655c67db86e
uprightclear <uprightclear@gmail.com>
2021-12-06 14:19:18 -0800

    20211206 update

---
+++ Java/111.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        if(root.left == null && root.right == null) return 1;
+        int min = Integer.MAX_VALUE;
+        if(root.left != null) min = Math.min(min, minDepth(root.left));
+        if(root.right != null) min = Math.min(min, minDepth(root.right));
+        return 1 + min;
+    }
+}
