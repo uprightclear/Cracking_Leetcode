9487befaa1e5b75f56add88bbc6cf69b70e3eb66
uprightclear <uprightclear@gmail.com>
2021-12-11 11:48:33 -0800

    20211211 update

---
+++ Java/Leetcode/709.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String toLowerCase(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            if (ch >= 65 && ch <= 90) {
+                ch |= 32;
+            }
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
