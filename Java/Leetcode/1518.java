5660a32719616b16af2ee7d7360d7087f3340ec3
uprightclear <uprightclear@gmail.com>
2021-12-16 12:35:12 -0800

    20211215 update

---
+++ Java/Leetcode/1518.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int ans = numBottles;
+        while(numBottles / numExchange >= 1) {
+            ans += numBottles / numExchange;
+            numBottles = numBottles - (numBottles / numExchange) * (numExchange - 1);
+        }
+        return ans;
+    }
+}
