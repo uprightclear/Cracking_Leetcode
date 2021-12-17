32a9bf86665608951d2fb27133e46e01779af5b5
uprightclear <uprightclear@gmail.com>
2021-12-17 15:51:17 -0800

    20211217 update

--- Java/Leetcode/419.java
+++ Java/Leetcode/419.java
@@ -25,3 +25,28 @@ class Solution {
         }
     }
 }
+
+//follow up(without changing orinal matrix)
+class Solution {
+    public int countBattleships(char[][] board) {
+        int row = board.length;
+        int col = board[0].length;
+        int ans = 0;
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (board[i][j] == 'X') {
+                    if (i > 0 && board[i - 1][j] == 'X') {
+                        continue;
+                    }
+                    if (j > 0 && board[i][j - 1] == 'X') {
+                        continue;
+                    }
+                    //if the current node's up and current node's left is not 'X'
+                    //or the current node is on the boundary;
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
