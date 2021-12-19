8ce665ebd2d87b7e6d2756f7e037d72506b4eed9
uprightclear <uprightclear@gmail.com>
2021-12-18 22:30:19 -0800

    20211218 update

---
+++ Java/Leetcode/2111.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int kIncreasing(int[] arr, int k) {
+        int result = 0;
+        for (int i = 0; i < k; i++) {
+           List<Integer> list = new ArrayList<>();
+           for (int j = i; j < arr.length; j += k) list.add(arr[j]);
+           result += increasing(list);
+        }
+       return result;
+    }
+
+    private int increasing(List<Integer> nums) {
+        // 将 nums 变成递增
+        // nlogn 求 LIS
+        int[] dp = new int[nums.size()];
+        int len = 0;
+        for (int num : nums) {
+            int l = 0, r = len;
+            while (l < r) {
+                int mid = (l + r) / 2;
+                if (dp[mid] <= num) { // 非严格递增，等于也可
+                   l = mid + 1;
+                } else {
+                   r = mid;
+                }
+            }
+            if (r >= len) {
+                len++;
+            }
+            dp[r] = num;
+        }
+        return nums.size() - len;
+    }
+}
