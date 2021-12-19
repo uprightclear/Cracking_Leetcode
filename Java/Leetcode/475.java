26290cff321c2b0b444e0ba3f0b593943f2ab71f
uprightclear <uprightclear@gmail.com>
2021-12-19 11:46:30 -0800

    20211219 update

---
+++ Java/Leetcode/475.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int findRadius(int[] houses, int[] heaters) {
+        int ans = 0;
+        Arrays.sort(heaters);
+        for (int house : houses) {
+            int i = binarySearch(heaters, house);
+            int j = i + 1;
+            int leftDistance = i < 0 ? Integer.MAX_VALUE : house - heaters[i];
+            int rightDistance = j >= heaters.length ? Integer.MAX_VALUE : heaters[j] - house;
+            int curDistance = Math.min(leftDistance, rightDistance);
+            ans = Math.max(ans, curDistance);
+        }
+        return ans;
+    }
+
+    public int binarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        if (nums[left] > target) {
+            return -1;
+        }
+        while (left < right) {
+            int mid = (right - left + 1) / 2 + left;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        return left;
+    }
+}
