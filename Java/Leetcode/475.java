cc4bc491db72a03c93a2e2986969d25260c5fc64
uprightclear <uprightclear@gmail.com>
2021-12-19 11:58:10 -0800

    20211219 update

--- Java/Leetcode/475.java
+++ Java/Leetcode/475.java
@@ -29,3 +29,21 @@ class Solution {
         return left;
     }
 }
+
+//2 pointers
+class Solution {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        int ans = 0;
+        for (int i = 0, j = 0; i < houses.length; i++) {
+            int curDistance = Math.abs(houses[i] - heaters[j]);
+            while (j < heaters.length - 1 && Math.abs(houses[i] - heaters[j]) >= Math.abs(houses[i] - heaters[j + 1])) {
+                j++;
+                curDistance = Math.min(curDistance, Math.abs(houses[i] - heaters[j]));
+            }
+            ans = Math.max(ans, curDistance);
+        }
+        return ans;
+    }
+}
