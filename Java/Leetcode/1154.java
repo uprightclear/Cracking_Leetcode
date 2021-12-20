76a24c52c4227a96e9de9e58370c65ad0f4f66ae
uprightclear <uprightclear@gmail.com>
2021-12-20 10:45:14 -0800

    20211220 update

---
+++ Java/Leetcode/1154.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int dayOfYear(String date) {
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8));
+
+        int[] amount = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        //四年一闰，百年不闰，四百年再闰
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            ++amount[1];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < month - 1; ++i) {
+            ans += amount[i];
+        }
+        return ans + day;
+    }
+}
