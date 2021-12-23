c4ac1d05901067270a8865497b5dcc1591593219
uprightclear <uprightclear@gmail.com>
2021-12-22 16:27:13 -0800

    20211222 update

---
+++ Java/Leetcode/992.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] A, int K) {
+        return atMostK(A, K) - atMostK(A, K - 1);
+    }
+    int atMostK(int[] A, int K) {
+        int i = 0, res = 0;
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int j = 0; j < A.length; ++j) {//ending point
+            if (count.getOrDefault(A[j], 0) == 0) K--;
+            count.put(A[j], count.getOrDefault(A[j], 0) + 1);
+            while (K < 0) {
+                count.put(A[i], count.get(A[i]) - 1);
+                if (count.get(A[i]) == 0) K++;
+                i++;
+            }
+            res += j - i + 1;
+        }
+        return res;
+    }
+}
