fc65f7267abfc157f12dd986fbfbf3ba2cb78c40
uprightclear <uprightclear@gmail.com>
2021-12-26 11:14:51 -0800

    20211226 update

--- Java/Leetcode/825.java
+++ Java/Leetcode/825.java
@@ -18,3 +18,25 @@ class Solution {
         return ans;
     }
 }
+
+
+class Solution {
+    public int numFriendRequests(int[] ages) {
+        int[] cnt = new int[121];
+        for (int age : ages) {
+            ++cnt[age];
+        }
+        int[] pre = new int[121];
+        for (int i = 1; i <= 120; ++i) {
+            pre[i] = pre[i - 1] + cnt[i];
+        }
+        int ans = 0;
+        for (int i = 15; i <= 120; ++i) {
+            if (cnt[i] > 0) {
+                int bound = (int) (i * 0.5 + 8);
+                ans += cnt[i] * (pre[i] - pre[bound - 1] - 1);
+            }
+        }
+        return ans;
+    }
+}
