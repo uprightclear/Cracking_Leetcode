a4ae7d5ded70b68c7d59ea7c85f00a0c53ad265b
uprightclear <uprightclear@gmail.com>
2021-12-26 11:11:50 -0800

    20211226 update

---
+++ Java/Leetcode/825.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numFriendRequests(int[] ages) {
+        int n = ages.length;
+        Arrays.sort(ages);
+        int left = 0, right = 0, ans = 0;
+        for (int age : ages) {
+            if (age < 15) {
+                continue;
+            }
+            while (ages[left] <= 0.5 * age + 7) {
+                ++left;
+            }
+            while (right + 1 < n && ages[right + 1] <= age) {
+                ++right;
+            }
+            ans += right - left;
+        }
+        return ans;
+    }
+}
