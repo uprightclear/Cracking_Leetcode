dd987ac926bdf2684e07343ce981de4646d41400
uprightclear <uprightclear@gmail.com>
2021-12-28 12:16:38 -0800

    20211228 update

--- Java/Leetcode/1995.java
+++ Java/Leetcode/1995.java
@@ -1,3 +1,42 @@
+//o(n^4)
+class Solution {
+    public int countQuadruplets(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for (int a = 0; a < n; ++a) {
+            for (int b = a + 1; b < n; ++b) {
+                for (int c = b + 1; c < n; ++c) {
+                    for (int d = c + 1; d < n; ++d) {
+                        if (nums[a] + nums[b] + nums[c] == nums[d]) {
+                            ++ans;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+//o(n^3)
+class Solution {
+    public int countQuadruplets(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+        for (int c = n - 2; c >= 2; --c) {
+            cnt.put(nums[c + 1], cnt.getOrDefault(nums[c + 1], 0) + 1);
+            for (int a = 0; a < c; ++a) {
+                for (int b = a + 1; b < c; ++b) {
+                    ans += cnt.getOrDefault(nums[a] + nums[b] + nums[c], 0);
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+//o(n^2)
 class Solution {
     public int countQuadruplets(int[] nums) {
         int n = nums.length;
