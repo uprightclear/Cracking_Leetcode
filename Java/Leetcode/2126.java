1a1059698646916bd450e59db75b769305f226f8
uprightclear <uprightclear@gmail.com>
2022-01-02 15:42:47 -0800

    20220102 update

---
+++ Java/Leetcode/2126.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long m = mass;
+        for(int a : asteroids) {
+            if(m >= a) m += a; 
+            else return false;
+        }
+        return true;
+    }
+}
