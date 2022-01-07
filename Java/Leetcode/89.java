0a7f1a1856f409e68927032b84ac30f411e9f653
uprightclear <uprightclear@gmail.com>
2022-01-07 08:27:02 -0800

    20220107 update

---
+++ Java/Leetcode/89.java
@@ -0,0 +1,14 @@
+//https://leetcode-cn.com/problems/gray-code/solution/ge-lei-bian-ma-by-leetcode-solution-cqi7/
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> ret = new ArrayList<Integer>();
+        ret.add(0);
+        for (int i = 1; i <= n; i++) {
+            int m = ret.size();
+            for (int j = m - 1; j >= 0; j--) {
+                ret.add(ret.get(j) | (1 << (i - 1)));
+            }
+        }
+        return ret;
+    }
+}
