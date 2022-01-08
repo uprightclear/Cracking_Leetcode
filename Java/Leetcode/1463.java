b3266d4193a6840612752689e0825cf9094eb2b2
uprightclear <uprightclear@gmail.com>
2022-01-07 16:24:43 -0800

    20220107 update

---
+++ Java/Leetcode/1463.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int cherryPickup(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][][] dpCache = new int[m][n][n];
+        // initial all elements to -1 to mark unseen
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    dpCache[i][j][k] = -1;
+                }
+            }
+        }
+        return dp(0, 0, n - 1, grid, dpCache);
+    }
+
+    private int dp(int row, int col1, int col2, int[][] grid, int[][][] dpCache) {
+        if (col1 < 0 || col1 >= grid[0].length || col2 < 0 || col2 >= grid[0].length) {
+            return 0;
+        }
+        // check cache
+        if (dpCache[row][col1][col2] != -1) {
+            return dpCache[row][col1][col2];
+        }
+        // current cell
+        int result = 0;
+        result += grid[row][col1];
+        if (col1 != col2) {
+            result += grid[row][col2];
+        }
+        // transition
+        if (row != grid.length - 1) {
+            int max = 0;
+            for (int newCol1 = col1 - 1; newCol1 <= col1 + 1; newCol1++) {
+                for (int newCol2 = col2 - 1; newCol2 <= col2 + 1; newCol2++) {
+                    max = Math.max(max, dp(row + 1, newCol1, newCol2, grid, dpCache));
+                }
+            }
+            result += max;
+        }
+
+        dpCache[row][col1][col2] = result;
+        return result;
+    }
+}
