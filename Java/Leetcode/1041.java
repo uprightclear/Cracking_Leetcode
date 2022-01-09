d7105fe96d932c2994ac0c0bc16f9bba9505b971
uprightclear <uprightclear@gmail.com>
2022-01-08 21:43:10 -0800

    20220108 update

--- Java/Leetcode/1041.java
+++ Java/Leetcode/1041.java
@@ -19,3 +19,31 @@ class Solution {
         return (x == 0 && y == 0) || dir != 0;
     }
 }
+
+//with comment
+class Solution {
+    public boolean isRobotBounded(String instructions) {
+        // north = 0, east = 1, south = 2, west = 3
+        int[][] directions = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        // Initial position is in the center
+        int x = 0, y = 0;
+        // facing north
+        int idx = 0;
+        
+        for (char i : instructions.toCharArray()) {
+            if (i == 'L')
+                idx = (idx + 3) % 4;
+            else if (i == 'R')
+                idx = (idx + 1) % 4;
+            else {
+                x += directions[idx][0];
+                y += directions[idx][1];   
+            }    
+        }
+        
+        // after one cycle:
+        // robot returns into initial position
+        // or robot doesn't face north
+        return (x == 0 && y == 0) || (idx != 0);
+    }
+}
