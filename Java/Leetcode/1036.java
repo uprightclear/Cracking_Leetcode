d3e5ed81b213e397445425932fea42c6c550a184
uprightclear <uprightclear@gmail.com>
2022-01-10 12:39:39 -0800

    20220110 update

---
+++ Java/Leetcode/1036.java
@@ -0,0 +1,87 @@
+class Solution {
+    // 在包围圈中
+    static final int BLOCKED = -1;
+    // 不在包围圈中
+    static final int VALID = 0;
+    // 无论在不在包围圈中，但在 n(n-1)/2 步搜索的过程中经过了 target
+    static final int FOUND = 1;
+    
+    static final int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    static final int BOUNDARY = 1000000;
+
+    public boolean isEscapePossible(int[][] blocked, int[] source, int[] target) {
+        if (blocked.length < 2) {
+            return true;
+        }
+
+        Set<Pair> hashBlocked = new HashSet<Pair>();
+        for (int[] pos : blocked) {
+            hashBlocked.add(new Pair(pos[0], pos[1]));
+        }
+
+        int result = check(blocked, source, target, hashBlocked);
+        if (result == FOUND) {
+            return true;
+        } else if (result == BLOCKED) {
+            return false;
+        } else {
+            result = check(blocked, target, source, hashBlocked);
+            return result != BLOCKED;
+        }
+    }
+
+    public int check(int[][] blocked, int[] start, int[] finish, Set<Pair> hashBlocked) {
+        int sx = start[0], sy = start[1];
+        int fx = finish[0], fy = finish[1];
+        int countdown = blocked.length * (blocked.length - 1) / 2;
+        Pair startPair = new Pair(sx, sy);
+        Queue<Pair> queue = new ArrayDeque<Pair>();
+        queue.offer(startPair);
+        Set<Pair> visited = new HashSet<Pair>();
+        visited.add(startPair);
+        while (!queue.isEmpty() && countdown > 0) {
+            Pair pair = queue.poll();
+            int x = pair.x, y = pair.y;
+            for (int d = 0; d < 4; ++d) {
+                int nx = x + dirs[d][0], ny = y + dirs[d][1];
+                Pair newPair = new Pair(nx, ny);
+                if (nx >= 0 && nx < BOUNDARY && ny >= 0 && ny < BOUNDARY && !hashBlocked.contains(newPair) && !visited.contains(newPair)) {
+                    if (nx == fx && ny == fy) {
+                        return FOUND;
+                    }
+                    --countdown;
+                    queue.offer(newPair);
+                    visited.add(newPair);
+                }
+            }
+        }
+        if (countdown > 0) {
+            return BLOCKED;
+        }
+        return VALID;
+    }
+}
+
+class Pair {
+    int x;
+    int y;
+
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) ((long) x << 20 | y);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Pair) {
+            Pair pair2 = (Pair) obj;
+            return x == pair2.x && y == pair2.y;
+        }
+        return false;
+    }
+}
