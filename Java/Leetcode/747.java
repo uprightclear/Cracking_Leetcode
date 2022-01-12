f96350ee3b9bf250720d2e77c0a181535940ee81
uprightclear <uprightclear@gmail.com>
2022-01-12 10:47:52 -0800

    20220112 update

---
+++ Java/Leetcode/747.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int dominantIndex(int[] nums) {
+        //m1: the biggest one
+        //m2: the second biggest one
+        int m1 = -1, m2 = -1;
+        int index = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > m1) {
+                m2 = m1;
+                m1 = nums[i];
+                index = i;
+            } else if (nums[i] > m2) {
+                m2 = nums[i];
+            }
+        }
+        return m1 >= m2 * 2 ? index : -1;
+    }
+}
