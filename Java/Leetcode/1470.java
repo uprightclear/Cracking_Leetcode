ed156779497426ef4bd386a62426ab21b87c2f27
uprightclear <uprightclear@gmail.com>
2022-01-12 15:30:37 -0800

    20220112 update

---
+++ Java/Leetcode/1470.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] res = new int[2 * n];
+        for (int i = 0, j = n, idx = 0; idx < res.length; i++, j++) {
+            res[idx++] = nums[i];
+            res[idx++] = nums[j];
+        } 
+        return res;
+    }
+}
