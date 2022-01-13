a036d43d8e295851195bdc7cb9815ce684055ff3
uprightclear <uprightclear@gmail.com>
2022-01-13 10:46:33 -0800

    20220113 update

---
+++ Java/Leetcode/373.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(k, (o1, o2)->{
+            return nums1[o1[0]] + nums2[o1[1]] - nums1[o2[0]] - nums2[o2[1]];
+        });
+        List<List<Integer>> ans = new ArrayList<>();
+        int m = nums1.length;
+        int n = nums2.length;
+        for (int i = 0; i < Math.min(m, k); i++) {
+            pq.offer(new int[]{i,0});
+        }
+        while (k-- > 0 && !pq.isEmpty()) {
+            int[] idxPair = pq.poll();
+            List<Integer> list = new ArrayList<>();
+            list.add(nums1[idxPair[0]]);
+            list.add(nums2[idxPair[1]]);
+            ans.add(list);
+            if (idxPair[1] + 1 < n) {
+                pq.offer(new int[]{idxPair[0], idxPair[1] + 1});
+            }
+        }
+        
+        return ans;
+    }
+}
