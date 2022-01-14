5effc3f503290e3dac67e4b823336d1726557cd3
uprightclear <uprightclear@gmail.com>
2022-01-14 11:49:01 -0800

    20220114 update

--- Java/Leetcode/1716.java
+++ Java/Leetcode/1716.java
@@ -13,3 +13,20 @@ class Solution {
         return res;
     }
 }
+
+
+class Solution {
+    public int totalMoney(int n) {
+        // 所有完整的周存的钱
+        int weekNumber = n / 7;
+        int firstWeekMoney = (1 + 7) * 7 / 2;
+        int lastWeekMoney = firstWeekMoney + 7 * (weekNumber - 1);
+        int weekMoney = (firstWeekMoney + lastWeekMoney) * weekNumber / 2;
+        // 剩下的不能构成一个完整的周的天数里存的钱
+        int dayNumber = n % 7;
+        int firstDayMoney = 1 + weekNumber;
+        int lastDayMoney = firstDayMoney + dayNumber - 1;
+        int dayMoney = (firstDayMoney + lastDayMoney) * dayNumber / 2;
+        return weekMoney + dayMoney;
+    }
+}
