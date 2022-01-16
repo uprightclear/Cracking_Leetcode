7b48d8d9a4e66af1f7c7119f02e90da71f7507fc
uprightclear <uprightclear@gmail.com>
2022-01-15 16:37:33 -0800

    20220115 update

--- Java/Leetcode/849.java
+++ Java/Leetcode/849.java
@@ -1,22 +1,21 @@
 class Solution {
     public int maxDistToClosest(int[] seats) {
-        int N = seats.length;
+        int len = seats.length;
+        //prev: the prev person
+        //future: the next person
         int prev = -1, future = 0;
         int ans = 0;
-
-        for (int i = 0; i < N; ++i) {
-            if (seats[i] == 1) {
-                prev = i;
-            } else {
-                while (future < N && seats[future] == 0 || future < i)
-                    future++;
-
-                int left = prev == -1 ? N : i - prev;
-                int right = future == N ? N : future - i;
+        
+        for(int i = 0; i < len; i++) {
+            if(seats[i] == 1) prev = i;
+            else {
+                while(future < len && seats[future] == 0 || future < i) future++;
+                
+                int left = prev == -1 ? len : i - prev;
+                int right = future == len ? len : future - i;
                 ans = Math.max(ans, Math.min(left, right));
             }
         }
-
         return ans;
     }
 }
