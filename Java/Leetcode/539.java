7248790b93e5a30a9f68b59f8433218c033dfc95
uprightclear <uprightclear@gmail.com>
2022-01-17 10:51:22 -0800

    20220117 update

---
+++ Java/Leetcode/539.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMinDifference(List<String> timePoints) {
+        Collections.sort(timePoints);
+        int ans = Integer.MAX_VALUE;
+        int t0Minutes = getMinutes(timePoints.get(0));
+        int preMinutes = t0Minutes;
+        for (int i = 1; i < timePoints.size(); ++i) {
+            int minutes = getMinutes(timePoints.get(i));
+            ans = Math.min(ans, minutes - preMinutes); // 相邻时间的时间差
+            preMinutes = minutes;
+        }
+        ans = Math.min(ans, t0Minutes + 1440 - preMinutes); // 首尾时间的时间差
+        return ans;
+    }
+
+    public int getMinutes(String t) {
+        return ((t.charAt(0) - '0') * 10 + (t.charAt(1) - '0')) * 60 + (t.charAt(3) - '0') * 10 + (t.charAt(4) - '0');
+    }
+}
