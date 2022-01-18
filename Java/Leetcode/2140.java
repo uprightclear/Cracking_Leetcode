f88535ba05b303325f4dd045e428930c5738f4db
uprightclear <uprightclear@gmail.com>
2022-01-17 16:07:38 -0800

    20220117 update

--- Java/Leetcode/2140.java
+++ Java/Leetcode/2140.java
@@ -1,4 +1,18 @@
 class Solution {
+    long dp[];
+    public long mostPoints(int[][] questions) {
+        dp = new long[questions.length];
+        return dfs(questions, 0);
+    }
+    public long dfs(int[][] Q, int i) {
+        if (i >= Q.length) return 0;
+        if (dp[i] > 0) return dp[i];
+        int points = Q[i][0], jump = Q[i][1];
+        return dp[i] = Math.max(dfs(Q, i + 1), points + dfs(Q, i + jump + 1));
+    }
+}
+
+class Solution {
     public long mostPoints(int[][] questions) {
         //f[i]: if solve question i, the max score can we get
         //f[i] = max(f[j]) + score[i] (forbid[j] + j < i)
