e69ea56ea37ca99b04401dd39d978d809e99fe36
uprightclear <uprightclear@gmail.com>
2022-01-18 13:45:27 -0800

    20220118 update

--- Java/Leetcode/69.java
+++ Java/Leetcode/69.java
@@ -1,3 +1,4 @@
+//Math
 class Solution {
     public int mySqrt(int x) {
       if (x < 2) return x;
@@ -7,3 +8,22 @@ class Solution {
       return (long)right * right > x ? left : right;
     }
   }
+
+//BinarySearch
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+
+        long num;
+        int pivot, left = 2, right = x / 2;
+        while (left <= right) {
+            pivot = left + (right - left) / 2;
+            num = (long)pivot * pivot;
+            if (num > x) right = pivot - 1;
+            else if (num < x) left = pivot + 1;
+            else return pivot;
+        }
+
+        return right;
+    }
+}
