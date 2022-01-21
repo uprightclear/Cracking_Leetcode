fd0eb94abb0c87fcb6e89dbcfbf5fe15de1b3670
uprightclear <uprightclear@gmail.com>
2022-01-20 19:26:44 -0800

    20220120 update

--- Java/Leetcode/1937.java
+++ Java/Leetcode/1937.java
@@ -2,8 +2,8 @@ class Solution {
     //dp[i][j] =  points[i][j] + max{dp[i-1][k] - abs(j -k)}
     //dp[i][j]指取(i,j)下标的元素能达到的最大值
     //优化后变成
-    //k<=j k在j左边 dp[i][j] =  points[i][j] - j + max{dp[i-1][k] + k}
-    //k> j k在j右边 dp[i][j] =  points[i][j] + j + max{dp[i-1][k] - k}
+    //k <= j k在j左边 dp[i][j] =  points[i][j] - j + max{dp[i-1][k] + k}
+    //k > j k在j右边 dp[i][j] =  points[i][j] + j + max{dp[i-1][k] - k}
     public long maxPoints(int[][] points) {
         int m = points.length;
         int n = points[0].length;
@@ -21,7 +21,7 @@ class Solution {
             best = Long.MIN_VALUE;
             //倒序遍历
             //为何要倒序，因为这时是k在j右边的情况，那么max{dp[i-1][k] - k}中的k可取[j,n-1]
-            for(int j = n-1;j>=0;j--){
+            for(int j = n - 1;j >= 0;j--){
                 best = Math.max(best, pre[j] - j);//再倒序遍历，找上一行最大的dp[i-1][k]-k
                 cur[j] = Math.max(cur[j], best + point[j] + j);
             }
