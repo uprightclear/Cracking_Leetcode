da8938edd10b0cd4fb633b846c5f8def616b60e3
uprightclear <uprightclear@gmail.com>
2022-01-21 11:24:58 -0800

    20220121 update

---
+++ Java/Leetcode/1332.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int removePalindromeSub(String s) {
+        int n = s.length();
+        for (int i = 0; i < n / 2; ++i) {
+            if (s.charAt(i) != s.charAt(n - 1 - i)) {
+                return 2;
+            }
+        }
+        return 1;
+    }
+}
