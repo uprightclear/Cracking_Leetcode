07fde7e04dbe3374822a140c369befbd1a31b84d
uprightclear <uprightclear@gmail.com>
2022-01-21 17:29:25 -0800

    20220121 update

---
+++ Java/Leetcode/1510.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        boolean[] dp = new boolean[n + 1];
+        for (int i = 1; i <= n; ++i) {
+            for (int k = 1; k * k <= i; ++k) {
+                if (!dp[i - k * k]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
