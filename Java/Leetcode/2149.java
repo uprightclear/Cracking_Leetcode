0def7a7109cff3010fa8bdea3f81943ab4eab69d
uprightclear <uprightclear@gmail.com>
2022-01-24 18:39:30 -0800

    20220124 update

--- Java/Leetcode/2149.java
+++ Java/Leetcode/2149.java
@@ -15,3 +15,22 @@ class Solution {
         return ans;
     }
 }
+
+//2 pointers
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int [] out = new int [nums.length];
+        int j = 0;
+        int k = 1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] >= 0){
+                out[j] = nums[i];
+                j += 2;
+            }else {
+                out[k] = nums[i];
+                k += 2;
+            }
+        }
+        return out;
+    }
+}
