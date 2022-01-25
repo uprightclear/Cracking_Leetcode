fdda921dda494d43e4e4770b060fa8d9f31eb6d5
uprightclear <uprightclear@gmail.com>
2022-01-24 16:32:23 -0800

    20220124 update

---
+++ Java/Leetcode/941.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int len = arr.length;
+        int i = 0;
+
+        // walk up
+        while (i + 1 < len && arr[i] < arr[i + 1]) i++;
+
+        // peak can't be first or last
+        if (i == 0 || i == len - 1) return false;
+
+        // walk down
+        while (i + 1 < len && arr[i] > arr[i + 1]) i++;
+
+        return i == len - 1;
+    }
+}
