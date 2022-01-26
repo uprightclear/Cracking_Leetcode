de2499fd9a8ce63e6ac1a0d225b2c36a5ec179ef
uprightclear <uprightclear@gmail.com>
2022-01-26 12:49:27 -0800

    20220126 update

---
+++ Java/Leetcode/954.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canReorderDoubled(int[] A) {
+        Map<Integer, Integer> count = new TreeMap<>();
+        for (int a : A)
+            count.put(a, count.getOrDefault(a, 0) + 1);
+        for (int x : count.keySet()) {
+            if (count.get(x) == 0) continue;
+            int want = x < 0 ? x / 2 : x * 2;
+            if (x < 0 && x % 2 != 0 || count.get(x) > count.getOrDefault(want, 0))
+                return false;
+            count.put(want, count.get(want) - count.get(x));
+        }
+        return true;
+    }
+}
