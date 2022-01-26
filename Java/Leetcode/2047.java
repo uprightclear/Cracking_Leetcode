21b1558116e9c019206e3b2d3236c0614368a024
uprightclear <uprightclear@gmail.com>
2022-01-26 11:09:02 -0800

    20220126 update

---
+++ Java/Leetcode/2047.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countValidWords(String sentence) {
+        int ans = 0;
+        List<String> wordList = Arrays.asList(sentence.trim().split("\\s+"));
+        for(String s : wordList) {
+            if(isValid(s)) ans++;
+        }
+        return ans;
+    }
+
+    public boolean isValid(String word) {
+        int n = word.length();
+        boolean hasHyphens = false;
+        for (int i = 0; i < n; i++) {
+            if (Character.isDigit(word.charAt(i))) {
+                return false;
+            } else if (word.charAt(i) == '-') {
+                if (hasHyphens == true || i == 0 || i == n - 1 || !Character.isLetter(word.charAt(i - 1)) || !Character.isLetter(word.charAt(i + 1))) {
+                    return false;
+                }
+                hasHyphens = true;
+            } else if (word.charAt(i) == '!' || word.charAt(i) == '.' || word.charAt(i) == ',') {
+                if (i != n - 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
