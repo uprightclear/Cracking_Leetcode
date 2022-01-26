bd132cc61822d6aba500e344edb4be833ed2df84
uprightclear <uprightclear@gmail.com>
2022-01-25 16:16:41 -0800

    20220125 update

--- Java/Leetcode/1305.java
+++ Java/Leetcode/1305.java
@@ -8,18 +8,17 @@
  * }
  */
 class Solution {
-    public List<Integer> inorder(TreeNode root, List<Integer> arr) {
-      if (root == null) return arr;
-      inorder(root.left, arr);
-      arr.add(root.val);
-      inorder(root.right, arr);
-      return arr;
-    }
-  
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-      List<Integer> output = new ArrayList<>();
-      Stream.of(inorder(root1, new ArrayList()), inorder(root2, new ArrayList())).forEach(output::addAll);
-      Collections.sort(output);
-      return output;
+        List<Integer> ans = new ArrayList<>();
+        inorder(root1, ans);
+        inorder(root2, ans);
+        Collections.sort(ans); // Here it calls List.sort(object) method.
+        return ans;
     }
-  }
+    private void inorder(TreeNode n, List<Integer> ans) {
+        if (n == null) return;
+        inorder(n.left, ans);
+        ans.add(n.val);
+        inorder(n.right, ans);
+    }
+}
