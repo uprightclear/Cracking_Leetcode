e8488596f613a45a73527eff90ba17869ad759a7
uprightclear <uprightclear@gmail.com>
2022-01-25 20:33:29 -0800

    20220125 update

---
+++ Java/Leetcode/690.java
@@ -0,0 +1,27 @@
+/*
+// Definition for Employee.
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
+*/
+
+class Solution {
+    public int getImportance(List<Employee> employees, int id) {
+        Map<Integer, Employee> map = new HashMap<>();
+        for (Employee employee : employees) {
+            map.put(employee.id, employee);
+        }
+        return getImportanceHelper(map, id);
+    }
+    
+    private int getImportanceHelper(Map<Integer, Employee> map, int rootId) {
+        Employee root = map.get(rootId);
+        int total = root.importance;
+        for (int subordinate : root.subordinates) {
+            total += getImportanceHelper(map, subordinate);
+        }
+        return total;
+    }
+}
