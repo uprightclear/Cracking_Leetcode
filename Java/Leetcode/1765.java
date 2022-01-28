146252a2de420a7d093783ca651347f17e637f0d
uprightclear <uprightclear@gmail.com>
2022-01-28 10:48:15 -0800

    20220128 update

--- Java/Leetcode/1765.java
+++ Java/Leetcode/1765.java
@@ -3,19 +3,18 @@ class Solution {
     int m, n;
     int[][] ans;
     Queue<int[]> queue;
-    boolean[][] visited;
     public int[][] highestPeak(int[][] isWater) {
         m = isWater.length;
         n = isWater[0].length;
         ans = new int[m][n];
         queue = new LinkedList<>();
-        visited = new boolean[m][n];
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(isWater[i][j] == 1) {
                     ans[i][j] = 0;
-                    queue.offer(new int[]{i, j, 0});
-                    visited[i][j] = true;
+                    queue.offer(new int[]{i, j});
+                } else {
+                    ans[i][j] = -1;
                 }
             }
         }
@@ -23,14 +22,13 @@ class Solution {
             int size = queue.size();
             for(int i = 0; i < size; i++) {
                 int[] cur = queue.poll();
-                int x = cur[0], y = cur[1], v = cur[2];
+                int x = cur[0], y = cur[1];
                 for(int k = 0; k < 4; k++) {
                     int newx = x + directions[k][0];
                     int newy = y + directions[k][1];
-                    if(newx >= 0 && newx < m && newy >= 0 && newy < n && !visited[newx][newy]) {
-                        ans[newx][newy] = v + 1;
-                        queue.offer(new int[]{newx, newy, v + 1});
-                        visited[newx][newy] = true;
+                    if(newx >= 0 && newx < m && newy >= 0 && newy < n && ans[newx][newy] == -1) {
+                        ans[newx][newy] = ans[x][y] + 1;
+                        queue.offer(new int[]{newx, newy});
                     }
                 }
             }
