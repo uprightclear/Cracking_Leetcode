10d93a0b38ba6b5702bac287c6e5aa2614cb9248
uprightclear <uprightclear@gmail.com>
2022-01-29 09:56:52 -0800

    20220129 update

---
+++ Java/Leetcode/884.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String[] uncommonFromSentences(String s1, String s2) {
+        Map<String, Integer> freq = new HashMap<String, Integer>();
+        insert(s1, freq);
+        insert(s2, freq);
+
+        List<String> ans = new ArrayList<String>();
+        for (Map.Entry<String, Integer> entry : freq.entrySet()) {
+            if (entry.getValue() == 1) {
+                ans.add(entry.getKey());
+            }
+        }
+        return ans.toArray(new String[ans.size()]);
+    }
+
+    public void insert(String s, Map<String, Integer> freq) {
+        String[] arr = s.split(" ");
+        for (String word : arr) {
+            freq.put(word, freq.getOrDefault(word, 0) + 1);
+        }
+    }
+}
