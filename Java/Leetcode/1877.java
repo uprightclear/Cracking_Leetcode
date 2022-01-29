f68a488e419c8c47b88a1d21a1bdd54909ece8e3
uprightclear <uprightclear@gmail.com>
2022-01-28 21:27:26 -0800

    20220128 update

--- Java/Leetcode/1877.java
+++ Java/Leetcode/1877.java
@@ -9,3 +9,14 @@ class Solution {
         return arr[arr.length - 1];
     }
 }
+
+//more elegant
+class Solution {
+    public int minPairSum(int[] A) {
+        Arrays.sort(A);
+        int res = 0, n = A.length;
+        for (int i = 0; i < n / 2; ++i)
+            res = Math.max(res, A[i] + A[n - i - 1]);
+        return res;
+    }
+}
