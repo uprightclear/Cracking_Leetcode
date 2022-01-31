50c46c3f170a14d5be899484e6d44e0c35867cc7
uprightclear <uprightclear@gmail.com>
2022-01-30 16:55:34 -0800

    20220130 update

---
+++ Java/Leetcode/1672.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxWealthSoFar = 0;
+        for (int[] account : accounts) {
+            int currCustomerWealth = 0;
+            for (int money : account) currCustomerWealth += money;
+            maxWealthSoFar = Math.max(maxWealthSoFar, currCustomerWealth);
+        }
+        return maxWealthSoFar;
+    }
+}
