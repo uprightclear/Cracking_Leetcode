d4819deaba4dd2045bd76acb9bbf684ba7cb1e51
uprightclear <uprightclear@gmail.com>
2022-02-02 10:57:13 -0800

    20220202 update

---
+++ Java/Leetcode/1414.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMinFibonacciNumbers(int k) {
+        List<Integer> f = new ArrayList<Integer>();
+        f.add(1);
+        int a = 1, b = 1;
+        while (a + b <= k) {
+            int c = a + b;
+            f.add(c);
+            a = b;
+            b = c;
+        }
+        int ans = 0;
+        for (int i = f.size() - 1; i >= 0 && k > 0; i--) {
+            int num = f.get(i);
+            if (k >= num) {
+                k -= num;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
