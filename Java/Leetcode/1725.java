05bf63dea3751876e796578c983510642112fad2
uprightclear <uprightclear@gmail.com>
2022-02-03 09:55:45 -0800

    20220203 update

--- Java/Leetcode/1725.java
+++ Java/Leetcode/1725.java
@@ -16,3 +16,20 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public int countGoodRectangles(int[][] rectangles) {
+        int res = 0, maxLen = 0;
+        for (int[] rectangle : rectangles) {
+            int l = rectangle[0], w = rectangle[1];
+            int k = Math.min(l, w);
+            if (k == maxLen) {
+                ++res;
+            } else if (k > maxLen) {
+                res = 1;
+                maxLen = k;
+            }
+        }
+        return res;
+    }
+}
