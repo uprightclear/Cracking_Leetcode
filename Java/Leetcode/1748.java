cede0192e073512674bc77eae8e99a4dab8a1464
uprightclear <uprightclear@gmail.com>
2022-02-05 11:39:29 -0800

    20220205 update

--- Java/Leetcode/1748.java
+++ Java/Leetcode/1748.java
@@ -11,3 +11,20 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            cnt.put(num, cnt.getOrDefault(num, 0) + 1);
+        }
+        int ans = 0;
+        for (Map.Entry<Integer, Integer> entry : cnt.entrySet()) {
+            int num = entry.getKey(), c = entry.getValue();
+            if (c == 1) {
+                ans += num;
+            }
+        }
+        return ans;
+    }
+}
