c4d563a6dadf342efdfed83749574dbb969fc4cc
uprightclear <uprightclear@gmail.com>
2022-02-06 16:25:59 -0800

    20220206 update

--- Java/Leetcode/389.java
+++ Java/Leetcode/389.java
@@ -26,3 +26,23 @@ class Solution {
         return extraChar;
     }
 }
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+
+        // Initialize ch with 0, because 0 ^ X = X
+        // 0 when XORed with any bit would not change the bits value.
+        char ch = 0;
+
+        // XOR all the characters of both s and t.
+        for (int i = 0; i < s.length(); i += 1) {
+            ch ^= s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); i += 1) {
+            ch ^= t.charAt(i);
+        }
+
+        // What is left after XORing everything is the difference.
+        return ch;
+    }
+}
