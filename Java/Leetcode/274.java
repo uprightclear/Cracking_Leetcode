6afd4c62611b406e1e646910af96eb49dcb53aee
uprightclear <uprightclear@gmail.com>
2022-02-08 18:01:58 -0800

    20220208 update

---
+++ Java/Leetcode/274.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int h = 0, i = citations.length - 1; 
+        while (i >= 0 && citations[i] > h) {
+            h++; 
+            i--;
+        }
+        return h;
+    }
+}
