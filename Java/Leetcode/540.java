f17c1bbb7228160eba3c442850827e9e27db5255
uprightclear <uprightclear@gmail.com>
2022-02-13 11:40:20 -0800

    20220213 update

--- Java/Leetcode/540.java
+++ Java/Leetcode/540.java
@@ -7,3 +7,18 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (nums[mid] == nums[mid ^ 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+}
