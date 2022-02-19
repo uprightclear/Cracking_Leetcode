1581d2e291f7326fa420456b6f9fdc5c0a8b129e
uprightclear <uprightclear@gmail.com>
2022-02-19 11:32:38 -0800

    20220219 update

---
+++ Java/Leetcode/717.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        int n = bits.length, i = 0;
+        while (i < n - 1) {
+            i += bits[i] + 1;
+        }
+        return i == n - 1;
+    }
+}
