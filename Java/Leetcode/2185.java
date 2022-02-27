afa0be88fa3f871033c98b956ab95326d5ff6986
uprightclear <uprightclear@gmail.com>
2022-02-26 21:04:49 -0800

    20220226 update

---
+++ Java/Leetcode/2185.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int ans = 0;
+        for(String word : words) {
+            if(word.indexOf(pref) == 0) ans++;
+        }
+        return ans;
+    }
+}
