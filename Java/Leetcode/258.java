8d46242505a9613ddf6f43c00ec69928c4bba73b
uprightclear <uprightclear@gmail.com>
2022-03-02 10:23:08 -0800

    20220302 update

--- Java/Leetcode/258.java
+++ Java/Leetcode/258.java
@@ -5,3 +5,17 @@ class Solution {
         return num % 9;
     }
 }
+
+class Solution {
+    public int addDigits(int num) {
+        while(num / 10 > 0) {
+            int tmp = 0;
+            while(num > 0) {
+                tmp += num % 10;
+                num /= 10;
+            }
+            num = tmp;
+        }
+        return num;
+    }
+}
