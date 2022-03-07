fef8b71c277fba383bd4c25da0994dc8915eadfb
uprightclear <uprightclear@gmail.com>
2022-03-07 15:34:31 -0800

    20220307 update

--- Java/Leetcode/261.java
+++ Java/Leetcode/261.java
@@ -1,3 +1,4 @@
+//Union Find
 class Solution {
     public boolean validTree(int n, int[][] edges) {
         int[] set = new int[n];
@@ -18,5 +19,42 @@ class Solution {
     private int find(int[] set, int i) {
         if(set[i] == -1) return i;
         return find(set, set[i]);
+    }
+}
+
+//DFS
+class Solution {
+    
+    private Map<Integer, Set<Integer>> adjList = new HashMap<>();
+    private Set<Integer> seen = new HashSet<>();
+    
+    
+    public boolean validTree(int n, int[][] edges) {
+        
+        if (edges.length != n - 1) return false;
+        
+        for (int i = 0; i < n; i++) {
+            adjList.put(i, new HashSet<>());
+        }
+        for (int[] edge : edges) {
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
+        }
+        
+        // We return true iff no cycles were detected,
+        // AND the entire graph has been reached.
+        return dfs(0, -1) && seen.size() == n;   
+    }
+    
+    public boolean dfs(int node, int parent) {
+        if (seen.contains(node)) return false;
+        seen.add(node);
+        for (int neighbour : adjList.get(node)) {
+            if (parent != neighbour) {
+                boolean result = dfs(neighbour, node);
+                if(!result) return false;
+            }
+        }
+        return true;
     }
 }
