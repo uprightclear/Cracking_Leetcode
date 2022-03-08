9971e5925938cd507354d1ac02ce539d7d4684a5
uprightclear <uprightclear@gmail.com>
2022-03-08 15:34:59 -0800

    20220308 update

--- Java/Leetcode/300.java
+++ Java/Leetcode/300.java
@@ -2,15 +2,12 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int len = nums.length;
         int[] dp = new int[len];
-        dp[0] = 1;
+        Arrays.fill(dp, 1);
         int res = 1;
-        for(int i = 1; i < len; i++){
-            for(int j = i - 1; j >= 0; j--){
-                if(nums[j] < nums[i]){
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+        for(int i = 1; i < len; i++) {
+            for(int j = i - 1; j >= 0; j--) {
+                if(nums[i] > nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
             }
-            if(dp[i] == 0) dp[i] = 1;
             res = Math.max(res, dp[i]);
         }
         return res;
