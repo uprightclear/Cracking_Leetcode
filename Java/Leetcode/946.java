a8d355f4ec578ca33f077e27d93c45af42803180
uprightclear <uprightclear@gmail.com>
2022-03-16 00:47:47 -0700

    20220316 update

---
+++ Java/Leetcode/946.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        for (int x : pushed) {
+            stack.push(x);
+            while (!stack.empty() && stack.peek() == popped[i]) {
+                stack.pop();
+                i++;
+            }
+        }
+        return stack.empty();
+    }
+}
