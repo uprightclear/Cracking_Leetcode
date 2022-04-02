7e7466450c7a12bce892347e35c5cac49d92d790
uprightclear <uprightclear@gmail.com>
2022-04-02 10:37:55 -0700

    20220402 update

---
+++ Java/Leetcode/744.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int length = letters.length;
+        if (target >= letters[length - 1]) {
+            return letters[0];
+        }
+        int low = 0, high = length - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (letters[mid] > target) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return letters[low];
+    }
+}
