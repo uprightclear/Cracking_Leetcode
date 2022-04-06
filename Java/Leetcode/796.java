483991625c092fbd040a0baa41f45acd58f853a4
uprightclear <uprightclear@gmail.com>
2022-04-06 10:01:51 -0700

    20220406 update

---
+++ Java/Leetcode/796.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        int n = s.length();
+        for(int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            if(sb.append(s.substring(i)).append(s.substring(0, i)).toString().equals(goal)) return true;
+        }
+        return false;
+    }
+}
