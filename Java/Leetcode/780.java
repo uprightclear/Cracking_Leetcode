43ea9988f2f865e8d0e703b8724a931ab391205e
uprightclear <uprightclear@gmail.com>
2022-04-08 10:44:39 -0700

    20220408 update

--- Java/Leetcode/780.java
+++ Java/Leetcode/780.java
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/reaching-points/discuss/375429/Detailed-explanation.-or-full-through-process-or-Java-100-beat
 class Solution {
     public boolean reachingPoints(int sx, int sy, int tx, int ty) {
         while (tx >= sx && ty >= sy) {
@@ -11,5 +12,15 @@ class Solution {
             }
         }
         return (tx == sx && ty == sy);
+    }
+}
+
+
+//TLE from top to bottom
+class Solution {
+    public static boolean reachingPoints(int sx, int sy, int tx, int ty) {
+        if (sx == tx && sy == ty) return true;
+        if (sx > tx || sy > ty) return false;
+        return (reachingPoints(sx + sy, sy, tx, ty) || reachingPoints(sx, sx + sy, tx, ty));
     }
 }
