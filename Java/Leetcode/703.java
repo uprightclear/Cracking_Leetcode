c148a41614005808193d2719c4c0c02c921b7b99
uprightclear <uprightclear@gmail.com>
2022-04-07 17:26:50 -0700

    20220407 update

---
+++ Java/Leetcode/703.java
@@ -0,0 +1,27 @@
+class KthLargest {
+    final PriorityQueue<Integer> q;
+    final int k;
+
+    public KthLargest(int k, int[] a) {
+        this.k = k;
+        q = new PriorityQueue<>(k);
+        for (int n : a)
+            add(n);
+    }
+
+    public int add(int n) {
+        if (q.size() < k)
+            q.offer(n);
+        else if (q.peek() < n) {
+            q.poll();
+            q.offer(n);
+        }
+        return q.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
