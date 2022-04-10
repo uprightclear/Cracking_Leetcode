859e1b268097ff0b7ba383742aab5a963eb40a59
uprightclear <uprightclear@gmail.com>
2022-04-10 11:24:31 -0700

    20220410 update

---
+++ Java/Leetcode/357.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 10;
+        }
+        int res = 10, cur = 9;
+        for (int i = 0; i < n - 1; i++) {
+            cur *= 9 - i;
+            res += cur;
+        }
+        return res;
+    }
+}
