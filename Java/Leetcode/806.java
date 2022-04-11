a26b1a2e4aea46b7e79a577c86ce23593578c687
uprightclear <uprightclear@gmail.com>
2022-04-11 10:43:22 -0700

    20220411 update

---
+++ Java/Leetcode/806.java
@@ -0,0 +1,16 @@
+class Solution {
+    static final int MAX_WIDTH = 100;
+    public int[] numberOfLines(int[] widths, String s) {
+        int lines = 1;
+        int width = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int need = widths[s.charAt(i) - 'a'];
+            width += need;
+            if (width > MAX_WIDTH) {
+                lines++;
+                width = need;
+            }
+        }
+        return new int[]{lines, width};
+    }
+}
