7b2a7cf84cecab617d561f9d2c7d72f00fbb0404
uprightclear <uprightclear@gmail.com>
2022-04-15 17:30:32 -0700

    20220415 update

---
+++ Java/Leetcode/538.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        convert(root);
+        return root;
+    }
+
+    public void convert(TreeNode cur) {
+        if (cur == null) return;
+        convert(cur.right);
+        cur.val += sum;
+        sum = cur.val;
+        convert(cur.left);
+    }
+}
