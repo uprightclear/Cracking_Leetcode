4212fdf5b3e1fef5264037f12fc59753ede33903
uprightclear <uprightclear@gmail.com>
2022-04-19 09:50:54 -0700

    20220419 update

--- Java/Leetcode/388.java
+++ Java/Leetcode/388.java
@@ -1,7 +1,7 @@
 class Solution {
     public int lengthLongestPath(String input) {
         String[] paths = input.split("\n");
-        int[] stack = new int[paths.length+1];
+        int[] stack = new int[paths.length + 1];
         int maxLen = 0;
         for(String s : paths){
             int lev = s.lastIndexOf("\t") + 1, curLen = stack[lev + 1] = stack[lev] + s.length() - lev + 1;
