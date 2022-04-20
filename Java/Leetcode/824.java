38d286dcc1bf2950ebf289552dfdf8ecaa5622eb
uprightclear <uprightclear@gmail.com>
2022-04-20 10:49:24 -0700

    20220420 update

---
+++ Java/Leetcode/824.java
@@ -0,0 +1,46 @@
+class Solution {
+    public String toGoatLatin(String sentence) {
+        Set<Character> vowels = new HashSet<Character>() {{
+            add('a');
+            add('e');
+            add('i');
+            add('o');
+            add('u');
+            add('A');
+            add('E');
+            add('I');
+            add('O');
+            add('U');
+        }};
+
+        int n = sentence.length();
+        int i = 0, cnt = 1;
+        StringBuffer ans = new StringBuffer();
+
+        while (i < n) {
+            int j = i;
+            while (j < n && sentence.charAt(j) != ' ') {
+                ++j;
+            }
+
+            ++cnt;
+            if (cnt != 2) {
+                ans.append(' ');
+            }
+            if (vowels.contains(sentence.charAt(i))) {
+                ans.append(sentence.substring(i, j));
+            } else {
+                ans.append(sentence.substring(i + 1, j));
+                ans.append(sentence.charAt(i));
+            }
+            ans.append('m');
+            for (int k = 0; k < cnt; ++k) {
+                ans.append('a');
+            }
+
+            i = j + 1;
+        }
+
+        return ans.toString();
+    }
+}
