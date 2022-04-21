affcac4cc0ddcd01ae9d8be6ef45729ba06df59c
uprightclear <uprightclear@gmail.com>
2022-04-21 10:08:10 -0700

    20220421 update

---
+++ Java/Leetcode/396.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int f = 0, n = nums.length, numSum = Arrays.stream(nums).sum();
+        for (int i = 0; i < n; i++) {
+            f += i * nums[i];
+        }
+        int res = f;
+        for (int i = n - 1; i > 0; i--) {
+            f += numSum - n * nums[i];
+            res = Math.max(res, f);
+        }
+        return res;
+    }
+}
