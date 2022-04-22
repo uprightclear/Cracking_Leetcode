4e70025001317962493c8bce3bedf28a745182e0
uprightclear <uprightclear@gmail.com>
2022-04-21 20:46:08 -0700

    20220421 update

--- Java/Leetcode/1166.java
+++ Java/Leetcode/1166.java
@@ -24,3 +24,68 @@ class FileSystem {
  * boolean param_1 = obj.createPath(path,value);
  * int param_2 = obj.get(path);
  */
+
+
+
+class FileSystem {
+    class File{
+        String name;
+        int val = -1;
+        Map<String, File> map = new HashMap<>();
+        
+        File(String name){
+            this.name = name;
+        }
+    }
+    File root;
+    public FileSystem() {
+        root = new File("");
+    }
+    
+    public boolean create(String path, int value) {
+        String[] array = path.split("/");
+        File cur = root;
+        
+        for(int i=1;i<array.length;i++){
+            String cur_name = array[i];
+            if(cur.map.containsKey(cur_name)==false){
+                if(i==array.length-1){
+                    cur.map.put(cur_name, new File(cur_name));
+                }else{
+                    return false;
+                }
+            }
+            cur = cur.map.get(cur_name);
+        }
+        
+        if(cur.val!=-1){
+            return false;
+        }
+        
+        cur.val = value;
+        return true;
+    }
+    
+    public int get(String path) {
+        String[] array = path.split("/");
+        File cur = root;
+        for(int i=1;i<array.length;i++){
+            String cur_name = array[i];
+            if(cur.map.containsKey(cur_name)==false){
+                return -1;
+            }
+            cur = cur.map.get(cur_name);
+        }
+        
+        return cur.val;
+        
+        
+    }
+}
+
+/**
+ * Your FileSystem object will be instantiated and called as such:
+ * FileSystem obj = new FileSystem();
+ * boolean param_1 = obj.create(path,value);
+ * int param_2 = obj.get(path);
+ */
