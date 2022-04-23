7a484448d8ad316fa20f08a9296ae47b7999078a
uprightclear <uprightclear@gmail.com>
2022-04-23 09:47:09 -0700

    20220423 update

---
+++ Java/Leetcode/868.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int binaryGap(int n) {
+        int last = -1, ans = 0;
+        for (int i = 0; n != 0; ++i) {
+            if ((n & 1) == 1) {
+                if (last != -1) {
+                    ans = Math.max(ans, i - last);
+                }
+                last = i;
+            }
+            n >>= 1;
+        }
+        return ans;
+    }
+}
