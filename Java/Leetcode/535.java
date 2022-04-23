43eeba535c70f9cdaaa5d3dd6c4372495888e9f3
uprightclear <uprightclear@gmail.com>
2022-04-22 18:53:27 -0700

    20220422 update

---
+++ Java/Leetcode/535.java
@@ -0,0 +1,18 @@
+public class Codec {
+    List<String> urls = new ArrayList<String>();
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        urls.add(longUrl);
+        return String.valueOf(urls.size() - 1);
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        int index = Integer.valueOf(shortUrl);
+        return (index < urls.size()) ? urls.get(index) : "";
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
