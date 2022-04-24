b9f4c8ca6e66227fd498dc80d20e88b1ad7059ec
uprightclear <uprightclear@gmail.com>
2022-04-24 10:17:12 -0700

    20220423 update

---
+++ Java/Leetcode/398.java
@@ -0,0 +1,22 @@
+class Solution {
+    Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+    Random rand = new Random();
+    public Solution(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            ArrayList<Integer> list = map.getOrDefault(nums[i], new ArrayList<Integer>());
+            list.add(i);
+            map.put(nums[i], list);
+        }
+    }
+    
+    public int pick(int target) {
+        List<Integer> list = map.get(target);
+        return list.get(rand.nextInt(list.size()));
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
