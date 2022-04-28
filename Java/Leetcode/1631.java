6d295d58b49d20feb024f38a15a709bc50e3c682
uprightclear <uprightclear@gmail.com>
2022-04-27 22:08:18 -0700

    20220427 update

---
+++ Java/Leetcode/1631.java
@@ -0,0 +1,28 @@
+class Solution {
+    int[] DIR = new int[]{0, 1, 0, -1, 0};
+    public int minimumEffortPath(int[][] heights) {
+        int m = heights.length, n = heights[0].length;
+        int[][] dist = new int[m][n];
+        for (int i = 0; i < m; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
+        
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        minHeap.offer(new int[]{0, 0, 0}); // distance, row, col
+        while (!minHeap.isEmpty()) {
+            int[] top = minHeap.poll();
+            int d = top[0], r = top[1], c = top[2];
+            if (d > dist[r][c]) continue;
+            if (r == m - 1 && c == n - 1) return d; // Reach to bottom right
+            for (int i = 0; i < 4; i++) {
+                int nr = r + DIR[i], nc = c + DIR[i + 1];
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
+                    int newDist = Math.max(d, Math.abs(heights[nr][nc] - heights[r][c]));
+                    if (dist[nr][nc] > newDist) {
+                        dist[nr][nc] = newDist;
+                        minHeap.offer(new int[]{dist[nr][nc], nr, nc});
+                    }
+                }
+            }
+        }
+        return 0; // Unreachable code, Java require to return interger value.
+    }
+}
