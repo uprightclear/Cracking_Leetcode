91cc839f45c34bc561ddf5cf8d69ee66dd0e8a2d
uprightclear <uprightclear@gmail.com>
2022-05-04 10:26:07 -0700

    20220504 update

--- Java/Leetcode/713.java
+++ Java/Leetcode/713.java
@@ -10,3 +10,16 @@ class Solution {
         return ans;
     }
 }
+
+
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int prod = 1, ans = 0, left = 0;
+        for(int right = 0; right < nums.length; right++) {
+            prod *= nums[right];
+            while(left <= right && prod >= k) prod /= nums[left++];
+            ans += right - left + 1;
+        }
+        return ans;
+    }
+}
