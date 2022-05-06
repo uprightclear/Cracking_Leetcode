2d6bd89762c39c54e688b2214d9df171029602e5
uprightclear <uprightclear@gmail.com>
2022-05-05 17:47:51 -0700

    20220505 update

---
+++ Java/Leetcode/1004.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int longestOnes(int[] A, int K) {
+        int i = 0, j;
+        for (j = 0; j < A.length; ++j) {
+            if (A[j] == 0) K--;
+            if (K < 0 && A[i++] == 0) K++;
+        }
+        return j - i;
+    }
+}
