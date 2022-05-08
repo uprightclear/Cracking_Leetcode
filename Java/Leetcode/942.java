47a2ccf7b11c802769407467449045ba2d1a4a12
uprightclear <uprightclear@gmail.com>
2022-05-08 10:57:35 -0700

    20220508 update

---
+++ Java/Leetcode/942.java
@@ -0,0 +1,12 @@
+//greedy
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n = s.length(), lo = 0, hi = n;
+        int[] perm = new int[n + 1];
+        for (int i = 0; i < n; ++i) {
+            perm[i] = s.charAt(i) == 'I' ? lo++ : hi--;
+        }
+        perm[n] = lo;
+        return perm;
+    }
+}
