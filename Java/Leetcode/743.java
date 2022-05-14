1bddbcf1969cca1edce38bbdf2d7221217cb5bcd
uprightclear <uprightclear@gmail.com>
2022-05-13 22:30:41 -0700

    20220513 update

--- Java/Leetcode/743.java
+++ Java/Leetcode/743.java
@@ -81,3 +81,38 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public int networkDelayTime(int[][] times, int N, int K) {
+        Map<Integer, Map<Integer,Integer>> map = new HashMap<>();
+        for(int[] time : times){
+            map.putIfAbsent(time[0], new HashMap<>());
+            map.get(time[0]).put(time[1], time[2]);
+        }
+        
+        //distance, node into pq
+        Queue<int[]> pq = new PriorityQueue<>((a,b) -> (a[0] - b[0]));
+        
+        pq.add(new int[]{0, K});
+        
+        boolean[] visited = new boolean[N+1];
+        int res = 0;
+        
+        while(!pq.isEmpty()){
+            int[] cur = pq.remove();
+            int curNode = cur[1];
+            int curDist = cur[0];
+            if(visited[curNode]) continue;
+            visited[curNode] = true;
+            res = curDist;
+            N--;
+            if(map.containsKey(curNode)){
+                for(int next : map.get(curNode).keySet()){
+                    pq.add(new int[]{curDist + map.get(curNode).get(next), next});
+                }
+            }
+        }
+        return N == 0 ? res : -1;
+            
+    }
+}
