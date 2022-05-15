1ccf45b1b903c5c93765da292cd32a85e90ab3b8
uprightclear <uprightclear@gmail.com>
2022-05-15 11:11:15 -0700

    20220515 update

--- Java/Leetcode/面试题04.06.后继者.java
+++ Java/Leetcode/面试题04.06.后继者.java
@@ -26,3 +26,26 @@ class Solution {
         return null;
     }
 }
+
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode successor = null;
+        if (p.right != null) {
+            successor = p.right;
+            while (successor.left != null) {
+                successor = successor.left;
+            }
+            return successor;
+        }
+        TreeNode node = root;
+        while (node != null) {
+            if (node.val > p.val) {
+                successor = node;
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return successor;
+    }
+}
