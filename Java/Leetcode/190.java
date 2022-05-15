c78856000576bcc77c1d3c03ab1846cc1bf18c71
uprightclear <uprightclear@gmail.com>
2022-05-15 16:20:17 -0700

    20220515 update

--- Java/Leetcode/190.java
+++ Java/Leetcode/190.java
@@ -24,3 +24,17 @@ public class Solution {
         return n >>> 16 | n << 16;
     }
 }
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        if(n == 0) return n;
+        int ans = 0;
+        for(int i = 0; i < 32; i++) {
+            ans <<= 1;
+            if((n & 1) == 1) ans++;
+            n >>= 1;
+        }
+        return ans;
+    }
+}
