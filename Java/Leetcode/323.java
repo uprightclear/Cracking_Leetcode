1b1a565229b97e1c2da1d5d9f1c4214070792dc5
uprightclear <uprightclear@gmail.com>
2022-05-15 23:18:11 -0700

    20220515 update

--- Java/Leetcode/323.java
+++ Java/Leetcode/323.java
@@ -172,3 +172,25 @@ class Solution {
         }
     }
 }
+
+//simplified union-find
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+        int components = n;
+        for (int[] e : edges) {
+            int p1 = findParent(parent, e[0]);
+            int p2 = findParent(parent, e[1]);
+            if (p1 != p2) {
+                parent[p1] = p2; // Union 2 component
+                components--;
+            }
+        }
+        return components;
+    }
+    private int findParent(int[] parent, int i) {
+        while (i != parent[i]) i = parent[i];
+        return i; // Without Path Compression
+    }
+}
