58bc238c44c39603b790e9e7451713f4783d1fa5
uprightclear <uprightclear@gmail.com>
2022-05-16 15:40:56 -0700

    20220516 update

---
+++ Java/Leetcode/271.java
@@ -0,0 +1,27 @@
+public class Codec {
+	// Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for(String s : strs) {
+            sb.append(s.length()).append('/').append(s);
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> ret = new ArrayList<String>();
+        int i = 0;
+        while(i < s.length()) {
+            int slash = s.indexOf('/', i);
+            int size = Integer.valueOf(s.substring(i, slash));
+            i = slash + size + 1;
+            ret.add(s.substring(slash + 1, i));
+        }
+        return ret;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
