ca136ed7d4a4ae0c31648b3981669aa0ff7ce26b
uprightclear <uprightclear@gmail.com>
2022-05-17 10:25:27 -0700

    20220517 update

--- Java/Leetcode/668.java
+++ Java/Leetcode/668.java
@@ -17,3 +17,23 @@ class Solution {
         return lo;
     }
 }
+
+
+class Solution {
+    public int findKthNumber(int m, int n, int k) {
+        int l = 1, r = m * n;
+        while(l < r){
+            int mid = l + r >> 1;
+            if(chk(mid, m, n, k)) r = mid;
+            else l = mid + 1;
+        }
+        return l;
+    }
+    public boolean chk(int mid, int m, int n, int k){
+        int cnt = 0;
+        for(int i = 1; i <= m; i++){
+            cnt += Math.min(mid / i,n);
+        }
+        return cnt >= k;
+    }
+}
