230dba057e4942aea21dfc2d4edff001df944260
uprightclear <uprightclear@gmail.com>
2022-05-18 16:45:08 -0700

    20220518 update

--- Java/Leetcode/676.java
+++ Java/Leetcode/676.java
@@ -40,3 +40,60 @@ class MagicDictionary {
  * obj.buildDict(dictionary);
  * boolean param_2 = obj.search(searchWord);
  */
+
+
+class MagicDictionary {
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isWord;
+        public TrieNode() {}
+    }
+    TrieNode root;
+    /** Initialize your data structure here. */
+    public MagicDictionary() {
+        root = new TrieNode();
+    }
+    
+    /** Build a dictionary through a list of words */
+    public void buildDict(String[] dict) {
+        for (String s : dict) {
+            TrieNode node = root;
+            for (char c : s.toCharArray()) {
+                if (node.children[c - 'a'] == null) {
+                    node.children[c - 'a'] = new TrieNode();
+                }
+                node = node.children[c - 'a'];
+            }
+            node.isWord = true;
+        }
+    }
+    
+    /** Returns if there is any word in the trie that equals to the given word after modifying exactly one character */
+    public boolean search(String word) {
+        char[] arr = word.toCharArray();
+        for (int i = 0; i < word.length(); i++) {
+            for (char c = 'a'; c <= 'z'; c++) {
+                if (arr[i] == c) {
+                    continue;
+                }
+                char org = arr[i];
+                arr[i] = c;
+                if (helper(new String(arr), root)) {
+                    return true;
+                }
+                arr[i] = org;
+            }
+        }
+        return false;
+    }
+    public boolean helper(String s, TrieNode root) {
+        TrieNode node = root;
+        for (char c : s.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return node.isWord;
+    }
+}
