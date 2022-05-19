8021ea0cc4a32583b135526d873c6e44faf1dc17
uprightclear <uprightclear@gmail.com>
2022-05-19 14:34:15 -0700

    20220519 update

--- Java/Leetcode/2110.java
+++ Java/Leetcode/2110.java
@@ -1,9 +1,9 @@
 class Solution {
     public long getDescentPeriods(int[] prices) {
         long res = 0;
-        for(int i = 0, j = 0; i < prices.length; i++) {
-            if(i > 0 && prices[i] != prices[i - 1] - 1) j = i;
-            res += i - j + 1;
+        for(int end = 0, start = 0; end < prices.length; end++) {
+            if(end > 0 && prices[end] != prices[end - 1] - 1) start = end;
+            res += end - start + 1;
         }
         return res;
     }
