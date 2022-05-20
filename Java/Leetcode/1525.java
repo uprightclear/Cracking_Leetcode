1278fa103d8d801958cb608ee478553b3fd2888d
uprightclear <uprightclear@gmail.com>
2022-05-19 21:36:05 -0700

    20220519 update

---
+++ Java/Leetcode/1525.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numSplits(String str) {
+        int l[] = new int[26], r[] = new int[26], d_l = 0, d_r = 0, res = 0;
+        var s = str.toCharArray();
+        for (char ch : s)
+            d_r += ++r[ch - 'a'] == 1 ? 1 : 0;
+        for (int i = 0; i < s.length; ++i) {
+            d_l += ++l[s[i] - 'a'] == 1 ? 1 : 0;
+            d_r -= --r[s[i] - 'a'] == 0 ? 1 : 0;
+            res += d_l == d_r ? 1 : 0;
+        }
+        return res;
+    }
+}
