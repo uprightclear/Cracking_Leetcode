d040f05101b9fe8533ad929d238bcc1005d91c8e
uprightclear <uprightclear@gmail.com>
2022-05-19 21:26:54 -0700

    20220519 update

---
+++ Java/Leetcode/362.java
@@ -0,0 +1,31 @@
+class HitCounter {
+    Queue<Integer> q;
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        q = new LinkedList<Integer>();
+    }
+
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        q.offer(timestamp);
+    }
+
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        while(!q.isEmpty() && timestamp - q.peek() >= 300) {
+            q.poll();
+        }
+        return q.size();
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
+
+
