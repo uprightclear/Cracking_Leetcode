d8e055c292ea3d554f580c51cff5b423aed925da
uprightclear <uprightclear@gmail.com>
2022-05-20 15:25:09 -0700

    20220520 update

--- Java/Leetcode/907.java
+++ Java/Leetcode/907.java
@@ -1,24 +1,35 @@
+//左右区间组合问题
 class Solution {
-    public int sumSubarrayMins(int[] A) {
-        long res = 0, mod = (int)1e9 + 7;
-        int n = A.length, left[] = new int[n], right[] = new int[n];
-        Stack<int[]> s1 = new Stack<>(), s2 = new Stack<>();
-        for (int i = 0; i < n; ++i) {
+    public int sumSubarrayMins(int[] arr) {
+        long res = 0, mod = (int) 1e9 + 7;
+        int n = arr.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Deque<int[]> s1 = new LinkedList<>();
+        Deque<int[]> s2 = new LinkedList<>();
+        
+        for(int i = 0; i < n; i++) {
             int count = 1;
-            while (!s1.isEmpty() && s1.peek()[0] > A[i])
+            while(!s1.isEmpty() && s1.peek()[0] > arr[i]) {
                 count += s1.pop()[1];
-            s1.push(new int[] {A[i], count});
+            }
+            s1.push(new int[]{arr[i], count});
             left[i] = count;
         }
-        for (int i = n - 1; i >= 0; --i) {
+        
+        for(int i = n - 1; i >= 0; i--) {
             int count = 1;
-            while (!s2.isEmpty() && s2.peek()[0] >= A[i])
+            while(!s2.isEmpty() && s2.peek()[0] >= arr[i]) {
                 count += s2.pop()[1];
-            s2.push(new int[] {A[i], count});
+            }
+            s2.push(new int[]{arr[i], count});
             right[i] = count;
         }
-        for (int i = 0; i < n; ++i)
-            res = (res + (long)A[i] * left[i] * right[i]) % mod;
+        
+        for(int i = 0; i < n; i++) {
+            res = (res + (long)arr[i] * left[i] * right[i]) % mod;
+        }
+        
         return (int)res;
     }
 }
