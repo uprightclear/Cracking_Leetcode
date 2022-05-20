2c584855a8919df16ae345c7a12fe94d56762d7a
uprightclear <uprightclear@gmail.com>
2022-05-20 15:35:24 -0700

    20220520 update

--- Java/Leetcode/937.java
+++ Java/Leetcode/937.java
@@ -39,3 +39,31 @@ class Solution {
         return logs;
     }
 }
+
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        Arrays.sort(logs, new Comparator<String>(){
+            public int compare(String log1, String log2) {
+                String[] split1 = log1.split(" ", 2);
+                String[] split2 = log2.split(" ", 2);
+                
+                boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+                boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+                
+                //if both of them are not digit, then sort them lexicographically
+                if(!isDigit1 && !isDigit2) {
+                    int cmp = split1[1].compareTo(split2[1]);
+                    if(cmp != 0) return cmp;
+                    return split1[0].compareTo(split2[0]);
+                }
+                
+                if(!isDigit1 && isDigit2) {
+                    return -1;
+                }else if(isDigit1 && !isDigit2) {
+                    return 1;
+                }else return 0;
+            }
+        });
+        return logs;
+    }
+}
