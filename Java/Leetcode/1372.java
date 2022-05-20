0580115848321476b8e4e07653c830fcfd9cf330
uprightclear <uprightclear@gmail.com>
2022-05-19 17:35:21 -0700

    20220519 update

---
+++ Java/Leetcode/1372.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max = 0;
+    public int longestZigZag(TreeNode root) {
+        path(root.left, 0, false);
+        path(root.right, 0, true);
+        return max;
+    }
+    
+    public void path(TreeNode node, int depth, boolean direction) { // true: right. false: false
+        max = Math.max(max, depth);
+        if(node == null) return;
+        path(node.left, direction ? depth + 1 : 0, false);
+        path(node.right, direction ? 0 : depth + 1, true);
+    }
+}
