56c6613c4c11b2d65cf53fb7fe78024f70ff3dee
uprightclear <uprightclear@gmail.com>
2022-05-22 13:26:52 -0700

    20220521 update

--- Java/Leetcode/1438.java
+++ Java/Leetcode/1438.java
@@ -38,3 +38,40 @@ class Solution {
         return end - start;
     }
 }
+
+
+public int longestSubarray(int[] nums, int limit) {
+    Deque<Integer> maxQ = new LinkedList<>();
+    Deque<Integer> minQ = new LinkedList<>();
+    
+    int start = 0;
+    int size = 0;
+    
+    for (int end = 0; end < nums.length; end++) {
+        
+        while (!maxQ.isEmpty() && maxQ.peekLast() < nums[end]) {
+            maxQ.pollLast();
+        } 
+        
+        while (!minQ.isEmpty() && minQ.peekLast() > nums[end]) {
+            minQ.pollLast();
+        }
+        
+        maxQ.addLast(nums[end]);
+        minQ.addLast(nums[end]);
+        
+        if (maxQ.peekFirst() - minQ.peekFirst() > limit) {
+            if (nums[start] == maxQ.peekFirst()) {
+                maxQ.pollFirst();
+            }
+            if (nums[start] == minQ.peekFirst()) {
+                minQ.pollFirst();
+            }
+            start++;
+        }
+        
+        size = Math.max(size, end - start + 1);
+    }
+    
+    return size;
+}
