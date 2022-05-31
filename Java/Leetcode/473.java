c36ce67c380d8f3e164fabd7981aa358989a6c7a
uprightclear <uprightclear@gmail.com>
2022-05-31 10:26:09 -0700

    20220531 update

---
+++ Java/Leetcode/473.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean makesquare(int[] matchsticks) {
+        int totalLen = Arrays.stream(matchsticks).sum();
+        if (totalLen % 4 != 0) {
+            return false;
+        }
+        Arrays.sort(matchsticks);
+        for (int i = 0, j = matchsticks.length - 1; i < j; i++, j--) {
+            int temp = matchsticks[i];
+            matchsticks[i] = matchsticks[j];
+            matchsticks[j] = temp;
+        }
+
+        int[] edges = new int[4];
+        return dfs(0, matchsticks, edges, totalLen / 4);
+    }
+
+    public boolean dfs(int index, int[] matchsticks, int[] edges, int len) {
+        if (index == matchsticks.length) {
+            return true;
+        }
+        for (int i = 0; i < edges.length; i++) {
+            edges[i] += matchsticks[index];
+            if (edges[i] <= len && dfs(index + 1, matchsticks, edges, len)) {
+                return true;
+            }
+            edges[i] -= matchsticks[index];
+        }
+        return false;
+    }
+}
