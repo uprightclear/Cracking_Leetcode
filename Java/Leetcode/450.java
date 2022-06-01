99a7ca861b438acf510e213987c0b1d2ac2638a7
uprightclear <uprightclear@gmail.com>
2022-06-01 10:16:51 -0700

    20220601 update

--- Java/Leetcode/450.java
+++ Java/Leetcode/450.java
@@ -14,46 +14,32 @@
  * }
  */
 class Solution {
-    /*
-    One step right and then always left
-    */
-    public int successor(TreeNode root) {
-      root = root.right;
-      while (root.left != null) root = root.left;
-      return root.val;
-    }
-  
-    /*
-    One step left and then always right
-    */
-    public int predecessor(TreeNode root) {
-      root = root.left;
-      while (root.right != null) root = root.right;
-      return root.val;
-    }
-  
-    public TreeNode deleteNode(TreeNode root, int key) {
-      if (root == null) return null;
-  
-      // delete from the right subtree
-      if (key > root.val) root.right = deleteNode(root.right, key);
-      // delete from the left subtree
-      else if (key < root.val) root.left = deleteNode(root.left, key);
-      // delete the current node
+  public TreeNode deleteNode(TreeNode root, int key) {
+      if(root == null) return null;
+      if(key > root.val) root.right = deleteNode(root.right, key);
+      else if(key < root.val) root.left = deleteNode(root.left, key);
       else {
-        // the node is a leaf
-        if (root.left == null && root.right == null) root = null;
-        // the node is not a leaf and has a right child
-        else if (root.right != null) {
-          root.val = successor(root);
-          root.right = deleteNode(root.right, root.val);
-        }
-        // the node is not a leaf, has no right child, and has a left child    
-        else {
-          root.val = predecessor(root);
-          root.left = deleteNode(root.left, root.val);
-        }
+          if(root.left == null && root.right == null) root = null;
+          else if(root.right != null) {
+              root.val = successor(root);
+              root.right = deleteNode(root.right, root.val);
+          } else {
+              root.val = predecessor(root);
+              root.left = deleteNode(root.left, root.val);
+          }
       }
       return root;
-    }
   }
+
+  public int predecessor(TreeNode root) {
+      root = root.left;
+      while(root.right != null) root = root.right;
+      return root.val;
+  }
+
+  public int successor(TreeNode root) {
+      root = root.right;
+      while(root.left != null) root = root.left;
+      return root.val;
+  }
+}
