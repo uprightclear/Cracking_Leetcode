10c015c20d0f950d16ee913af461447df39a47ab
uprightclear <uprightclear@gmail.com>
2022-06-02 16:45:14 -0700

    20220602 update

--- Java/Leetcode/366.java
+++ Java/Leetcode/366.java
@@ -1,3 +1,22 @@
+//better solution
+class Solution {
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        height(root, res);
+        return res;
+    }
+    private int height(TreeNode node, List<List<Integer>> res){
+        if(node == null)  return -1;
+        int level = 1 + Math.max(height(node.left, res), height(node.right, res));
+        if(res.size() < level + 1)  res.add(new ArrayList<>());
+        res.get(level).add(node.val);
+        return level;
+    }
+}
+
+
+
+
 class Solution {
     private List<Pair<Integer, Integer>> pairs;
     
