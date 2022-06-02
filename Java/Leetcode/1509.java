c4fe74f27a65755d02954894d1f0e43388921df5
uprightclear <uprightclear@gmail.com>
2022-06-02 11:11:02 -0700

    20220602 update

--- Java/Leetcode/1509.java
+++ Java/Leetcode/1509.java
@@ -7,6 +7,9 @@ class Solution {
         int ans = Integer.MAX_VALUE;
         
         for(int i = 0; i < 4; i++) {
+            //we change only the first or the last
+            //eg. if we select the first than we should compare it with the fourth to the last
+            //since we assume we have change the last 3 to the same as the fourth to the last
             ans = Math.min(ans, nums[n - 4 + i] - nums[i]);
         }
         
