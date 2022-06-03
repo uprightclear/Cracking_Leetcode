5804f060def263fc89e8545fdb220ca5e2a8bd61
uprightclear <uprightclear@gmail.com>
2022-06-02 18:06:53 -0700

    20220602 update

--- Java/Leetcode/818.java
+++ Java/Leetcode/818.java
@@ -2,8 +2,8 @@ class Solution {
     public int racecar(int target) {
         int[] f = new int[target + 2];
         //f[0]=0
-        f[1]=1; //A
-        f[2]=4; //AARA 或者 ARRA
+        f[1] = 1; //A
+        f[2] = 4; //AARA 或者 ARRA
         //f[3]=2; //AA
         //f[4]=5; //AARRA
         //f[5]=7; //AARARAA
@@ -16,15 +16,15 @@ class Solution {
             if(i == S) {
                 f[i] = k++;
                 // 2^k - 1
-                S = (1<<k) - 1;
+                S = (1 << k) - 1;
             } else {
                 // 情况1：连续k个A后，回退
-                f[i] = k + 1 + f[S-i];
+                f[i] = k + 1 + f[S - i];
                 // 情况2：连续k-1个A后，回退(0/1/.../k-2)步后，再前进
-                for (int back = 0; back <= k-2; back++) {
+                for (int back = 0; back <= k - 2; back++) {
                     // 回退后还需前进的距离：i+S(back)-S(k-1)
-                    int distance = i + (1<<back) - (1<<(k-1));
-                    f[i] = Math.min(f[i], (k-1)+2+back+f[distance]);
+                    int distance = i + (1 << back) - (1 << (k - 1));
+                    f[i] = Math.min(f[i], (k - 1) + 2 + back + f[distance]);
                 }
             }
         }
