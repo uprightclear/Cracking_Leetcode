25734f0d500e6e5955693036150b5f5835707155
uprightclear <uprightclear@gmail.com>
2022-06-02 22:34:14 -0700

    20220602 update

--- Java/Leetcode/2128.java
+++ Java/Leetcode/2128.java
@@ -21,3 +21,17 @@ class Solution {
         }
     }
 }
+
+
+//better solution
+class Solution {
+    public boolean removeOnes(int[][] grid) {
+        for (int[] g : grid){
+            for (int i = 0; i < g.length; i++){
+                if (Math.abs(g[i] - grid[0][i]) != Math.abs(g[0] - grid[0][0]))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
