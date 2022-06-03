84a8e449406dadfb1da6fcbeeee23a3d7edf573a
uprightclear <uprightclear@gmail.com>
2022-06-03 11:23:04 -0700

    20220603 update

--- Java/Leetcode/1268.java
+++ Java/Leetcode/1268.java
@@ -45,74 +45,38 @@ class Solution {
 
 
 
-
-
-// Custom class Trie with function to get 3 words starting with given prefix
-class Trie {
-
-    // Node definition of a trie
-    class Node {
-        boolean isWord = false;
-        List<Node> children = Arrays.asList(new Node[26]);
-    };
-    Node Root, curr;
-    List<String> resultBuffer;
-
-    // Runs a DFS on trie starting with given prefix and adds all the words in the resultBuffer, limiting result size to 3
-    void dfsWithPrefix(Node curr, String word) {
-        if (resultBuffer.size() == 3)
-            return;
-        if (curr.isWord)
-            resultBuffer.add(word);
-
-        // Run DFS on all possible paths.
-        for (char c = 'a'; c <= 'z'; c++)
-            if (curr.children.get(c - 'a') != null)
-                dfsWithPrefix(curr.children.get(c - 'a'), word + c);
-    }
-    Trie() {
-        Root = new Node();
-    }
-
-    // Inserts the string in trie.
-    void insert(String s) {
-
-        // Points curr to the root of trie.
-        curr = Root;
-        for (char c : s.toCharArray()) {
-            if (curr.children.get(c - 'a') == null)
-                curr.children.set(c - 'a', new Node());
-            curr = curr.children.get(c - 'a');
-        }
-
-        // Mark this node as a completed word.
-        curr.isWord = true;
-    }
-    List<String> getWordsStartingWith(String prefix) {
-        curr = Root;
-        resultBuffer = new ArrayList<String>();
-        // Move curr to the end of prefix in its trie representation.
-        for (char c : prefix.toCharArray()) {
-            if (curr.children.get(c - 'a') == null)
-                return resultBuffer;
-            curr = curr.children.get(c - 'a');
-        }
-        dfsWithPrefix(curr, prefix);
-        return resultBuffer;
-    }
-};
+//Trie
 class Solution {
-    List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        Trie trie = new Trie();
-        List<List<String>> result = new ArrayList<>();
-        // Add all words to trie.
-        for (String w : products)
-            trie.insert(w);
-        String prefix = new String();
-        for (char c : searchWord.toCharArray()) {
-            prefix += c;
-            result.add(trie.getWordsStartingWith(prefix));
-        }
-        return result;
+    class Trie {
+        Trie[] sub = new Trie[26];
+        LinkedList<String> suggestion = new LinkedList<>();
     }
-};
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        Trie root = new Trie();
+        for (String p : products) { // build Trie.
+            insert(p, root); // insert a product into Trie.
+        }
+        return search(searchWord, root);
+    }
+    private void insert(String p, Trie root) {
+        Trie t = root;
+        for (char c : p.toCharArray()) { // insert current product into Trie.
+            if (t.sub[c - 'a'] == null)
+                t.sub[c - 'a'] = new Trie();
+            t = t.sub[c - 'a'];
+            t.suggestion.offer(p); // put products with same prefix into suggestion list.
+            Collections.sort(t.suggestion);
+            if (t.suggestion.size() > 3) // maintain 3 lexicographically minimum strings.
+                t.suggestion.pollLast();        
+        }
+    }
+    private List<List<String>> search(String searchWord, Trie root) {
+        List<List<String>> ans = new ArrayList<>();
+        for (char c : searchWord.toCharArray()) { // search product.
+            if (root != null) // if there exist products with current prefix.
+                root = root.sub[c - 'a'];
+            ans.add(root == null ? Arrays.asList() : root.suggestion); // add it if there exist products with current prefix.
+        }
+        return ans;
+    }
+}
