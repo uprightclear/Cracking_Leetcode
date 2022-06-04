d18e11fffb26f0fb495d09fd814ba9711344f928
uprightclear <uprightclear@gmail.com>
2022-06-04 14:37:56 -0700

    20220604 update

--- Java/Leetcode/2135.java
+++ Java/Leetcode/2135.java
@@ -1,28 +1,28 @@
 class Solution {
     public int wordCount(String[] startWords, String[] targetWords) {
-        Set<Integer> startSet = new HashSet<>();
-        for (var s : startWords) {
-            startSet.add(toBinary(s));
-       }
-        int result = 0;
-        for (var t : targetWords) {
+        Set<Integer> starts = new HashSet<>();
+        for(String s : startWords) {
+            starts.add(toBinary(s));
+        }
+        int ans = 0;
+        for(String t : targetWords) {
             int target = toBinary(t);
             // 枚举追加的是哪个字母
-            for (char c : t.toCharArray()) {
-                if (startSet.contains(target - (1 << (c - 'a')))) {
-                    result++;
+            for(char c : t.toCharArray()) {
+                if(starts.contains(target - (1 << (c - 'a')))) {
+                    ans++;
                     break;
-               }
-           }
-       }
-        return result;
-   }
- 
-    private int toBinary(String s) {
+                }
+            }
+        }
+        return ans;
+    }
+    
+    public int toBinary(String s) {
         int res = 0;
-        for (char c : s.toCharArray()) {
+        for(char c : s.toCharArray()) {
             res += 1 << (c - 'a');
-       }
+        }
         return res;
-   }
- }
+    }
+}
