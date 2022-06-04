dca63711e8b6853e3f6a1921e9546f1b3fb1e855
uprightclear <uprightclear@gmail.com>
2022-06-03 17:34:27 -0700

    20220603 update

--- Java/Leetcode/1146.java
+++ Java/Leetcode/1146.java
@@ -1,23 +1,20 @@
 class SnapshotArray {
-    
     List<TreeMap<Integer, Integer>> arr;
-    int currId = 0;
-
+    int snapId = 0;
     public SnapshotArray(int length) {
-        arr = new ArrayList();
-        
-        for (int i = 0; i < length; i++) {
+        arr = new ArrayList<>();
+        for(int i = 0; i < length; i++) {
             arr.add(i, new TreeMap<Integer, Integer>());
             arr.get(i).put(0, 0);
         }
     }
     
     public void set(int index, int val) {
-        arr.get(index).put(currId, val);
+        arr.get(index).put(snapId, val);
     }
     
     public int snap() {
-        return currId++;
+        return snapId++;
     }
     
     public int get(int index, int snap_id) {
