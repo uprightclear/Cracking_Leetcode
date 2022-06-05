2bd92c229c5f548ae863c9a48e1387331e0e9e35
uprightclear <uprightclear@gmail.com>
2022-06-05 15:31:45 -0700

    20220605 update

--- Java/Leetcode/975.java
+++ Java/Leetcode/975.java
@@ -1,16 +1,18 @@
 class Solution {
-    public int oddEvenJumps(int[] A) {
-        int n  = A.length, res = 1;
+    public int oddEvenJumps(int[] arr) {
+        int n  = arr.length, res = 1;
         boolean[] higher = new boolean[n], lower = new boolean[n];
+        //higher[i]: whether can index i jump higher
+        //lower[i]: whether can index i jump lower
         higher[n - 1] = lower[n - 1] = true;
         TreeMap<Integer, Integer> map = new TreeMap<>();
-        map.put(A[n - 1], n - 1);
+        map.put(arr[n - 1], n - 1);
         for (int i = n - 2; i >= 0; --i) {
-            Map.Entry<Integer, Integer> hi = map.ceilingEntry(A[i]), lo = map.floorEntry(A[i]);
+            Map.Entry<Integer, Integer> hi = map.ceilingEntry(arr[i]), lo = map.floorEntry(arr[i]);
             if (hi != null) higher[i] = lower[(int)hi.getValue()];
             if (lo != null) lower[i] = higher[(int)lo.getValue()];
             if (higher[i]) res++;
-            map.put(A[i], i);
+            map.put(arr[i], i);
         }
         return res;
     }
