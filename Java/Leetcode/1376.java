1cf61147ef3d98bae3e451063df606e0124c6fc4
uprightclear <uprightclear@gmail.com>
2022-06-05 21:12:58 -0700

    20220605 update

--- Java/Leetcode/1376.java
+++ Java/Leetcode/1376.java
@@ -1,22 +1,22 @@
 class Solution {
-    public int numOfMinutes(final int n, final int headID, final int[] manager, final int[] informTime) {
-        final Map<Integer, List<Integer>> graph = new HashMap<>();
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        //key: manager  value: list of employees
+        Map<Integer, List<Integer>> graph = new HashMap<>();
         int total = 0;
-        for (int i = 0; i < manager.length; i++) {
+        for(int i = 0; i < manager.length; i++) {
             int j = manager[i];
-            if (!graph.containsKey(j))
-                graph.put(j, new ArrayList<>());
+            if(!graph.containsKey(j)) graph.put(j, new ArrayList<>());
             graph.get(j).add(i);
         }
         return dfs(graph, informTime, headID);
     }
-
-    private int dfs(Map<Integer, List<Integer>> graph, int[] informTime, int cur) {
+    
+    public int dfs(Map<Integer, List<Integer>> graph, int[] informTime, int cur) {
         int max = 0;
-        if (!graph.containsKey(cur))
-            return max;
-        for (int i = 0; i < graph.get(cur).size(); i++)
+        if(!graph.containsKey(cur)) return max;
+        for(int i = 0; i < graph.get(cur).size(); i++) {
             max = Math.max(max, dfs(graph, informTime, graph.get(cur).get(i)));
+        }
         return max + informTime[cur];
     }
 }
