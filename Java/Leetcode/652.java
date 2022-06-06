2ff19614326934ce21cfca97e8620fea0f37cb7a
uprightclear <uprightclear@gmail.com>
2022-06-06 10:40:48 -0700

    20220606 update

--- Java/Leetcode/652.java
+++ Java/Leetcode/652.java
@@ -1,4 +1,5 @@
 //序列化二叉树，是每一个子树都有唯一的序列化标识
+//o(n^2)
 class Solution {
     Map<String, Integer> count;
     List<TreeNode> ans;
@@ -18,6 +19,35 @@ class Solution {
         return serial;
     }
 }
+
+
+
+//o(n)
+class Solution {
+    int curId = 1;
+ 
+     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+         Map<String, Integer> serialToId = new HashMap<>();
+         Map<Integer, Integer> idToCount = new HashMap<>();
+         List<TreeNode> res = new LinkedList<>();
+         postorder(root, serialToId, idToCount, res);
+         return res;
+     }
+     
+     private int postorder(TreeNode root, Map<String, Integer> serialToId, Map<Integer, Integer> idToCount, List<TreeNode> res) {
+         if (root == null) return 0;
+         int leftId = postorder(root.left, serialToId, idToCount, res);
+         int rightId = postorder(root.right, serialToId, idToCount, res);
+         String curSerial = leftId + "," + root.val + "," + rightId;
+         int serialId = serialToId.getOrDefault(curSerial, curId);
+         if (serialId == curId) curId++;
+         serialToId.put(curSerial, serialId);
+         idToCount.put(serialId, idToCount.getOrDefault(serialId, 0) + 1);
+         if (idToCount.get(serialId) == 2) res.add(root);
+         return serialId;
+     }
+     
+ }
 
 
 
