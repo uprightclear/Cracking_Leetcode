9b6c08bf28dba87804f78f1ddf22a68215eea2ae
uprightclear <uprightclear@gmail.com>
2022-06-07 11:22:34 -0700

    20220607 update

---
+++ Java/Leetcode/1820.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maximumInvitations(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        int[] girlMatched = new int[n];
+        for(int i = 0; i < n; i++) girlMatched[i] = -1;
+        
+        int ans = 0;
+        
+        for(int i = 0; i < m; i++) {
+            Set<Integer> seenGirl = new HashSet<>();
+            if(dfs(grid, i, seenGirl, girlMatched)) ans++;
+        }
+        return ans;
+    }
+    
+    public boolean dfs(int[][] grid, int boy, Set<Integer> seenGirl, int[] girlMatched) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        for(int i = 0; i < n; i++) {
+            if(grid[boy][i] == 1 && !seenGirl.contains(i)) {
+                seenGirl.add(i);
+                if(girlMatched[i] == -1 || dfs(grid, girlMatched[i], seenGirl, girlMatched)) {
+                    girlMatched[i] = boy;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
