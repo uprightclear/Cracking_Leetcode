1c53939e2452b68b1f278de2c8a316d650452f6d
uprightclear <uprightclear@gmail.com>
2022-06-07 21:42:06 -0700

    20220607 update

--- Java/Leetcode/692.java
+++ Java/Leetcode/692.java
@@ -17,30 +17,30 @@ class Solution {
     }
 }
 
-
+//pq
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        Map<String, Integer> cnt = new HashMap<String, Integer>();
-        for (String word : words) {
-            cnt.put(word, cnt.getOrDefault(word, 0) + 1);
+        List<String> res = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        for (String w : words) {
+            map.put(w, map.getOrDefault(w, 0) + 1);
         }
-        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<Map.Entry<String, Integer>>(new Comparator<Map.Entry<String, Integer>>() {
-            public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2) {
-                return entry1.getValue() == entry2.getValue() ? entry2.getKey().compareTo(entry1.getKey()) : entry1.getValue() - entry2.getValue();
+        PriorityQueue<String> pq = new PriorityQueue<String>(new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if (map.get(s1) == map.get(s2)) {
+                    return s1.compareTo(s2);
+                }
+                return map.get(s2) - map.get(s1);
             }
         });
-        for (Map.Entry<String, Integer> entry : cnt.entrySet()) {
-            pq.offer(entry);
-            if (pq.size() > k) {
-                pq.poll();
+        pq.addAll(map.keySet());
+        for (int i = 0; i < k; i++) {
+            if (!pq.isEmpty()) {
+                res.add(pq.poll());
             }
         }
-        List<String> ret = new ArrayList<String>();
-        while (!pq.isEmpty()) {
-            ret.add(pq.poll().getKey());
-        }
-        Collections.reverse(ret);
-        return ret;
+        return res;
     }
 }
 
