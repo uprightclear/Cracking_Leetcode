a26263a0d7da905c44652862bcfe243726ddb726
uprightclear <uprightclear@gmail.com>
2022-06-08 15:03:07 -0700

    20220608 update

---
+++ Java/Leetcode/2092.java
@@ -0,0 +1,116 @@
+class Solution {
+    public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
+	
+		// create <time, index> map
+        Map<Integer, List<Integer>> timeToIndexes = new TreeMap<>();
+        int m = meetings.length;
+        for (int i = 0; i < m; i++) {
+            timeToIndexes.putIfAbsent(meetings[i][2], new ArrayList<>());
+            timeToIndexes.get(meetings[i][2]).add(i);
+        }
+		
+        UF uf = new UF(n);
+		// base
+        uf.union(0, firstPerson);
+		
+		// for every time we have a pool of people that talk to each other
+		// if someone knows a secret proir to this meeting - all pool will too
+		// if not - reset unions from this pool
+        for (int time : timeToIndexes.keySet()) {
+            Set<Integer> pool = new HashSet<>();
+			
+            for (int ind : timeToIndexes.get(time)) {
+                int[] currentMeeting = meetings[ind];
+                uf.union(currentMeeting[0], currentMeeting[1]);
+                pool.add(currentMeeting[0]);
+                pool.add(currentMeeting[1]);
+            }
+			
+			// meeting that took place now should't affect future
+			// meetings if people don't know the secret
+            for (int i : pool) if (!uf.connected(0, i)) uf.reset(i);
+        }
+		
+		// if the person is conneted to 0 - they know a secret
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) if (uf.connected(i,0)) ans.add(i);
+        return ans;
+    }
+    
+	// regular union find
+    private static class UF {
+        int[] parent, rank;
+		
+        public UF(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < n; i++) parent[i] = i;
+        }
+        
+        public void union(int p, int q) {
+            int rootP = find(p);
+            int rootQ = find(q);
+
+            if (rootP == rootQ)
+                return;
+
+            if (rank[rootP] < rank[rootQ]) {
+                parent[rootP] = rootQ;
+            } else {
+                parent[rootQ] = rootP;
+                rank[rootP]++;
+            }
+        }
+        
+        public int find(int p) {
+            while (parent[p] != p) {
+                p = parent[parent[p]];
+            }
+            return p;
+        }
+        
+        public boolean connected(int p, int q) {
+            return find(p) == find(q);
+        }
+        
+        public void reset(int p) {
+            parent[p] = p;
+            rank[p] = 0;
+        }
+    }
+}
+
+
+
+
+//dijkstra
+class Solution {
+    public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
+        List<int[]>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int[] meeting : meetings) {
+            graph[meeting[0]].add(new int[]{meeting[1], meeting[2]});
+            graph[meeting[1]].add(new int[]{meeting[0], meeting[2]});
+        }
+        List<Integer> res = new ArrayList<>();
+        // 按时间排序
+        PriorityQueue<int[]> q = new PriorityQueue<>((o1, o2) -> (o1[1] - o2[1]));
+        q.offer(new int[]{0, 0}); q.offer(new int[]{firstPerson, 0});
+        boolean[] vis = new boolean[n];
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int v = cur[0], t = cur[1];
+            if (vis[v]) continue;
+            vis[v] = true;
+            res.add(v);
+            for (int[] next : graph[cur[0]]) {
+                int p = next[0], time = next[1];
+                if (vis[p] || time < t) continue;
+                q.offer(next);
+            }
+        }
+        return res;
+    }
+}
