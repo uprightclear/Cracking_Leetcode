917d1e592e091375c4666d0d1b6a6703bd167d38
uprightclear <uprightclear@gmail.com>
2022-06-08 14:46:05 -0700

    20220608 update

---
+++ Java/Leetcode/759.java
@@ -0,0 +1,38 @@
+/*
+// Definition for an Interval.
+class Interval {
+    public int start;
+    public int end;
+
+    public Interval() {}
+
+    public Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+*/
+
+class Solution {
+    public List<Interval> employeeFreeTime(List<List<Interval>> avails) {
+
+        List<Interval> result = new ArrayList<>();
+
+        PriorityQueue<Interval> pq = new PriorityQueue<>((a, b) -> a.start - b.start);
+        for(List<Interval> avail : avails) {
+            for(Interval intv : avail) pq.offer(intv);
+        }
+
+        Interval temp = pq.poll();
+        while(!pq.isEmpty()) {
+            if(temp.end < pq.peek().start) { // no intersect
+                result.add(new Interval(temp.end, pq.peek().start));
+                temp = pq.poll(); // becomes the next temp interval
+            }else { // intersect or sub merged
+                temp = temp.end < pq.peek().end ? pq.peek() : temp;
+                pq.poll();
+            }
+        }
+        return result;
+    }
+}
