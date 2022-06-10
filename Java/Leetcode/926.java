950ba0df1bf74e26a1c86be540bed59c7a492da6
uprightclear <uprightclear@gmail.com>
2022-06-10 09:50:35 -0700

    20220610 update

--- Java/Leetcode/926.java
+++ Java/Leetcode/926.java
@@ -13,3 +13,17 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public int minFlipsMonoIncr(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][2];
+        dp[0][0] = s.charAt(0) == '0' ? 0 : 1;
+        dp[0][1] = s.charAt(0) == '0' ? 1 : 0;
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = s.charAt(i) == '0' ? dp[i - 1][0] : dp[i - 1][0] + 1;
+            dp[i][1] = s.charAt(i) == '0' ? Math.min(dp[i - 1][1], dp[i - 1][0]) + 1 : Math.min(dp[i - 1][1], dp[i - 1][0]);
+        }
+        return Math.min(dp[n - 1][0], dp[n - 1][1]);
+    }
+}
