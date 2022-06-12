1936f2c0422dce1328b22f8e4113fd48f89fabaf
uprightclear <uprightclear@gmail.com>
2022-06-12 10:37:35 -0700

    20220612 update

--- Java/Leetcode/1051.java
+++ Java/Leetcode/1051.java
@@ -11,3 +11,26 @@ class Solution {
         return ans;
     }
 }
+
+
+
+class Solution {
+    public int heightChecker(int[] heights) {
+        int m = Arrays.stream(heights).max().getAsInt();
+        int[] cnt = new int[m + 1];
+        for (int h : heights) {
+            ++cnt[h];
+        }
+
+        int idx = 0, ans = 0;
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= cnt[i]; ++j) {
+                if (heights[idx] != i) {
+                    ++ans;
+                }
+                ++idx;
+            }
+        }
+        return ans;
+    }
+}
