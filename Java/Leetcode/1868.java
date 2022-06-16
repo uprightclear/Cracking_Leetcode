0764e7062be8ee9d3ef8872e036935c2d24fc122
uprightclear <uprightclear@gmail.com>
2022-06-16 15:55:33 -0700

    20220616 update

--- Java/Leetcode/1868.java
+++ Java/Leetcode/1868.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<List<Integer>> findRLEArray(int[][] encoded1, int[][] encoded2) {
-        int p1 = 0, p2=0;
-        List<List<Integer>> res = new ArrayList<>();
-
+        List<List<Integer>> ans = new ArrayList<>();
+        int p1 = 0, p2 = 0;
         while(p1 < encoded1.length) {
             int len = Math.min(encoded1[p1][1], encoded2[p2][1]);
             int mult = encoded1[p1][0] * encoded2[p2][0];
-
-            if(res.size() > 0 && res.get(res.size()-1).get(0) == mult) //to handle cases like [[1,3],[2,3]] * [[6,3],[3,3]] --> [[6,6]]
-                res.get(res.size()-1).set(1, res.get(res.size()-1).get(1)+len); //update previous mult in res instead of adding a new one
-            else
-                res.add(Arrays.asList(mult, len));
-
+            
+            if(ans.size() > 0 && ans.get(ans.size() - 1).get(0) == mult) { //to handle cases like [[1,3],[2,3]] * [[6,3],[3,3]] --> [[6,6]]
+                ans.get(ans.size() - 1).set(1, ans.get(ans.size() - 1).get(1) + len);
+            } else {
+                ans.add(Arrays.asList(mult, len));
+            }
+            
             encoded1[p1][1] -= len;
             encoded2[p2][1] -= len;
             if(encoded1[p1][1] == 0) p1++;
             if(encoded2[p2][1] == 0) p2++;
         }
-        return res;
+        return ans;
     }
 }
