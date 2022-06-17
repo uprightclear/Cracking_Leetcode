1c453049a51b83e91759d1d91e37456825baa909
uprightclear <uprightclear@gmail.com>
2022-06-17 16:17:28 -0700

    20220617 update

--- Java/Leetcode/272.java
+++ Java/Leetcode/272.java
@@ -14,6 +14,39 @@
  * }
  */
 class Solution {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> res = new ArrayList<>();
+
+        Stack<Integer> s1 = new Stack<>(); // predecessors
+        Stack<Integer> s2 = new Stack<>(); // successors
+
+        inorder(root, target, false, s1);
+        inorder(root, target, true, s2);
+
+        while (k-- > 0) {
+            if (s1.isEmpty()) res.add(s2.pop());
+            else if (s2.isEmpty()) res.add(s1.pop());
+            else if (Math.abs(s1.peek() - target) < Math.abs(s2.peek() - target)) res.add(s1.pop());
+            else res.add(s2.pop());
+        }
+
+        return res;
+    }
+
+    // inorder traversal
+    void inorder(TreeNode root, double target, boolean reverse, Stack<Integer> stack) {
+        if (root == null) return;
+
+        inorder(reverse ? root.right : root.left, target, reverse, stack);
+        // early terminate, no need to traverse the whole tree
+        if ((reverse && root.val <= target) || (!reverse && root.val > target)) return;
+        // track the value of current node
+        stack.push(root.val);
+        inorder(reverse ? root.left : root.right, target, reverse, stack);
+    }
+}
+
+class Solution {
     public void inorder(TreeNode root, List<Integer> nums) {
         if (root == null) return;
         inorder(root.left, nums);
@@ -32,5 +65,26 @@ class Solution {
             }
         });
         return nums.subList(0, k);
+    }
+}
+
+
+class Solution {
+    public void inorder(TreeNode r, Queue<Integer> heap, int k) {
+        if (r == null) 
+            return;
+                    
+        inorder(r.left, heap, k);
+        heap.add(r.val);
+        if (heap.size() > k) 
+            heap.remove();
+        inorder(r.right, heap, k);
+    }
+
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        // init heap 'less close element first'
+        Queue<Integer> heap = new PriorityQueue<>((o1, o2) -> Math.abs(o1 - target) > Math.abs(o2 - target) ? -1 : 1);
+        inorder(root, heap, k);
+        return new ArrayList<>(heap);
     }
 }
