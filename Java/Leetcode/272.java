994e42eb931f96772889e3ccd4fb8562f981d2b9
uprightclear <uprightclear@gmail.com>
2022-06-17 16:04:34 -0700

    20220617 update

---
+++ Java/Leetcode/272.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void inorder(TreeNode root, List<Integer> nums) {
+        if (root == null) return;
+        inorder(root.left, nums);
+        nums.add(root.val);
+        inorder(root.right, nums);
+    }
+
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> nums = new ArrayList();
+        inorder(root, nums);
+        
+        Collections.sort(nums, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Math.abs(o1 - target) < Math.abs(o2 - target) ? -1 : 1;
+            }
+        });
+        return nums.subList(0, k);
+    }
+}
