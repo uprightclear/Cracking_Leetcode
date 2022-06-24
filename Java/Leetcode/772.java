c31153718116fd15c4ab2d6777ab91f07f2cb255
uprightclear <uprightclear@gmail.com>
2022-06-24 15:01:57 -0700

    20220624 update

--- Java/Leetcode/772.java
+++ Java/Leetcode/772.java
@@ -1,50 +1,31 @@
 class Solution {
     public int calculate(String s) {
-        if (s == null) {
-            return 0;
-        }    
         Queue<Character> q = new LinkedList<>();
-        for (char c : s.toCharArray()) {
-            q.offer(c);
-        }
+        for(char c : s.toCharArray()) q.offer(c);
         q.offer('+');
         return cal(q);
     }
     
-    private int cal(Queue<Character> q) {
+    public int cal(Queue<Character> q) {
         char sign = '+';
         int num = 0;
-        Stack<Integer> stack = new Stack<>();
-        while (!q.isEmpty()) {
+        Deque<Integer> stack = new LinkedList<>();
+        while(!q.isEmpty()) {
             char c = q.poll();
-            if (c == ' ') {
-                continue;
-            }
-            if (Character.isDigit(c)) {
-                num = 10 * num + c - '0';
-            } else if (c == '(') {
-                num = cal(q);
-            } else {
-                if (sign == '+') {
-                    stack.push(num);
-                } else if (sign == '-') {
-                    stack.push(-num);
-                } else if (sign == '*') {
-                    stack.push(stack.pop() * num);
-                } else if (sign == '/') {
-                    stack.push(stack.pop() / num);
-                }
+            if(Character.isDigit(c)) num = num * 10 + (c - '0');
+            else if(c == '(') num = cal(q);
+            else {
+                if(sign == '+') stack.push(num);
+                if(sign == '-') stack.push(-num);
+                if(sign == '*') stack.push(stack.pop() * num);
+                if(sign == '/') stack.push(stack.pop() / num);
                 num = 0;
                 sign = c;
-                if (c == ')') {
-                    break;
-                }
+                if(c == ')') break;
             }
         }
         int sum = 0;
-        while (!stack.isEmpty()) {
-            sum += stack.pop();
-        }
+        while(!stack.isEmpty()) sum += stack.pop();
         return sum;
     }
 }
