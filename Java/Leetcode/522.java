8ced923becf61290cf14d1b55b2711340ad243b3
uprightclear <uprightclear@gmail.com>
2022-06-26 11:06:31 -0700

    20220626 update

---
+++ Java/Leetcode/522.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findLUSlength(String[] strs) {
+        int n = strs.length;
+        int ans = -1;
+        for (int i = 0; i < n; ++i) {
+            boolean check = true;
+            for (int j = 0; j < n; ++j) {
+                if (i != j && isSubseq(strs[i], strs[j])) {
+                    check = false;
+                    break;
+                }
+            }
+            if (check) {
+                ans = Math.max(ans, strs[i].length());
+            }
+        }
+        return ans;
+    }
+
+    public boolean isSubseq(String s, String t) {
+        int ptS = 0, ptT = 0;
+        while (ptS < s.length() && ptT < t.length()) {
+            if (s.charAt(ptS) == t.charAt(ptT)) {
+                ++ptS;
+            }
+            ++ptT;
+        }
+        return ptS == s.length();
+    }
+}
