eb7b56567085b22bb59a0e2eaa758613f5cb633b
uprightclear <uprightclear@gmail.com>
2022-07-21 10:12:57 -0700

    20220721 update

---
+++ Java/Leetcode/757.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int intersectionSizeTwo(int[][] intervals) {
+        int n = intervals.length;
+        int res = 0;
+        int m = 2;
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            }
+            return a[0] - b[0];
+        });
+        List<Integer>[] temp = new List[n];
+        for (int i = 0; i < n; i++) {
+            temp[i] = new ArrayList<Integer>();
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = intervals[i][0], k = temp[i].size(); k < m; j++, k++) {
+                res++;
+                help(intervals, temp, i - 1, j);
+            }
+        }
+        return res;
+    }
+
+    public void help(int[][] intervals, List<Integer>[] temp, int pos, int num) {
+        for (int i = pos; i >= 0; i--) {
+            if (intervals[i][1] < num) {
+                break;
+            }
+            temp[i].add(num);
+        }
+    }
+}
