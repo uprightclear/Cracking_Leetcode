5a592387843e4b9219aae7cb408a7ebc3f34f4c5
uprightclear <uprightclear@gmail.com>
2022-07-23 10:51:32 -0700

    20220723 update

---
+++ Java/Leetcode/1184.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int distanceBetweenBusStops(int[] distance, int start, int destination) {
+        if (start > destination) {
+            int temp = start;
+            start = destination;
+            destination = temp;
+        }
+        int sum1 = 0, sum2 = 0;
+        for (int i = 0; i < distance.length; i++) {
+            if (i >= start && i < destination) {
+                sum1 += distance[i];
+            } else {
+                sum2 += distance[i];
+            }
+        }
+        return Math.min(sum1, sum2);
+    }
+}
