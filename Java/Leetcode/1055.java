cc22e61853718719f4716fff369fff99e3fbaa3b
uprightclear <uprightclear@gmail.com>
2022-07-25 15:59:15 -0700

    20220725 update

--- Java/Leetcode/1055.java
+++ Java/Leetcode/1055.java
@@ -19,3 +19,47 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public int shortestWay(String source, String target) {
+        char[] cs = source.toCharArray(), ts = target.toCharArray();
+        int res = 0;
+        for (int i = 0; i < ts.length; ) {
+            int oriI = i;
+            for (int j = 0; j < cs.length; j++) {
+                if (i < ts.length && cs[j] == ts[i])
+                    i++;
+            }
+            if (i == oriI) return -1;
+            res++;
+        }
+        return res;
+    }
+}
+
+
+class Solution {
+    public int shortestWay(String source, String target) {
+        char[] cs = source.toCharArray(), ts = target.toCharArray();
+        int res = 1;
+        List<Integer>[] idx = new List[26];
+        for (int i = 0; i < 26; i++) idx[i] = new ArrayList<>();
+        for (int i = 0; i < cs.length; i++) idx[cs[i] - 'a'].add(i);
+        int j = 0;
+        for (int i = 0; i < ts.length; ) {
+            List<Integer> tar = idx[ts[i] - 'a'];
+            if (tar.isEmpty()) return -1;
+            int k = Collections.binarySearch(tar,j);
+            if (k < 0) k = -k - 1;
+            if (k == tar.size()) {
+                res++;
+                j = 0;
+            } else {
+                j = tar.get(k) + 1;
+                i++;
+            }
+
+        }
+        return res;
+    }
+}
