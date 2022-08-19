1b0bbd1af6dc3a976d4c44564e113a98bd95b8c4
uprightclear <uprightclear@gmail.com>
2022-08-19 10:41:45 -0700

    20220819 update

---
+++ Java/Leetcode/654.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return construct(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode construct(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int best = left;
+        for (int i = left + 1; i <= right; ++i) {
+            if (nums[i] > nums[best]) {
+                best = i;
+            }
+        }
+        TreeNode node = new TreeNode(nums[best]);
+        node.left = construct(nums, left, best - 1);
+        node.right = construct(nums, best + 1, right);
+        return node;
+    }
+}
