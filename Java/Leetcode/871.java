bfa2d0bc1f6ccd3098c2e3ae2e0b2ae665223ef7
uprightclear <uprightclear@gmail.com>
2022-08-19 19:57:52 -0700

    20220819 update

---
+++ Java/Leetcode/871.java
@@ -0,0 +1,16 @@
+//dp[i]: the farthest location we can get to using i refueling stops.
+class Solution {
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        int N = stations.length;
+        long[] dp = new long[N + 1];
+        dp[0] = startFuel;
+        for (int i = 0; i < N; ++i)
+            for (int t = i; t >= 0; --t)
+                if (dp[t] >= stations[i][0])
+                    dp[t + 1] = Math.max(dp[t + 1], dp[t] + (long) stations[i][1]);
+
+        for (int i = 0; i <= N; ++i)
+            if (dp[i] >= target) return i;
+        return -1;
+    }
+}
