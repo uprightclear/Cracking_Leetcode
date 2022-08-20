ba440145626acfd92f76b5716f4b88911cdd9860
uprightclear <uprightclear@gmail.com>
2022-08-19 20:05:55 -0700

    20220819 update

--- Java/Leetcode/871.java
+++ Java/Leetcode/871.java
@@ -14,3 +14,21 @@ class Solution {
         return -1;
     }
 }
+
+
+class Solution {
+    public int minRefuelStops(int t, int start, int[][] ss) {
+        PriorityQueue<Integer> q = new PriorityQueue<>((a, b) -> b - a);
+        int n = ss.length, idx = 0;
+        int remain = start, loc = 0, ans = 0;
+        while (loc < t) {
+            if (remain == 0) {
+                if (!q.isEmpty() && ++ans >= 0) remain += q.poll();
+                else return -1;
+            }
+            loc += remain; remain = 0;
+            while (idx < n && ss[idx][0] <= loc) q.add(ss[idx++][1]);
+        }
+        return ans;
+    }
+}
