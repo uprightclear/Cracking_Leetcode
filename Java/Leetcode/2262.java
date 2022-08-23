cc539a07bf0339240b9f30e6e6100750cbd84e19
uprightclear <uprightclear@gmail.com>
2022-08-23 16:03:07 -0700

    20220823 update

---
+++ Java/Leetcode/2262.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long appealSum(String s) {
+        int last[] = new int[26];
+        long res = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            last[s.charAt(i) - 'a'] = i + 1;
+            for (int j : last)
+                res += j;
+        }
+        return res;
+    }
+}
