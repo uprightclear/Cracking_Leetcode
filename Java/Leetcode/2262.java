5fe02d23240a23e65264ccb9f0963d864253497e
uprightclear <uprightclear@gmail.com>
2022-08-23 16:07:32 -0700

    20220823 update

--- Java/Leetcode/2262.java
+++ Java/Leetcode/2262.java
@@ -10,3 +10,31 @@ class Solution {
         return res;
     }
 }
+
+
+class Solution {
+    public long appealSum(String s) {
+        int last[] = new int[26];
+        long res = 0, total = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            total += i + 1 - last[s.charAt(i) - 'a'];
+            last[s.charAt(i) - 'a'] = i + 1;
+            res += total;
+        }
+        return res;
+    }
+}
+
+
+class Solution {
+    public long appealSum(String s) {
+        int last[] = new int[26];
+        Arrays.fill(last, -1);
+        long res = 0, n = s.length();
+        for (int i = 0; i < s.length(); ++i) {
+            res += (i - last[s.charAt(i) - 'a']) * (n - i);
+            last[s.charAt(i) - 'a'] = i;
+        }
+        return res;
+    }
+}
