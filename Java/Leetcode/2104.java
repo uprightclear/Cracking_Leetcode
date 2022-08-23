e431d4b1f87457eb692a3876255760443002891d
uprightclear <uprightclear@gmail.com>
2022-08-23 10:30:21 -0700

    20220823 update

--- Java/Leetcode/2104.java
+++ Java/Leetcode/2104.java
@@ -13,3 +13,37 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public long subArrayRanges(int[] A) {
+        int n = A.length, j, k;
+        long res = 0;
+        
+        Stack<Integer> s = new Stack<>();
+        
+        //set A[j] as the min num
+        for (int i = 0; i <= n; i++) {
+            while (!s.isEmpty() && A[s.peek()] > (i == n ? Integer.MIN_VALUE : A[i])) {
+                j = s.pop();
+                k = s.isEmpty() ? -1 : s.peek();
+                res -= (long)A[j] * (i - j) * (j - k);
+
+            }
+            s.push(i);
+        }
+        
+        
+        s.clear();
+        //set A[j] as the max num
+        for (int i = 0; i <= n; i++) {
+            while (!s.isEmpty() && A[s.peek()] < (i == n ? Integer.MAX_VALUE : A[i])) {
+                j = s.pop();
+                k = s.isEmpty() ? -1 : s.peek();
+                res += (long)A[j] * (i - j) * (j - k);
+
+            }
+            s.push(i);
+        }
+        return res;
+    }
+}
