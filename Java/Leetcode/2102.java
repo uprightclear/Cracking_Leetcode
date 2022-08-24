4b4875b83af3983d38534a5876ebd3230309ebef
uprightclear <uprightclear@gmail.com>
2022-08-23 17:47:33 -0700

    20220823 update

---
+++ Java/Leetcode/2102.java
@@ -0,0 +1,43 @@
+class SORTracker {
+    private TreeSet<Location> locations = new TreeSet<>(); 
+    private Location lastReturned = new Location("", Integer.MAX_VALUE); // Last returned (or equivalent)
+    
+    public void add(String name, int score) {
+        Location location = new Location(name, score);
+        locations.add(location);
+        
+		// If the new location is before the last returned, we need to update the pointer
+        if (location.compareTo(lastReturned) < 0) {
+            lastReturned = locations.lower(lastReturned);
+        }
+    }
+    
+    public String get() {
+        // Update the pointer
+        lastReturned = locations.higher(lastReturned);
+        
+        return lastReturned.name;
+    }
+    
+    class Location implements Comparable<Location> {
+        private final String name;
+        private final int score;
+        
+        public Location(String name, int score) {
+            this.name = name;
+            this.score = score;
+        }
+        
+        public int compareTo(Location l) {
+            // Sort by score (desc), then name (asc)
+            return score != l.score ? -Integer.compare(score, l.score) : name.compareTo(l.name);
+        }
+    }
+}
+
+/**
+ * Your SORTracker object will be instantiated and called as such:
+ * SORTracker obj = new SORTracker();
+ * obj.add(name,score);
+ * String param_2 = obj.get();
+ */
