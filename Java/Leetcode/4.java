2c221dc758d03159f59a7c3d190756aa6a1081e7
uprightclear <uprightclear@gmail.com>
2022-08-25 16:13:20 -0700

    20220825 update

--- Java/Leetcode/4.java
+++ Java/Leetcode/4.java
@@ -1,28 +1,38 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length, n = nums2.length;
-        if(m > n) return findMedianSortedArrays(nums2, nums1);
-        
-        if(m == 0) return ((double)nums2[(n - 1) / 2] + (double)nums2[n / 2]) / 2;
-        
-        int len = m + n;
-        int astart = 0, aend = m;
-        int cuta, cutb;
-        while(astart <= aend) {
-            cuta = (astart + aend) / 2;
-            cutb = (len + 1) / 2 - cuta;
-            double l1 = cuta == 0 ? Integer.MIN_VALUE : nums1[cuta - 1];
-            double r1 = cuta == m ? Integer.MAX_VALUE : nums1[cuta];
-            double l2 = cutb == 0 ? Integer.MIN_VALUE : nums2[cutb - 1];
-            double r2 = cutb == n ? Integer.MAX_VALUE : nums2[cutb];
-            
-            if(l1 > r2) aend = cuta - 1;
-            else if(l2 > r1) astart = cuta + 1;
-            else {
-                if(len % 2 == 0) return (Math.max(l1, l2) + Math.min(r1, r2)) / 2;
-                else return Math.max(l1, l2);
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int m = nums1.length;
+        int n = nums2.length;
+        int left = 0, right = m;
+        // median1：前一部分的最大值
+        // median2：后一部分的最小值
+        int median1 = 0, median2 = 0;
+
+        while (left <= right) {
+            // 前一部分包含 nums1[0 .. i-1] 和 nums2[0 .. j-1]
+            // 后一部分包含 nums1[i .. m-1] 和 nums2[j .. n-1]
+            int i = (left + right) / 2;
+            int j = (m + n + 1) / 2 - i;
+
+            // nums_im1, nums_i, nums_jm1, nums_j 分别表示 nums1[i-1], nums1[i], nums2[j-1], nums2[j]
+            int nums_im1 = (i == 0 ? Integer.MIN_VALUE : nums1[i - 1]);
+            int nums_i = (i == m ? Integer.MAX_VALUE : nums1[i]);
+            int nums_jm1 = (j == 0 ? Integer.MIN_VALUE : nums2[j - 1]);
+            int nums_j = (j == n ? Integer.MAX_VALUE : nums2[j]);
+
+            if (nums_im1 <= nums_j) {
+                median1 = Math.max(nums_im1, nums_jm1);
+                median2 = Math.min(nums_i, nums_j);
+                left = i + 1;
+            } else {
+                right = i - 1;
             }
         }
-        return -1;
+
+        return (m + n) % 2 == 0 ? (median1 + median2) / 2.0 : median1;
     }
 }
+//https://leetcode.cn/problems/median-of-two-sorted-arrays/solution/xun-zhao-liang-ge-you-xu-shu-zu-de-zhong-wei-s-114/
