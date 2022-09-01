0d94f3d5b372bb251dc76bcf90f6b3f29008dc53
uprightclear <uprightclear@gmail.com>
2022-09-01 10:27:17 -0700

    20220901 update

---
+++ Java/Leetcode/687.java
@@ -0,0 +1,25 @@
+class Solution {
+    int res;
+
+    public int longestUnivaluePath(TreeNode root) {
+        res = 0;
+        dfs(root);
+        return res;
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left), right = dfs(root.right);
+        int left1 = 0, right1 = 0;
+        if (root.left != null && root.left.val == root.val) {
+            left1 = left + 1;
+        }
+        if (root.right != null && root.right.val == root.val) {
+            right1 = right + 1;
+        }
+        res = Math.max(res, left1 + right1);
+        return Math.max(left1, right1);
+    }
+}
