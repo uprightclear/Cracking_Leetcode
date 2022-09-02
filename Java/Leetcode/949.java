d1cd49e2416fcc025126579dfecb51be5e2b75d3
uprightclear <uprightclear@gmail.com>
2022-09-02 15:07:46 -0700

    20220902 update

---
+++ Java/Leetcode/949.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String largestTimeFromDigits(int[] A) {
+        String ans = "";
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                for (int k = 0; k < 4; ++k) {
+                    if (i == j || i == k || j == k) continue; // avoid duplicate among i, j & k.
+                    String h = "" + A[i] + A[j], m = "" + A[k] + A[6 - i - j - k], t = h + ":" + m; // hour, minutes, & time.
+                    if (h.compareTo("24") < 0 && m.compareTo("60") < 0 && ans.compareTo(t) < 0) ans = t; // hour < 24; minute < 60; update result.
+                }
+            }
+        }
+        return ans;
+    }
+}
