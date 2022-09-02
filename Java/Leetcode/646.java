bbaae75d1b87e5a1f142a93dd3d1cfe7080a9305
uprightclear <uprightclear@gmail.com>
2022-09-02 09:40:38 -0700

    20220902 update

---
+++ Java/Leetcode/646.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        int curr = Integer.MIN_VALUE, res = 0;
+        Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
+        for (int[] p : pairs) {
+            if (curr < p[0]) {
+                curr = p[1];
+                res++;
+            }
+        }
+        return res;
+    }
+}
