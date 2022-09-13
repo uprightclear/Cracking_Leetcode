b1518983557a2bb7b0aa41ed5d73f9ef70ecc607
uprightclear <uprightclear@gmail.com>
2022-09-13 10:33:48 -0700

    20220913 update

---
+++ Java/Leetcode/1619.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double trimMean(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        int partialSum = 0;
+        for (int i = n / 20; i < 19 * n / 20; i++) {
+            partialSum += arr[i];
+        }
+        return partialSum / (n * 0.9);
+    }
+}
