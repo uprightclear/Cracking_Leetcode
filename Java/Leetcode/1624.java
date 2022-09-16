b1d6f89ff62f014e22e941c4abc9b0787f03ab3e
uprightclear <uprightclear@gmail.com>
2022-09-16 10:01:29 -0700

    20220916 update

---
+++ Java/Leetcode/1624.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        int[] firstIndex = new int[26];
+        Arrays.fill(firstIndex, -1);
+        int maxLength = -1;
+        for (int i = 0; i < s.length(); i++) {
+            if (firstIndex[s.charAt(i) - 'a'] < 0) {
+                firstIndex[s.charAt(i) - 'a'] = i;
+            } else {
+                maxLength = Math.max(maxLength, i - firstIndex[s.charAt(i) - 'a'] - 1);
+            }
+        }
+        return maxLength;
+    }
+}
