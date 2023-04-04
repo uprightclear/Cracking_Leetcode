3c179ebfaecffa3911c083217ba26eac5102e3c3
uprightclear <uprightclear@gmail.com>
2023-04-04 12:13:20 -0400

    20230404 update

--- Java/Leetcode/2405.java
+++ Java/Leetcode/2405.java
@@ -1,16 +1,14 @@
 class Solution {
     public int partitionString(String s) {
-        int idx = 0;
-        int count = 0;
-        Map<Character, Boolean> mp = new HashMap<>(); // use a HashMap instead of an unordered_map
-        while (idx < s.length()) {
-            if (mp.containsKey(s.charAt(idx))) { // if current character has already appeared in current substring
-                count++; // increment count
-                mp.clear(); // clear HashMap to start a new substring
+        int count = 1;
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            if(map.containsKey(s.charAt(i))) {
+                count++;
+                map.clear();
             }
-            mp.put(s.charAt(idx), true); // mark current character as seen
-            idx++; // move to next character
+            map.put(s.charAt(i), 1);
         }
-        return count + 1; // return number of substrings (add 1 because we need to count the last substring as well)
+        return count;
     }
 }
