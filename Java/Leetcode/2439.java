148b75d3cf9cb601cee81d3a4d17780596012cad
uprightclear <uprightclear@gmail.com>
2023-04-04 22:41:25 -0400

    20230404 update

--- Java/Leetcode/2439.java
+++ Java/Leetcode/2439.java
@@ -9,3 +9,28 @@ public class Solution {
         return (int) result;
     }
 }
+
+class Solution {
+    static boolean check(int[] nums, int k) {
+        long have = 0;//前方的数字还可以帮我们后方的大数承载多少数字
+        for (int n : nums) {
+            if (n <= k) {
+                have += k - n;//较小数，可以算入承载量
+            } else {
+                if (have < n - k) return false;//承载不了了，该答案不可行
+                else have -= (n - k);//减去承载量
+            }
+        }
+        return true;
+    }
+    
+    public int minimizeArrayValue(int[] nums) {
+        int left = 0, right = 1000000000;
+        while (left < right) {//二分答案，寻找最大值
+            int mid = left + (right - left) / 2;
+            if (check(nums, mid)) right = mid;
+            else left = mid + 1;
+        }
+        return left;
+    }
+}
