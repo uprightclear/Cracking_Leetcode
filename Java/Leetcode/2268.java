8a68c96803a9b15b4eaab4357ae481507737958c
uprightclear <uprightclear@gmail.com>
2023-04-09 10:11:31 -0400

    20230409 update

--- Java/Leetcode/2268.java
+++ Java/Leetcode/2268.java
@@ -1,6 +1,7 @@
 class Solution {
     public int minimumKeypresses(String str) {
-        int[] freq = new int[26];
+        Integer[] freq = new Integer[26];
+        Arrays.fill(freq, 0);
         for(int i = 0; i < str.length(); i++)
             freq[str.charAt(i) - 'a']++;
         Arrays.sort(freq, (x, y) -> y - x);
