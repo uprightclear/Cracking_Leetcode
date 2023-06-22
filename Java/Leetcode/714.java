7776dbf85eba02a895174b860ce44d3ed6fcc453
uprightclear <uprightclear@gmail.com>
2023-06-22 11:23:56 +0800

    20230622 update

--- Java/Leetcode/714.java
+++ Java/Leetcode/714.java
@@ -25,3 +25,17 @@ class Solution {
         return dp[0];
     }
 }
+
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int buy = Integer.MIN_VALUE;
+        int sell = 0;
+
+        for (int price : prices) {
+            buy = Math.max(buy, sell - price);
+            sell = Math.max(sell, buy + price - fee);
+        }
+
+        return sell;
+    }
+}
