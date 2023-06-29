bc1bcf9ccfc2e0456cd9ef0393c554a6f85f8193
uprightclear <uprightclear@gmail.com>
2023-06-29 10:41:21 +0800

    20230629 update

---
+++ Java/Leetcode/250.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+    boolean all(TreeNode root, int val) {
+        if (root == null)
+            return true;
+        if (!all(root.left, root.val) | !all(root.right, root.val))
+            return false;
+        count++;
+        return root.val == val;
+    }
+    public int countUnivalSubtrees(TreeNode root) {
+        all(root, 0);
+        return count;
+    }
+}
