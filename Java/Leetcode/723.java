eda4fecc277ed945ce4b650b1546a26c3244d6c4
uprightclear <uprightclear@gmail.com>
2023-08-21 17:18:47 -0700

    20230821 update

--- Java/Leetcode/723.java
+++ Java/Leetcode/723.java
@@ -5,8 +5,8 @@ class Solution {
         for (int r = 0; r < R; ++r) {
             for (int c = 0; c + 2 < C; ++c) {
                 int v = Math.abs(board[r][c]);
-                if (v != 0 && v == Math.abs(board[r][c+1]) && v == Math.abs(board[r][c+2])) {
-                    board[r][c] = board[r][c+1] = board[r][c+2] = -v;
+                if (v != 0 && v == Math.abs(board[r][c + 1]) && v == Math.abs(board[r][c + 2])) {
+                    board[r][c] = board[r][c + 1] = board[r][c + 2] = -v;
                     todo = true;
                 }
             }
@@ -14,8 +14,8 @@ class Solution {
         for (int r = 0; r + 2 < R; ++r) {
             for (int c = 0; c < C; ++c) {
                 int v = Math.abs(board[r][c]);
-                if (v != 0 && v == Math.abs(board[r+1][c]) && v == Math.abs(board[r+2][c])) {
-                    board[r][c] = board[r+1][c] = board[r+2][c] = -v;
+                if (v != 0 && v == Math.abs(board[r + 1][c]) && v == Math.abs(board[r + 2][c])) {
+                    board[r][c] = board[r + 1][c] = board[r + 2][c] = -v;
                     todo = true;
                 }
             }
@@ -23,7 +23,7 @@ class Solution {
 
         for (int c = 0; c < C; ++c) {
             int wr = R - 1;
-            for (int r = R-1; r >= 0; --r)
+            for (int r = R - 1; r >= 0; --r)
                 if (board[r][c] > 0)
                     board[wr--][c] = board[r][c];
             while (wr >= 0)
