61e1f3819ad93d564ffc49d30e672e6ede4610fe
uprightclear <uprightclear@gmail.com>
2023-10-02 17:29:20 -0700

    20231002 update

---
+++ Java/Leetcode/1512.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        Map<Integer, Integer> counts = new HashMap<>();
+        int ans = 0;
+
+        for (int num: nums) {
+            ans += counts.getOrDefault(num, 0);
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+        }
+        
+        return ans;
+    }
+}
