aa40dacd6d89bc569977463faccfbb01d2655432
uprightclear <uprightclear@gmail.com>
2023-10-05 17:27:04 -0700

    20231005 update

--- Java/Leetcode/343.java
+++ Java/Leetcode/343.java
@@ -9,3 +9,35 @@ class Solution {
         return dp[n];
     }
 }
+
+
+class Solution {
+    int[] memo;
+    
+    public int integerBreak(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        
+        memo = new int[n + 1];
+        return dp(n);
+    }
+    
+    public int dp(int num) {
+        if (num <= 3) {
+            return num;
+        }
+        
+        if (memo[num] != 0) {
+            return memo[num];
+        }
+        
+        int ans = num;
+        for (int i = 2; i < num; i++) {
+            ans = Math.max(ans, i * dp(num - i));
+        }
+        
+        memo[num] = ans;
+        return ans;
+    }
+}
