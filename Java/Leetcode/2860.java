1535a46dd1763fbbff1f588aa2dc6fd5d5e8c3fb
uprightclear <uprightclear@gmail.com>
2023-10-06 20:05:03 -0700

    20231006 update

--- Java/Leetcode/2860.java
+++ Java/Leetcode/2860.java
@@ -1,10 +1,14 @@
 class Solution {
     public int countWays(List<Integer> nums) {
         Collections.sort(nums);
+        //if the first element is greater than 0, then we can select
         int ways = nums.get(0) == 0 ? 0 : 1;
+        //judge whether we can select first i + 1 elements
         for (int i = 0; i < nums.size(); ++i) {
-        int countWith = i + 1;
+            //countWidth: the length of the selected elements
+            int countWith = i + 1;
 
+        //if meet condition 1 & condition 2
         if (countWith > nums.get(i) && (i + 1 == nums.size() || countWith < nums.get(i + 1)))
             ways++;
         }
