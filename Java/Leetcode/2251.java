7740ad846363ae5814a69bb3a5214792b5800ef7
uprightclear <uprightclear@gmail.com>
2023-10-10 21:07:19 -0700

    20231010 update

--- Java/Leetcode/2251.java
+++ Java/Leetcode/2251.java
@@ -29,3 +29,36 @@ class Solution {
         return ans;
     }
 }
+
+
+class Solution {
+    public int[] fullBloomFlowers(int[][] flowers, int[] people) {
+        int[] sArr = new int[flowers.length];
+        int[] eArr = new int[flowers.length];
+        int index = 0;
+        for(int[] flower : flowers) {
+            int s = flower[0];
+            int e = flower[1];
+            sArr[index] = s;
+            eArr[index++] = e + 1;
+        }
+        Arrays.sort(sArr);
+        Arrays.sort(eArr);
+        for(int i = 0; i < people.length; i++) {
+            int start = bs(sArr, people[i]);
+            int end = bs(eArr, people[i]);
+            people[i] = start - end;
+        }
+        return people;
+    }
+
+    public int bs(int[] arr, int target) {
+        int l = 0, r = arr.length;
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            if(arr[mid] <= target) l = mid + 1;
+            else r = mid;
+        }
+        return l;
+    }
+}
