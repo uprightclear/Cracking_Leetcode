e68ddce84b4fbfa8c2ce91a7b54b531d99267be0
uprightclear <uprightclear@gmail.com>
2023-10-11 10:42:01 -0700

    20231011 update

---
+++ Java/Leetcode/2856.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minLengthAfterRemovals(List<Integer> nums) {
+        int len = nums.size();
+        int ans = nums.size();
+        int i = 0, j = (len + 1) / 2;
+        while(i < len / 2 && j < len) {
+            if(nums.get(i) < nums.get(j)) ans -= 2;
+            i++;
+            j++;
+        }
+        return ans;
+    }
+}
