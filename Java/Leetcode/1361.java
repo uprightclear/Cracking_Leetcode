908026a6043f1a5eaa263880830fb658979fa8ba
uprightclear <uprightclear@gmail.com>
2023-10-16 17:46:13 -0700

    20231016 update

--- Java/Leetcode/1361.java
+++ Java/Leetcode/1361.java
@@ -27,3 +27,60 @@ class Solution {
         return set.size() == 1 ? set.iterator().next() : -1;
     }
 }
+
+
+class Solution {
+    public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
+        UnionFind uf = new UnionFind(n);
+        for (int i = 0; i < n; i++) {
+            if (leftChild[i] >= 0 && !uf.union(i, leftChild[i])) {
+                return false;
+            }
+            if (rightChild[i] >= 0 && !uf.union(i, rightChild[i])) {
+                return false;
+            }
+        }
+        return uf.components() == 1;
+    }
+}
+
+class UnionFind {
+    private final int n;
+    private final int[] roots;
+    private int components;
+    
+    UnionFind (int n) {
+        this.n = n;
+        roots = new int[n];
+        for (int i = 0; i < n; i++) {
+            roots[i] = i;
+        }
+        components = n;
+    }
+    
+    public boolean union(int parent, int child) {
+        int rootParent = findRoot(parent);
+        int rootChild = findRoot(child);
+        
+        if (rootParent == rootChild || rootChild != child) {
+            return false;
+        }
+        
+        roots[rootChild] = rootParent;
+        components--;
+        return true;
+    }
+    
+    private int findRoot(int v) {
+        while (v != roots[v]) {
+            roots[v] = roots[roots[v]];
+            v = roots[v];
+        }
+        return v;
+    }
+    
+    public int components() {
+        return components;
+    }
+    
+}
