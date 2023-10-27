1d48459237124d073cbba228cc16e686a20a8caa
uprightclear <uprightclear@gmail.com>
2023-10-27 11:01:08 -0700

    20231027 update

--- Java/Leetcode/403.java
+++ Java/Leetcode/403.java
@@ -2,6 +2,8 @@ class Solution {
     Map<Integer, Integer> m = new HashMap<>();
     int[][] dp;
 
+    //dp[i][k]: whether we can jump to the end with status: current index = 1, preJump = k;
+    //1: True, 0: False, -1: Haven't been checked
     boolean solve(int i, int k, int[] stones) {
         if (i == stones.length - 1) {
             return true;
