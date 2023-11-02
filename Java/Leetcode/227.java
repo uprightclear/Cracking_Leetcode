7c03b8d56023b91c6fd57ed8b0c905cc7d2e16f9
uprightclear <uprightclear@gmail.com>
2023-11-02 15:33:12 -0700

    20231102 update

--- Java/Leetcode/227.java
+++ Java/Leetcode/227.java
@@ -5,7 +5,7 @@ class Solution {
         Stack<Integer> stack = new Stack<Integer>();
         int num = 0;
         char sign = '+';
-        for(int i  =0; i < len; i++){
+        for(int i = 0; i < len; i++){
             if(Character.isDigit(s.charAt(i))){
                 num = num * 10 + s.charAt(i) - '0';
             }
