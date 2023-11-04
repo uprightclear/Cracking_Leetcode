5faed3f9946e10cde7a0308801e7dfa46590445a
uprightclear <uprightclear@gmail.com>
2023-11-03 21:34:45 -0700

    20231103 update

--- Java/Leetcode/886.java
+++ Java/Leetcode/886.java
@@ -42,3 +42,33 @@ class Solution {
         return true;
     }
 }
+
+
+
+class Solution {
+    int[] color;
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        color = new int[n + 1];
+        for(int[] dislike : dislikes) {
+            int a = dislike[0], b = dislike[1];
+            adj.computeIfAbsent(a, value -> new ArrayList<Integer>()).add(b);
+            adj.computeIfAbsent(b, value -> new ArrayList<Integer>()).add(a);
+        }
+        for(int i = 1; i <= n; i++) {
+            if(color[i] == 0) {
+                if(!dfs(i, adj, 1)) return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean dfs(int source, Map<Integer, List<Integer>> adj, int red) {
+        color[source] = red;
+        for(int nei : adj.getOrDefault(source, new ArrayList<>())) {
+            if(color[nei] == -red) continue;
+            if(color[nei] == red || !dfs(nei, adj, -red)) return false;
+        }
+        return true;
+    }
+}
