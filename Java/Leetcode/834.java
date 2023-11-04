feb97b6cca0371f39040734ae6ef5f867d49b2f7
uprightclear <uprightclear@gmail.com>
2023-11-03 20:22:10 -0700

    20231103 update

--- Java/Leetcode/834.java
+++ Java/Leetcode/834.java
@@ -1,17 +1,16 @@
 class Solution {
     int[] ans, count;
     List<Set<Integer>> graph;
-    int N;
-    public int[] sumOfDistancesInTree(int N, int[][] edges) {
-        this.N = N;
-        graph = new ArrayList<Set<Integer>>();
-        ans = new int[N];
-        count = new int[N];
+    int n;
+    public int[] sumOfDistancesInTree(int n, int[][] edges) {
+        this.n = n;
+        graph = new ArrayList<>();
+        ans = new int[n];
+        count = new int[n];
         Arrays.fill(count, 1);
 
-        for (int i = 0; i < N; ++i)
-            graph.add(new HashSet<Integer>());
-        for (int[] edge: edges) {
+        for(int i = 0; i < n; i++) graph.add(new HashSet<Integer>());
+        for(int[] edge :edges) {
             graph.get(edge[0]).add(edge[1]);
             graph.get(edge[1]).add(edge[0]);
         }
@@ -21,19 +20,23 @@ class Solution {
     }
 
     public void dfs(int node, int parent) {
-        for (int child: graph.get(node))
-            if (child != parent) {
+        for(int child : graph.get(node)) {
+            if(child != parent) {
                 dfs(child, node);
                 count[node] += count[child];
                 ans[node] += ans[child] + count[child];
             }
+        }
     }
 
     public void dfs2(int node, int parent) {
-        for (int child: graph.get(node))
-            if (child != parent) {
-                ans[child] = ans[node] + (N - count[child]) - count[child];
+        for(int child : graph.get(node)) {
+            if(child != parent) {
+                ans[child] = ans[node] - count[child] + (n - count[child]);
                 dfs2(child, node);
             }
+        }
     }
+
+
 }
