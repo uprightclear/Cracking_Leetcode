91182c1b8d2c40db20f6ae9a68ffd053b00fc01e
uprightclear <uprightclear@gmail.com>
2023-11-03 17:17:05 -0700

    20231103 update

--- Java/Leetcode/1503.java
+++ Java/Leetcode/1503.java
@@ -1,14 +1,8 @@
 class Solution {
     public int getLastMoment(int n, int[] left, int[] right) {
         int ans = 0;
-        for (int num : left) {
-            ans = Math.max(ans, num);
-        }
-        
-        for (int num : right) {
-            ans = Math.max(ans, n - num);
-        }
-        
+        for (int num : left) ans = Math.max(ans, num);       
+        for (int num : right) ans = Math.max(ans, n - num);
         return ans;
     }
 }
