37e8b5d1b3e5c673bf5966076c9a16bc23672906
uprightclear <uprightclear@gmail.com>
2023-11-04 22:12:05 -0700

    20231104 update

--- Java/Leetcode/528.java
+++ Java/Leetcode/528.java
@@ -1,11 +1,10 @@
 class Solution {
     int[] pre;
     int total;
-    
     public Solution(int[] w) {
         pre = new int[w.length];
         pre[0] = w[0];
-        for (int i = 1; i < w.length; ++i) {
+        for(int i = 1; i < w.length; i++) {
             pre[i] = pre[i - 1] + w[i];
         }
         total = Arrays.stream(w).sum();
@@ -13,19 +12,22 @@ class Solution {
     
     public int pickIndex() {
         int x = (int) (Math.random() * total) + 1;
-        return binarySearch(x);
+        return bs(x);
     }
 
-    private int binarySearch(int x) {
-        int low = 0, high = pre.length - 1;
-        while (low < high) {
-            int mid = (high - low) / 2 + low;
-            if (pre[mid] < x) {
-                low = mid + 1;
-            } else {
-                high = mid;
-            }
+    private int bs(int x) {
+        int l = 0, r = pre.length;
+        while(l < r) {
+            int mid = (l + r) / 2;
+            if(pre[mid] >= x) r = mid;
+            else l = mid + 1;
         }
-        return low;
+        return l;
     }
 }
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
