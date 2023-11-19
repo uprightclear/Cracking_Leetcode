240922e234518f2ce915d00dd31ad61acf4a80ea
uprightclear <uprightclear@gmail.com>
2023-11-18 18:25:30 -0800

    20231118 update

---
+++ Java/Leetcode/1541.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minInsertions(String s) {
+        int res = 0, right = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '(') {
+                if (right % 2 > 0) {
+                    right--;
+                    res++;
+                }
+                right += 2;
+            } else {
+                right--;
+                if (right < 0) {
+                    right += 2;
+                    res++;
+                }
+            }
+        }
+        return right + res;
+    }
+}
