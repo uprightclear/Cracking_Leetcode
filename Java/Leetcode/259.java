0ff586d8b42b9cf3d7e7166e8bdf690911d55e87
uprightclear <uprightclear@gmail.com>
2024-01-10 19:10:56 -0800

    20240110 update

--- Java/Leetcode/259.java
+++ Java/Leetcode/259.java
@@ -23,3 +23,38 @@ class Solution {
         return sum;
     }
 }
+
+
+class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            sum += twoSumSmaller(nums, i + 1, target - nums[i]);
+        }
+        return sum;
+    }
+
+    private int twoSumSmaller(int[] nums, int startIndex, int target) {
+        int sum = 0;
+        for (int i = startIndex; i < nums.length - 1; i++) {
+            int j = binarySearch(nums, i, target - nums[i]);
+            sum += j - i;
+        }
+        return sum;
+    }
+
+    private int binarySearch(int[] nums, int startIndex, int target) {
+        int left = startIndex;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
