af817b6386a2194d7e62de19c334adcb7dd62cd0
uprightclear <uprightclear@gmail.com>
2024-01-10 17:05:21 -0800

    20240110 update

---
+++ Java/Leetcode/1573.java
@@ -0,0 +1,26 @@
+class Solution {
+    private static final int m = (int)1e9 + 7;
+    public int numWays(String s) {
+        int ones = 0, n = s.length();
+        for (int i = 0; i < n; ++i) {
+            ones += s.charAt(i) - '0';
+        }
+        if (ones == 0) {
+            return (int)((n - 2L) * (n - 1L) / 2 % m);
+        }
+        if (ones % 3 != 0) {
+            return 0;
+        }
+        int onesInEachSplitedBlock = ones / 3;
+        long waysOfFirstCut = 0, waysOfSecondCut = 0;
+        for (int i = 0, count = 0; i < n; ++i) {
+            count += s.charAt(i) - '0';
+            if (count == onesInEachSplitedBlock) {
+                ++waysOfFirstCut;
+            }else if (count == 2 * onesInEachSplitedBlock) {
+                ++waysOfSecondCut;
+            }
+        }
+        return (int)(waysOfFirstCut * waysOfSecondCut % m);      
+    }  
+}
