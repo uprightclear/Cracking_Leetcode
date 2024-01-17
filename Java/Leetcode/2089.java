8122273753894d6d4ba426ac9429e6dd1f6ad77c
uprightclear <uprightclear@gmail.com>
2024-01-16 20:49:15 -0800

    20240116 update

--- Java/Leetcode/2089.java
+++ Java/Leetcode/2089.java
@@ -8,3 +8,19 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        int count = 0, lessthan = 0;
+        for (int n : nums) {
+            if (n == target) count++;
+            if (n < target) lessthan++;
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            result.add(lessthan++);
+        }
+        return result;
+    }
+}
